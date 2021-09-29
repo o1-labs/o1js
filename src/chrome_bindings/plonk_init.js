837fb847c463a501b5102f748b62c876ee2ba203
Martin Minkov <minkovlmartin@gmail.com>
2021-09-29 11:51:44 -0700

    fix: Rename bundle files in plonk_init

--- src/chrome_bindings/plonk_init.js
+++ src/chrome_bindings/plonk_init.js
@@ -8,11 +8,11 @@ import init, * as plonk_wasm from './plonk_wasm.js';
   worker.onmessage = function() { set_workers_ready(); };
   await workers_ready;
   var newScript = document.createElement('script');
-  newScript.src = './snarkyjs_chrome_bindings.js'; // This is the bundled snarky bindings code -- see webpack.config.js on how the bundle is generated
+  newScript.src = './snarky_js_chrome.bc.js'; // This is the bundled snarky bindings code -- see webpack.config.js on how the bundle is generated
   document.body.appendChild(newScript);
   newScript.addEventListener('load', () => {
     var newScript = document.createElement('script');
-    newScript.src = './snarkyjs_chrome.js'; // This is the bundled snarkyjs code -- see webpack.config.js on how the bundle is generated
+    newScript.src = './snarkyjs_web.js'; // This is the bundled snarkyjs code -- see webpack.config.js on how the bundle is generated
     document.body.appendChild(newScript);
   });
 })();
