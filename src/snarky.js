21a6f466d74278f9c2881147faa122198f4af13d
mrmr1993 <mr_1993@hotmail.co.uk>
2021-10-01 20:03:47 +0100

    Tweaks to shared snarky.js file

--- src/snarky.js
+++ src/snarky.js
@@ -1,3 +1,14 @@
+// To build for node: Uncomment this export and comment the chrome exports
+/*export * from './node_bindings/snarky_js_node.bc.js';
+export const shutdown = () => { };*/
+
+// To build for web: Uncomment this section and comment the node exports
+export let Field;
+export let Bool;
+export let Circuit;
+export let Poseidon;
+export let Group;
+export let Scalar;
 export const shutdown = () => {
   if (global.wasm_rayon_poolbuilder) {
     global.wasm_rayon_poolbuilder.free();
@@ -9,16 +20,6 @@ export const shutdown = () => {
   }
 };
 
-// To build for node: Uncomment this export and comment the chrome exports
-// export * from './node_bindings/snarky_js_node.bc.js';
-
-// To build for web: Uncomment this section and comment the node exports
-export let Field;
-export let Bool;
-export let Circuit;
-export let Poseidon;
-export let Group;
-export let Scalar;
 
 import * as _snarky from './chrome_bindings/snarky_js_chrome.bc.js';
 (async () => {
