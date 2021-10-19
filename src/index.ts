9189fc797a437bec6dd79c929577b602e10d5eb1
mrmr1993 <mr_1993@hotmail.co.uk>
2021-10-19 14:34:29 +0100

    Remove some redundant changes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './snarky';
 export * from './lib/signature';
 export * from './lib/circuit_value';
 export * from './lib/merkle_proof';
+
+import { shutdown } from './snarky';
+shutdown();
