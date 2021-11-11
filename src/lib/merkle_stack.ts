c8f5c87e53a7cda26abddfa9a502f50fc0d77cf6
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-11 10:29:55 -0800

    merkle stack

---
+++ src/lib/merkle_stack.ts
@@ -0,0 +1,56 @@
+import { AsFieldElements, Circuit, Field, Poseidon } from '../snarky';
+import { CircuitValue } from './circuit_value';
+import { Optional } from './optional';
+import { MerkleList } from './party';
+
+// TODO: Implement AsFieldElements
+export class MerkleStack<A extends CircuitValue> {
+  commitment: Field;
+  eltTyp: AsFieldElements<A>;
+  values:
+  | { computed: true; value: Array<[A, Field]> }
+  | { computed: false; f: () => Array<[A, Field]> };
+
+  static pushCommitment<A extends CircuitValue>(x: A, comm: Field): Field {
+    return Poseidon.hash([comm].concat(x.toFieldElements()));
+  }
+
+  constructor(eltTyp: AsFieldElements<A>, f: () => Array<[A, Field]>) {
+    this.values = { computed: false, f };
+    this.eltTyp = eltTyp;
+    // TODO
+    this.commitment = Field.zero;
+  }
+
+  getValues(): Array<[A, Field]> {
+    if (this.values.computed === true) {
+      return this.values.value;
+    } else {
+      let v = this.values.f();
+      this.values = { computed: true, value: v };
+      return v;
+    }
+  }
+
+  pop(): A {
+    this.commitment.isZero().assertEquals(false);
+    let tail = Circuit.witness(Field, () => {
+      let xs = this.getValues();
+      let [_, tl] = xs[xs.length - 1];
+      return tl;
+    });
+    let value = Circuit.witness(this.eltTyp, () => {
+      let xs = this.getValues();
+      let last = xs.pop();
+      if (last === undefined) {
+        throw new Error('pop from empty list');
+      }
+      return last[0];
+    });
+    this.commitment.assertEquals(
+      MerkleStack.pushCommitment(value, tail)
+    );
+    this.commitment = tail;
+    return value;
+  }
+}
