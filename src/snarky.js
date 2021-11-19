3c87dee4f504666ed4b5cfbdd01b5f7bba838e24
Gregor <gregor.mitscha-baude@gmx.at>
2021-11-19 11:11:14 +0100

    make browser version more compatible with node ver -> for frameworks like next which may try to execute the same code in different environments

--- src/snarky.js
+++ src/snarky.js
@@ -1,5 +1,13 @@
 import { initSnarkyJS } from './chrome_bindings/plonk_init.js';
 export { Field, Bool, Circuit, Poseidon, Group, Scalar };
 
+// for compat with node version
+export { shutdown, snarkyReady, isSnarkyReady };
+let shutdown = () => {};
+let snarkyReady = Promise.resolve();
+let isSnarkyReady = true;
+
+// we should probably not do this
 await initSnarkyJS();
+
 let { Field, Bool, Circuit, Poseidon, Group, Scalar } = window.__snarky;
