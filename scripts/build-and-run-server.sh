cc9b9fa61240befdd8ebd656f1e6f19533e3f28f
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-22 15:00:15 -0800

    fix script

--- scripts/build-and-run-server.sh
+++ scripts/build-and-run-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npm run build:server
+#npm run build:server
+npm run prepublish:server
 node --experimental-wasm-modules --experimental-modules --experimental-wasm-threads -i dist/server/index.js
