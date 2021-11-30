80217df5e84e0c3d150d6b7a9b861b5775457cfe
Gregor <gregor.mitscha-baude@gmx.at>
2021-11-30 22:32:57 +0100

    delete index.d.ts

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './snarky';
-export * from './lib/signature';
-export * from './lib/circuit_value';
-export * from './lib/merkle_proof';
-
-export * from './lib/int';
-export * as Mina from './lib/mina';
-export * as DataStore from './lib/data_store';
-export * from './lib/snapp';
