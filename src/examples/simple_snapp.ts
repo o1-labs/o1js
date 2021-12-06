07f916562e03811b708416df239463b8cdfa7436
Gregor <gregor.mitscha-baude@gmx.at>
2021-12-06 13:58:27 +0100

    more doccomments on Field

---
+++ src/examples/simple_snapp.ts
@@ -0,0 +1,40 @@
+import {
+  Field,
+  Bool,
+  state,
+  State,
+  method,
+  UInt64,
+  PublicKey,
+  PrivateKey,
+  SmartContract,
+  Mina,
+} from '@o1labs/snarkyjs';
+
+class SimpleSnapp extends SmartContract {
+  @state(Field) x: State<Field>;
+
+  constructor(address: PublicKey, x: Field) {
+    super(address);
+    this.x = State.init(x);
+  }
+
+  @method async update(y: Field) {
+    Field.zero.equals(0);
+    Bool(true).equals(true);
+  }
+}
+
+const Local = Mina.LocalBlockchain();
+Mina.setActiveInstance(Local);
+
+const account1 = Local.testAccounts[0].privateKey;
+const snappPrivkey = PrivateKey.random();
+const snappPubkey = snappPrivkey.toPublicKey();
+
+await Mina.transaction(account1, async () => {
+  let snapp = new SimpleSnapp(snappPubkey, Field(2));
+  await snapp.update(Field(3));
+})
+  .send()
+  .wait();
