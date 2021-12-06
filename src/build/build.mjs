c01db425b3286af1c99542001dd3be4207e03599
Gregor <gregor.mitscha-baude@gmx.at>
2021-12-06 09:14:14 +0100

    change back es2021 -> esnext just for snarky-run ...so that consumers can use top level await with npx snarky-run

--- src/build/build.mjs
+++ src/build/build.mjs
@@ -16,7 +16,7 @@ async function buildAndImport(srcPath, { keepFile = false }) {
     format: 'esm',
     platform: 'node',
     outfile,
-    target: 'es2021',
+    target: 'esnext',
     resolveExtensions: ['.node.js', '.ts', '.js'],
     logLevel: 'error',
     plugins: [typescriptPlugin(tsConfig), makeNodeModulesExternal()],
