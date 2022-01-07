f42dd70d51976fb46550952aec38153bd4f97486
Gregor <gregor.mitscha-baude@gmx.at>
2022-01-07 15:26:52 +0100

    bring the simple_snapp example up to date with deploy

--- src/examples/simple_snapp.ts
+++ src/examples/simple_snapp.ts
@@ -1,6 +1,5 @@
 import {
   Field,
-  Bool,
   state,
   State,
   method,
@@ -9,19 +8,24 @@ import {
   PrivateKey,
   SmartContract,
   Mina,
-} from 'snarkyjs';
+  Party,
+} from '@o1labs/snarkyjs';
 
 class SimpleSnapp extends SmartContract {
   @state(Field) x: State<Field>;
 
-  constructor(address: PublicKey, x: Field) {
+  constructor(address: PublicKey) {
     super(address);
+  }
+
+  deploy(initialBalance: UInt64, x: Field) {
+    this.balance.addInPlace(initialBalance);
     this.x = State.init(x);
   }
 
   @method async update(y: Field) {
-    Field.zero.equals(0);
-    Bool(true).equals(true);
+    let x = await this.x.get();
+    this.x.set(x.add(y));
   }
 }
 
@@ -29,12 +33,32 @@ const Local = Mina.LocalBlockchain();
 Mina.setActiveInstance(Local);
 
 const account1 = Local.testAccounts[0].privateKey;
+const account2 = Local.testAccounts[1].privateKey;
+
 const snappPrivkey = PrivateKey.random();
 const snappPubkey = snappPrivkey.toPublicKey();
 
 await Mina.transaction(account1, async () => {
-  let snapp = new SimpleSnapp(snappPubkey, Field(2));
+  let snapp = new SimpleSnapp(snappPubkey);
+
+  const amount = UInt64.fromNumber(1000000);
+  const p = await Party.createSigned(account2);
+  p.balance.subInPlace(amount);
+
+  snapp.deploy(amount, Field(1));
+})
+  .send()
+  .wait();
+
+let snappState = (await Mina.getAccount(snappPubkey)).snapp.appState[0];
+console.log('initial state: ' + snappState);
+
+await Mina.transaction(account1, async () => {
+  let snapp = new SimpleSnapp(snappPubkey);
   await snapp.update(Field(3));
 })
   .send()
   .wait();
+
+snappState = (await Mina.getAccount(snappPubkey)).snapp.appState[0];
+console.log('final state: ' + snappState);
