b813f0710a4f746d0bed5de0496bc1dc1116c9aa
Gregor <gregor.mitscha-baude@gmx.at>
2022-01-11 20:15:21 +0100

    review suggestions

--- scripts/build-and-run-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm run build
-node --experimental-wasm-modules --experimental-modules --experimental-wasm-threads -i dist/server/index.js
--- scripts/build-and-run-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm run build:web
-pushd dist/web
-python3 server.py
-
