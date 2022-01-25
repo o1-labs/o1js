6448325e6a141f760004db6303f9839304a39a34
Gregor <gregor.mitscha-baude@gmx.at>
2022-01-25 17:47:53 +0100

    make preimage example deterministic

--- src/examples/ex00_preimage.ts
+++ src/examples/ex00_preimage.ts
@@ -26,7 +26,7 @@ await isReady;
 console.log('generating keypair...');
 const kp = Main.generateKeypair();
 
-const preimage = Field.random();
+const preimage = Field.one;
 const hash = Poseidon.hash([preimage]);
 
 console.log('prove...');
