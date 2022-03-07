a07b13ecb4773d7c6237f79d32c82d5895bc8bc3
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-07 21:24:17 +0100

    expose poseidon sponge from ocaml

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export * from './lib/snapp';
 export * from './lib/optional';
 export * from './lib/proof_system';
 export * from './lib/party';
+export * as Encryption from './lib/encryption';
+export * as Encoding from './lib/encoding';
