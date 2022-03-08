afca744df1a4b997fa03aad406f7ea019afdf623
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-08 12:52:29 +0100

    implement David's better encryption algo

--- src/lib/encryption.ts
+++ src/lib/encryption.ts
@@ -23,54 +23,55 @@ class Sponge {
 Poseidon.Sponge = Sponge;
 
 type CipherText = {
-  c1: Group;
-  c2: Group;
+  publicKey: Group;
   cipherText: Field[];
 };
 
 function encrypt(message: Field[], otherPublicKey: PublicKey) {
-  const r = Group.generator.scale(Scalar.random()); // inefficient Group.random
-  const y = Scalar.random();
+  // key exchange
+  let privateKey = Scalar.random();
+  let publicKey = Group.generator.scale(privateKey);
+  let sharedSecret = otherPublicKey.g.scale(privateKey);
 
-  const c1 = Group.generator.scale(y);
-  // c2 is a "blinded" version of r. r can be recovered from [c1, c2]
-  // if you have the private key corresponding to h
-  const c2 = r.add(otherPublicKey.g.scale(y));
-
-  // Now, r is a secret known only to the posessor of h's private key,
-  // so, we can use it to initialize a Poseidon sponge expand r out into
-  // a stream of field elements known only to the posessor of h's private key.
-  //
-  // We then use that stream of field elements to mask the message by adding
-  // them together.
   let sponge = new Poseidon.Sponge();
-  sponge.absorb(r.x);
-  sponge.absorb(r.y);
+  sponge.absorb(sharedSecret.x); // don't think we need y, that's enough entropy
 
+  // encryption
   let cipherText = [];
-  for (let i = 0; i < message.length; ++i) {
-    let x = sponge.squeeze();
-    cipherText.push(message[i].add(x));
+  for (let messageChunk of message) {
+    let keyStream = sponge.squeeze();
+    let encryptedChunk = messageChunk.add(keyStream);
+    sponge.absorb(encryptedChunk); // absorb for the auth tag
+    cipherText.push(encryptedChunk);
   }
+  // authentication tag
+  let authenticationTag = sponge.squeeze();
+  cipherText.push(authenticationTag);
 
-  return { c1, c2, cipherText };
+  return { publicKey, cipherText };
 }
 
-function decrypt({ c1, c2, cipherText }: CipherText, privateKey: PrivateKey) {
-  // recover r from c1, c2
-  let r = c2.sub(c1.scale(privateKey.s));
+function decrypt(
+  { publicKey, cipherText }: CipherText,
+  privateKey: PrivateKey
+) {
+  // key exchange
+  let sharedSecret = publicKey.scale(privateKey.s);
 
-  // We can now compute the same stream of field elements used in encryption
   let sponge = new Poseidon.Sponge();
-  sponge.absorb(r.x);
-  sponge.absorb(r.y);
+  sponge.absorb(sharedSecret.x);
+  let authenticationTag = cipherText.pop();
 
-  // Unmask m to get msg
-  let msg = [];
-  for (let i = 0; i < cipherText.length; ++i) {
-    let x = sponge.squeeze();
-    msg.push(cipherText[i].sub(x));
+  // decryption
+  let message = [];
+  for (let encryptedChunk of cipherText) {
+    let keyStream = sponge.squeeze();
+    let messageChunk = encryptedChunk.sub(keyStream);
+    sponge.absorb(encryptedChunk);
+    message.push(messageChunk);
   }
+  // authentication tag
+  sponge.squeeze().assertEquals(authenticationTag!);
 
-  return msg;
+  return message;
 }
