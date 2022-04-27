cadea589be6e029cd0194ffb458d2f3965b638ea
Gregor <gregor.mitscha-baude@gmx.at>
2022-04-27 16:28:33 +0200

    add some documentation to addons.js

--- src/snarky/addons.js
+++ src/snarky/addons.js
@@ -12,39 +12,44 @@ function getJsooRuntime() {
 
 /**
  *
- * @param { import("../snarky").Keypair } kp
- * @returns { import("./wrapper").WasmModule['WasmFpSrs'] } srs
+ * @param { import("../snarky").Keypair } keypair SNARK keypair, as returned by Circuit.generateKeypair
+ * @returns { import("./wrapper").WasmModule['WasmFpSrs'] } The SRS (structured reference string), needed to reconstruct the keypair later
  */
-function getSrs(kp) {
-  return kp.value[2][4];
+function getSrs(keypair) {
+  return keypair.value[2][4]; // this operates on the OCaml representation of a keypair
 }
 
 /**
  *
- * @param { import("../snarky").VerificationKey } vk
+ * @param { import("../snarky").VerificationKey } verificationKey the verification key of a Circuit
+ * @returns { string } string representation of the verification key
  */
-function serializeVerificationKey(vk) {
+function serializeVerificationKey(verificationKey) {
   let wasm = getWasm();
   let runtime = getJsooRuntime();
-  let isFp = vk.value[4].constructor.name === 'WasmFpSrs';
+  let isFp = verificationKey.value[4].constructor.name === 'WasmFpSrs';
   return isFp
     ? wasm.caml_pasta_fp_plonk_verifier_index_serialize(
-        runtime.caml_pasta_fp_plonk_verifier_index_to_rust(vk.value)
+        runtime.caml_pasta_fp_plonk_verifier_index_to_rust(
+          verificationKey.value
+        )
       )
     : wasm.caml_pasta_fq_plonk_verifier_index_serialize(
-        runtime.caml_pasta_fq_plonk_verifier_index_to_rust(vk.value)
+        runtime.caml_pasta_fq_plonk_verifier_index_to_rust(
+          verificationKey.value
+        )
       );
 }
 
 /**
- * @param { import("./wrapper").WasmModule['WasmFpSrs'] } srs
- * @param { String } vkBytes
- * @returns { import("../snarky").VerificationKey }
+ * @param { import("./wrapper").WasmModule['WasmFpSrs'] } srs the "structured reference string", a set of precomputed values needed for verifying proofs
+ * @param { String } serializedVk string representation of a Circuit verification key
+ * @returns { import("../snarky").VerificationKey } the recovered verification key
  */
-function recoverVerificationKey(srs, vkBytes) {
+function recoverVerificationKey(srs, serializedVk) {
   let vkRust = getWasm().caml_pasta_fp_plonk_verifier_index_deserialize(
     srs,
-    vkBytes
+    serializedVk
   );
   let vk = getJsooRuntime().caml_pasta_fp_plonk_verifier_index_of_rust(vkRust);
   return Circuit.getVerificationKey(vk);
