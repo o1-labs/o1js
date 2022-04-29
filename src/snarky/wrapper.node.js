58363d66b40539fbd8abbedac286e23bf9450e89
Gregor <gregor.mitscha-baude@gmx.at>
2022-04-29 11:32:50 +0200

    make shutdown exit the node process

--- src/snarky/wrapper.node.js
+++ src/snarky/wrapper.node.js
@@ -10,14 +10,11 @@ function getWasm() {
 }
 
 let didShutdown = false;
-function shutdown() {
+async function shutdown() {
   if (global.wasm_rayon_poolbuilder && !didShutdown) {
     didShutdown = true;
     global.wasm_rayon_poolbuilder.free();
-    return Promise.all(
-      global.wasm_workers.map(async (worker) => {
-        await worker.terminate();
-      })
-    );
+    await Promise.all(global.wasm_workers.map((worker) => worker.terminate()));
+    process.exit(0);
   }
 }
