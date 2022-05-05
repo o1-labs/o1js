712122bbdddcd65ed6a096c3f2aac4af411e97d1
Gregor <gregor.mitscha-baude@gmx.at>
2022-05-05 19:04:57 +0200

    add explaining comments to the berkeley example

--- src/examples/simple_zkapp_berkeley.ts
+++ src/examples/simple_zkapp_berkeley.ts
@@ -1,3 +1,12 @@
+/**
+ * This is an example for interacting with the Berkeley QANet, directly from snarkyjs.
+ *
+ * At a high level, it does the following:
+ * -) try fetching the account corresponding to the `zkappAddress` from chain
+ * -) if the account doesn't exist or is not a zkapp account yet, deploy a zkapp to it and initialize on-chain state
+ * -) if the zkapp is already deployed, send a state-updating transaction which proves execution of the "update" method
+ */
+
 import {
   Field,
   state,
@@ -16,6 +25,7 @@ import {
 
 await isReady;
 
+// a very simple SmartContract
 class SimpleZkapp extends SmartContract {
   @state(Field) x = State<Field>();
 
@@ -31,12 +41,17 @@ class SimpleZkapp extends SmartContract {
   }
 }
 
+// you can use this with any spec-compliant graphql endpoint
 let Berkeley = Mina.BerkeleyQANet(
   'https://proxy.berkeley.minaexplorer.com/graphql'
 );
 Mina.setActiveInstance(Berkeley);
 
+// hard-coded test account which has enough MINA to fund our zkapps
 let whaleKey = Berkeley.testAccounts[0].privateKey;
+let { account: whaleAccount } = await fetchAccount(whaleKey.toPublicKey());
+let { nonce, balance } = whaleAccount!;
+console.log(`using whale account with nonce ${nonce}, balance ${balance}`);
 
 // let zkappKey = PrivateKey.random();
 let zkappKey = PrivateKey.fromBase58(
@@ -48,24 +63,23 @@ let transactionFee = 100_000_000;
 let initialState = Field(1);
 let doCompile = true;
 
-// check if the zkapp is already deployed, based on whether the account exists and its first zkapp state is != 0
-let zkapp = new SimpleZkapp(zkappAddress);
-let x = await zkapp.x.fetch();
-let isDeployed = x.equals(0).not().toBoolean() ?? false;
-
-let { account: whaleAccount } = await fetchAccount(whaleKey.toPublicKey());
-let { nonce, balance } = whaleAccount!;
-console.log(`using whale account with nonce ${nonce}, balance ${balance}`);
-
+// compile the SmartContract to get the verification key (if deploying) or the cache the provers (if updating)
+// this can take a while...
 console.log('Compiling smart contract...');
 let { verificationKey } = doCompile
   ? await compile(SimpleZkapp, zkappAddress)
   : { verificationKey: undefined };
 
+// check if the zkapp is already deployed, based on whether the account exists and its first zkapp state is != 0
+let zkapp = new SimpleZkapp(zkappAddress);
+let x = await zkapp.x.fetch();
+let isDeployed = x.equals(0).not().toBoolean() ?? false;
+
+// if the zkapp is not deployed yet, create a deploy transaction
 if (!isDeployed) {
-  console.log(`Deploying zkapp for public key ${zkappAddress.toBase58()}!`);
-  console.log('Generating deploy transaction...');
-  let tx = await Berkeley.transaction(
+  console.log(`Deploying zkapp for public key ${zkappAddress.toBase58()}.`);
+  // the `transaction()` interface
+  let transaction = await Mina.transaction(
     { privateKey: whaleKey, fee: transactionFee },
     () => {
       const p = Party.createSigned(whaleKey, { isSameAsFeePayer: true });
@@ -73,22 +87,33 @@ if (!isDeployed) {
       zkapp.deploy({ zkappKey, verificationKey });
     }
   );
-  await tx.send().wait();
+  // if you want to inspect the transaction, you can print it out:
+  // console.log(transaction.toGraphqlQuery());
+
+  // send the transaction to the graphql endpoint
+  console.log('Sending the transaction...');
+  await transaction.send().wait();
 }
 
+// if the zkapp is not deployed yet, create an update transaction
 if (isDeployed) {
   let x = zkapp.x.get();
-  let xNext = x.add(2);
-  console.log(
-    `Found deployed zkapp with state x=${x}!\nCreating zkapp update to x=${xNext}.`
-  );
-  let tx = await Berkeley.transaction(
+  console.log(`Found deployed zkapp, updating state ${x} -> ${x.add(2)}.`);
+  let transaction = await Mina.transaction(
     { privateKey: whaleKey, fee: transactionFee },
-    () => zkapp.update(Field(2))
+    () => {
+      zkapp.update(Field(2));
+    }
   );
-  await tx.prove();
-  console.log(tx.toGraphqlQuery());
-  await tx.send().wait();
+  // fill in the proof - this can take a while...
+  await transaction.prove();
+
+  // if you want to inspect the transaction, you can print it out:
+  // console.log(transaction.toGraphqlQuery());
+
+  // send the transaction to the graphql endpoint
+  console.log('Sending the transaction...');
+  await transaction.send().wait();
 }
 
 shutdown();
