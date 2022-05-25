c685a3a24d97fcfa1d2dadd4ad2f65df8196d569
Gregor <gregor.mitscha-baude@gmx.at>
2022-05-25 13:28:33 +0200

    add circuit test to example

--- src/examples/circuit_string.ts
+++ src/examples/circuit_string.ts
@@ -1,29 +1,49 @@
-import { isReady, CircuitString } from 'snarkyjs';
+import { isReady, CircuitString, Circuit, Bool } from 'snarkyjs';
+import * as assert from 'assert/strict';
 
 await isReady;
+
+const str = CircuitString.fromString('Your size');
+const not_same_str = CircuitString.fromString('size');
+assert.equal(str.equals(not_same_str).toBoolean(), false);
+
+Circuit.runAndCheck(() => {
+  const str = Circuit.witness(CircuitString, () => {
+    return CircuitString.fromString('Your size');
+  });
+  const not_same_str = Circuit.witness(CircuitString, () => {
+    return CircuitString.fromString('size');
+  });
+  str.equals(not_same_str).assertEquals(false);
+});
 
 const equal1 = CircuitString.fromString('These strings are equivalent');
 const equal2 = CircuitString.fromString('These strings are equivalent');
 
-const circuitString = CircuitString.fromString('This string completely encompasses this string');
+const circuitString = CircuitString.fromString(
+  'This string completely encompasses this string'
+);
 const substring = CircuitString.fromString('this string');
 
-
-if (!equal1.equals(equal2).toBoolean()) throw Error('Strings are not equivalent');
+if (!equal1.equals(equal2).toBoolean())
+  throw Error('Strings are not equivalent 1');
 console.log('Equivalent: "', equal1.toString(), '", "', equal2.toString(), '"');
 
-if (!circuitString.substring(35, 46).equals(substring).toBoolean()) throw Error('Strings are not equivalent');
-console.log('Equivalent: "', circuitString.substring(35, 46).toString(), '", "', substring.toString(), '"');
-
-if (!circuitString.contains(substring).toBoolean()) throw Error('String does not contain substring')
-
+if (!circuitString.substring(35, 46).equals(substring).toBoolean())
+  throw Error('Strings are not equivalent 2');
 console.log(
-  circuitString.length(),
-  substring.length()
-)
-
-console.log(
-  circuitString.append(substring).toString()
+  'Equivalent: "',
+  circuitString.substring(35, 46).toString(),
+  '", "',
+  substring.toString(),
+  '"'
 );
 
-console.log('Everything looks good!')
+if (!circuitString.contains(substring).toBoolean())
+  throw Error('String does not contain substring');
+
+// console.log(circuitString.length(), substring.length());
+
+console.log(circuitString.append(substring).toString());
+
+console.log('Everything looks good!');
