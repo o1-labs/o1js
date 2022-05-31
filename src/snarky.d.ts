94c9ed3ecdbcdad51ee8acebebfab8b61a4279b5
Martin Minkov <minkovlmartin@gmail.com>
2022-05-31 14:57:39 -0700

    feat: Remove endoScale function with TODO

--- src/snarky.d.ts
+++ src/snarky.d.ts
@@ -584,7 +584,7 @@ export class Group {
   sub(y: Group): Group;
   neg(): Group;
   scale(y: Scalar): Group;
-  endoScale(y: EndoScalar): Group;
+  // TODO: Add this function when OCaml bindings are implemented : endoScale(y: EndoScalar): Group;
 
   assertEquals(y: Group): void;
   equals(y: Group): Bool;
@@ -605,7 +605,7 @@ export class Group {
   static sub(x: Group, y: Group): Group;
   static neg(x: Group): Group;
   static scale(x: Group, y: Scalar): Group;
-  static endoScale(x: Group, y: EndoScalar): Group;
+  // TODO: Add this function when OCaml bindings are implemented : static endoScale(x: Group, y: EndoScalar): Group;
 
   static assertEqual(x: Group, y: Group): void;
   static equal(x: Group, y: Group): Bool;
