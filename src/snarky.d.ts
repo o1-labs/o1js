8aa9f9230a166f5bdee778ce2ae7b8416db4bc85
Martin Minkov <minkovlmartin@gmail.com>
2022-06-01 10:30:05 -0700

    feat: Restrict Group.fromJSON type to an object literal (#223)

--- src/snarky.d.ts
+++ src/snarky.d.ts
@@ -621,7 +621,13 @@ declare class Group {
   static sizeInFields(): number;
 
   static toJSON(x: Group): JSONValue;
-  static fromJSON(x: JSONValue): Group | null;
+  static fromJSON({
+    x,
+    y,
+  }: {
+    x: string | number;
+    y: string | number;
+  }): Group | null;
 }
 
 declare class Sponge {
