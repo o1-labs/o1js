6d4fbddbac0fff636cef9f7ebfeb4f205fbe42ad
Gregor <gregor.mitscha-baude@gmx.at>
2022-06-07 22:00:37 +0200

    check that precondition works in a proof

--- src/examples/simple_zkapp.mjs
+++ src/examples/simple_zkapp.mjs
@@ -23,6 +23,7 @@ class SimpleZkapp extends SmartContract {
     this.x.set(initialState);
   }
   update(y) {
+    this.account.balance.assertEquals(this.account.balance.get());
     let x = this.x.get();
     this.x.set(x.add(y));
   }
