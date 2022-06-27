61be29559bc80e8edbeb1e6a831448ab7b833a53
Gregor <gregor.mitscha-baude@gmx.at>
2022-06-27 12:37:40 +0200

    WIP: verify for zkprogram proofs, not working yet

--- src/examples/program.ts
+++ src/examples/program.ts
@@ -1,4 +1,4 @@
-import { SelfProof, Field, ZkProgram } from 'snarkyjs';
+import { SelfProof, Field, ZkProgram, verify } from 'snarkyjs';
 
 let MyProgram = ZkProgram({
   publicInput: Field,
@@ -26,24 +26,45 @@ let MyProgram = ZkProgram({
 let MyProof = ZkProgram.Proof(MyProgram);
 
 console.log('compiling MyProgram...');
-await MyProgram.compile();
+let { verificationKey } = await MyProgram.compile();
+console.log('verification key', verificationKey.slice(0, 10) + '..');
 
 console.log('proving base case...');
 let proof = await MyProgram.baseCase(Field.zero);
 proof = testJsonRoundtrip(proof);
 
+console.log('verify...');
+let ok =
+  (await verify(proof, verificationKey)) &&
+  (await verify(proof.toJSON(), verificationKey));
+console.log('ok?', ok);
+
 console.log('proving step 1...');
 proof = await MyProgram.inductiveCase(Field.one, proof);
 proof = testJsonRoundtrip(proof);
+
+console.log('verify...');
+ok =
+  (await verify(proof, verificationKey)) &&
+  (await verify(proof.toJSON(), verificationKey));
+console.log('ok?', ok);
 
 console.log('proving step 2...');
 proof = await MyProgram.inductiveCase(Field(2), proof);
 proof = testJsonRoundtrip(proof);
 
-console.log('ok?', proof.publicInput.toString() === '2');
+console.log('verify...');
+ok = await verify(proof, verificationKey);
+console.log('verify (json version)...');
+ok = ok && (await verify(proof.toJSON(), verificationKey));
+
+console.log('ok?', ok && proof.publicInput.toString() === '2');
 
 function testJsonRoundtrip(proof: any): any {
   let jsonProof = proof.toJSON();
-  console.log({ ...jsonProof, proof: jsonProof.proof.slice(0, 10) + '..' });
+  console.log(
+    'json proof',
+    JSON.stringify({ ...jsonProof, proof: jsonProof.proof.slice(0, 10) + '..' })
+  );
   return MyProof.fromJSON(jsonProof);
 }
