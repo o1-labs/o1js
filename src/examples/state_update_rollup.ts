fc4dcf7ad5293e6f89ec5f8f4560e5d2a09bf48e
Gregor <gregor.mitscha-baude@gmx.at>
2022-07-07 17:13:23 +0200

    add missing preconditions to example & simplify

--- src/examples/state_update_rollup.ts
+++ src/examples/state_update_rollup.ts
@@ -9,7 +9,6 @@ import {
   Party,
   isReady,
   Permissions,
-  DeployArgs,
   circuitValue,
 } from 'snarkyjs';
 
@@ -17,54 +16,60 @@ await isReady;
 
 // version of the "simple zkapp" which accepts concurrent updates
 class StateUpdateZkapp extends SmartContract {
-  @state(Field) counter = State<Field>();
-
+  // the "stateUpdate" field describes a state and how it can be updated
+  // (the state doesn't have to be related to on-chain state. here it is, though)
   stateUpdate = SmartContract.StateUpdate({
+    // type for the state
     state: circuitValue<{ counter: Field }>({ counter: Field }),
+    // type for the update
     update: Field,
+    // function that says how to apply an update
     apply(state: { counter: Field }, update: Field) {
       state.counter = state.counter.add(update);
       return state;
     },
   });
 
-  deploy(args: DeployArgs) {
-    super.deploy(args);
-    if (!doProofs) {
-      this.setPermissions({
-        ...Permissions.default(),
-        editState: Permissions.proofOrSignature(),
-        editSequenceState: Permissions.proofOrSignature(),
-      });
-    }
-    this.counter.set(initialCounter);
-  }
+  // on-chain version of our state. it will typically lag behind the
+  // version that's implicitly represented by the list of updates
+  @state(Field) counter = State<Field>();
+  // helper field to store the point in the update history that our on-chain state is at
+  @state(Field) stateHash = State<Field>();
 
+  // emits an update
   @method incrementCounter(increment: Field) {
     this.stateUpdate.emit(increment);
   }
 
   @method rollupStateUpdate() {
-    // remark: it's not feasible to pass in the pending updates as method arguments, because they don't have static size
-    let { state, stateHash } = this.stateUpdate.applyUpdates(
-      stateAndPendingUpdates
-    );
+    // get previous state and assert that it's the same as on-chain state
+    let counter = this.counter.get();
+    let oldStateHash = this.stateHash.get();
+    this.account.balance.get();
+    this.account.balance.assertNothing();
+    this.counter.assertEquals(counter);
+    this.stateHash.assertEquals(oldStateHash);
+    // compute the new state and hash from pending updates
+    // remark: it's not feasible to pass in the pending updates as method arguments, because they have dynamic size
+    let { state, stateHash } = this.stateUpdate.applyUpdates({
+      state: { counter },
+      stateHash: oldStateHash,
+      updates: pendingUpdates,
+    });
+    // update on-chain state
     this.counter.set(state.counter);
-    // return the new stateAndPendingUpdates
-    return { state, stateHash, updates: [] };
+    this.stateHash.set(stateHash);
   }
 }
 
 const doProofs = true;
 const initialCounter = Field.zero;
 
-// this is a data structure where we internally keep track of the current state, state hash and pending updates
+// this is a data structure where we internally keep track of the pending updates
 // TODO: get these from a Mina node / the local blockchain
-let stateAndPendingUpdates = {
-  state: { counter: initialCounter },
-  stateHash: SmartContract.StateUpdate.initialStateHash,
-  updates: [] as Field[][],
-};
+// note: each entry in pendingUpdates is itself an array -- the list of updates emitted by one method
+// this is the structure we need to do the hashing correctly
+let pendingUpdates: Field[][] = [];
 
 let Local = Mina.LocalBlockchain();
 Mina.setActiveInstance(Local);
@@ -86,6 +91,15 @@ console.log('deploy');
 let tx = await Mina.transaction(feePayer, () => {
   Party.fundNewAccount(feePayer);
   zkapp.deploy({ zkappKey });
+  if (!doProofs) {
+    zkapp.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+      editSequenceState: Permissions.proofOrSignature(),
+    });
+  }
+  zkapp.counter.set(initialCounter);
+  zkapp.stateHash.set(SmartContract.StateUpdate.initialStateHash);
 });
 tx.send();
 
@@ -98,7 +112,7 @@ tx = await Mina.transaction(feePayer, () => {
 if (doProofs) await tx.prove();
 tx.send();
 // update internal state
-stateAndPendingUpdates.updates.push([increment]);
+pendingUpdates.push([increment]);
 
 console.log('update 2');
 increment = Field(2);
@@ -109,29 +123,20 @@ tx = await Mina.transaction(feePayer, () => {
 if (doProofs) await tx.prove();
 tx.send();
 // update internal state
-stateAndPendingUpdates.updates.push([increment]);
+pendingUpdates.push([increment]);
 
 console.log('state (on-chain): ' + zkapp.counter.get());
-console.log('state (internal): ' + stateAndPendingUpdates.state.counter);
-console.log(
-  'pending updates (internal):',
-  JSON.stringify(stateAndPendingUpdates.updates)
-);
+console.log('pending updates:', JSON.stringify(pendingUpdates));
 
 console.log('rollup transaction');
-let newStateAndUpdates: typeof stateAndPendingUpdates;
 tx = await Mina.transaction(feePayer, () => {
-  newStateAndUpdates = zkapp.rollupStateUpdate();
+  zkapp.rollupStateUpdate();
   if (!doProofs) zkapp.sign(zkappKey);
 });
 if (doProofs) await tx.prove();
 tx.send();
-// update internal state
-stateAndPendingUpdates = newStateAndUpdates!;
+// reset pending updates
+pendingUpdates = [];
 
 console.log('state (on-chain): ' + zkapp.counter.get());
-console.log('state (internal): ' + stateAndPendingUpdates.state.counter);
-console.log(
-  'pending updates (internal):',
-  JSON.stringify(stateAndPendingUpdates.updates)
-);
+console.log('pending updates:', JSON.stringify(pendingUpdates));
