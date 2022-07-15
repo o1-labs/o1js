60882d7f35e6c213a39509659dbece3d726bbda4
Gregor <gregor.mitscha-baude@gmx.at>
2022-07-15 08:48:40 +0200

    address pr feedback

--- src/lib/circuit_value.test.ts
+++ src/lib/circuit_value.test.ts
@@ -13,7 +13,16 @@ describe('circuit', () => {
     expect(x.toString()).toBe('-2');
   });
 
-  it('Circuit.switch throws when mask has >1 nonzero elements', () => {
+  it('Circuit.switch returns 0 if the mask has only false elements', () => {
+    const x = Circuit.switch([Bool(false), Bool(false), Bool(false)], Int64, [
+      Int64.from(-1),
+      Int64.from(-2),
+      Int64.from(-3),
+    ]);
+    expect(x.toString()).toBe('0');
+  });
+
+  it('Circuit.switch throws when mask has >1 true elements', () => {
     expect(() =>
       Circuit.switch([Bool(true), Bool(true), Bool(false)], Int64, [
         Int64.from(-1),
