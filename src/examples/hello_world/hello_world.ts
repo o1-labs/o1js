f2ff0625b496a88a2637525b8fb234f9c03603c1
ymekuria <ymekuria@gmail.com>
2022-07-17 15:46:17 -0700

    feat: add update @method

--- src/examples/hello_world/hello_world.ts
+++ src/examples/hello_world/hello_world.ts
@@ -19,4 +19,14 @@ class HelloWorld extends SmartContract {
     super.deploy(input);
     this.x.set(3);
   }
+
+  @method update(squared: Field, admin: PrivateKey) {
+    const x = this.x.get();
+    this.x.assertEquals(x);
+    x.square().assertEquals(squared);
+    this.x.set(squared);
+
+    const adminPk = admin.toPublicKey();
+    this.account.delegate.assertEquals(adminPk);
+  }
 }
