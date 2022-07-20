0ab8088d5b8bd6ce7c8bfe7d35cb3d0b6b31182b
ymekuria <ymekuria@gmail.com>
2022-07-19 17:00:12 -0700

    feat passing test stub

--- src/examples/hello_world/tests/hello_world.integration.test.ts
+++ src/examples/hello_world/tests/hello_world.integration.test.ts
@@ -2,14 +2,13 @@ import { HelloWorld } from '../hello_world';
 import {
   isReady,
   shutdown,
-  Field,
   Mina,
   PrivateKey,
   PublicKey,
   Party,
 } from '../../../../dist/server';
 
-function setupLocalBlockchain() {
+function setupLocalBlockchain(): PrivateKey {
   const Local = Mina.LocalBlockchain();
   Mina.setActiveInstance(Local);
   return Local.testAccounts[0].privateKey;
@@ -23,7 +22,6 @@ async function localDeploy(
   const txn = await Mina.transaction(deployerAccount, () => {
     Party.fundNewAccount(deployerAccount);
     zkAppInstance.deploy({ zkappKey: zkAppPrivatekey });
-    zkAppInstance.init();
   });
   await txn.send().wait();
 }
@@ -32,17 +30,23 @@ describe('HelloWorld', () => {
   let deployerAccount: PrivateKey,
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey;
-  beforeAll(async () => await isReady);
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    await isReady;
     deployerAccount = setupLocalBlockchain();
     zkAppPrivateKey = PrivateKey.random();
     zkAppAddress = zkAppPrivateKey.toPublicKey();
   });
+
   afterAll(async () => {
     // `shutdown()` internally calls `process.exit()` which will exit the running Jest process early.
     // Specifying a timeout of 0 is a workaround to defer `shutdown()` until Jest is done running all tests.
     // This should be fixed with https://github.com/MinaProtocol/mina/issues/10943
     setTimeout(shutdown, 0);
+  });
+
+  describe('Update Contract State', () => {
+    it('Generates and deploys the `HelloWorld` smart contract', async () => {});
+    it('Can change state with update method', async () => {});
   });
 });
