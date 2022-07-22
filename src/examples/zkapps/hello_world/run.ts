a88bdacb165bff1cfb29b5d8f7378ed7b938fb0f
ymekuria <ymekuria@gmail.com>
2022-07-22 13:24:22 -0700

    test: attempt state update with failing precondition

--- src/examples/zkapps/hello_world/run.ts
+++ src/examples/zkapps/hello_world/run.ts
@@ -29,8 +29,8 @@ try {
 const initialState = zkAppInstance.x.get().toString();
 let currentState;
 
+// Update state with value that satisfies preconditions and correct admin private key
 console.log(`Updating state from ${initialState} to 9 with Admin Private Key`);
-
 txn = await Mina.transaction(deployerAccount, () => {
   zkAppInstance.update(Field(9), adminPrivateKey);
   zkAppInstance.sign(zkAppPrivateKey);
@@ -51,7 +51,7 @@ try {
 }
 
 const wrongAdminPrivateKey = PrivateKey.random();
-
+// Attempt to update state with value that satisfies preconditions and incorrect admin private key
 console.log(
   `Attempting to update state from ${currentState} to 81 with incorrect Admin Private Key`
 );
@@ -71,5 +71,27 @@ try {
 } catch (err: any) {
   console.log(
     `State correctly was not updated with wrong Admin Private Key. Current state is still ${currentState}`
+  );
+}
+
+// Attempt to update state with value that fails precondition x.square().assertEquals(squared).
+try {
+  console.log(
+    `Attempting to update state from ${currentState} to the value that fails precondition of 30`
+  );
+  txn = await Mina.transaction(deployerAccount, () => {
+    zkAppInstance.update(Field(30), adminPrivateKey);
+    zkAppInstance.sign(zkAppPrivateKey);
+  });
+  txn.send().wait();
+  currentState = zkAppInstance.x.get().toString();
+  if (currentState !== '9') {
+    console.error(
+      `State was updated from 9 to ${currentState} which fails the precondition x.square().assertEquals(squared);`
+    );
+  }
+} catch (err: any) {
+  console.log(
+    `State correctly was not updated when fails the precondition x.square().assertEquals(squared). Current state is still ${currentState}`
   );
 }
