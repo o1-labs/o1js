f0fdc1628d83d7884f7661db9c52fedf6394940a
ymekuria <ymekuria@gmail.com>
2022-08-02 15:14:16 -0600

    feat: setup local blockchain

--- src/examples/zkapps/voting/run.ts
+++ src/examples/zkapps/voting/run.ts
@@ -1,2 +1,7 @@
 import { Members } from './members';
 import { Mina, PrivateKey, Party, Field } from 'snarkyjs';
+
+console.log('Running script...');
+
+let Local = Mina.LocalBlockchain();
+Mina.setActiveInstance(Local);
