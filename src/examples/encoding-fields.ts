354b89e3ae144e0050197e53258a654e58b354fe
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-04 15:05:34 +0200

    update encoding test

--- src/examples/encoding-fields.ts
+++ src/examples/encoding-fields.ts
@@ -1,27 +1,26 @@
-import { Field, Ledger } from 'snarkyjs';
-let { versionBytes, fieldToBinaryString, toBase58 } = Ledger.encoding;
+import { Field, Ledger, Encoding } from 'snarkyjs';
 
-let x = Field(25);
+let tokenId = Field.random();
 
-let encoded1 = Ledger.fieldToBase58(x);
+let encoded1 = Ledger.fieldToBase58(tokenId);
+let encoded2 = Encoding.TokenId.toBase58(tokenId);
 
-let binaryString = fieldToBinaryString(x);
-
-console.log([...binaryString].map((c) => c.charCodeAt(0)));
-
-let encoded2 = toBase58(binaryString, versionBytes.tokenIdKey);
-
+console.log('ENCODED:');
+console.log(encoded1 === encoded2);
 console.log(encoded1);
 console.log(encoded2);
+console.log('---------------------------');
+console.log('DECODED');
+let decoded = Encoding.TokenId.fromBase58(encoded2);
+console.log(decoded.toBigInt() === tokenId.toBigInt());
+console.log(decoded.toBigInt());
+console.log(tokenId.toBigInt());
+console.log('---------------------------');
 
-let receiptChainHash = Field(56);
+let receiptChainHash = Field.random();
+let encodedHash2 = Encoding.ReceiptChainHash.toBase58(receiptChainHash);
+let receiptChainHash2 = Encoding.ReceiptChainHash.fromBase58(encodedHash2);
+console.log(receiptChainHash.toString() === receiptChainHash2.toString());
 
-console.log(
-  (Ledger as any).encoding.receiptChainHashToBase58(receiptChainHash)
-);
-console.log(encode(receiptChainHash, versionBytes.receiptChainHash));
-
-function encode(x: Field, versionByte: number) {
-  console.log({ versionByte });
-  return toBase58(fieldToBinaryString(x), versionByte);
-}
+console.log('default token id', Ledger.fieldToBase58(Field.one));
+console.log('default token id', Encoding.TokenId.toBase58(Field.one));
