d2047780c40066869d49d403e4006961f11c2ce8
ymekuria <ymekuria@gmail.com>
2022-08-08 16:04:22 -0700

    feat: declare node and zeroes

--- src/lib/merkle_tree.ts
+++ src/lib/merkle_tree.ts
@@ -1,3 +1,15 @@
+import { Poseidon } from './hash';
+import { Field } from './core'
+
+export type Witness = {
+  isLeft: boolean;
+  sibling: Field;
+}
+
 export class MerkleTree {
+  private nodes: Record<number, Record<string, Field>> = {};
+  
+  private zeroes: Field[];
+
 
 }
