6be37d3d767aad6f482db14e95e8435cca81d304
ymekuria <ymekuria@gmail.com>
2022-08-09 12:17:12 -0700

    feat: voting contract boilerplate

---
+++ src/examples/zkapps/voting/voting.ts
@@ -0,0 +1,15 @@
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  DeployArgs,
+  Permissions,
+} from 'snarkyjs';
+
+export class Voting extends SmartContract {
+  // TODO: Add state variables
+
+  
+}
