dd036a2ad9f79ba9ef0172194370304b9d32251f
ymekuria <ymekuria@gmail.com>
2022-08-08 19:24:07 -0700

    feat: merkletree constructor

--- src/lib/merkle_tree.ts
+++ src/lib/merkle_tree.ts
@@ -8,8 +8,13 @@ export type Witness = {
 
 export class MerkleTree {
   private nodes: Record<number, Record<string, Field>> = {};
-  
   private zeroes: Field[];
 
+  constructor(public readonly height: number) {
+    this.zeroes = [Field(0)];
+    for (let i = 1; i < height; i++) {
+      this.zeroes.push(Poseidon.hash([this.zeroes[i - 1], this.zeroes[i - 1]]));
+    }
+  }
 
 }
