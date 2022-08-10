74a037d6f455b4dc70ca6ea5343636a30623dca6
ymekuria <ymekuria@gmail.com>
2022-08-10 09:12:21 -0700

    feat: add beforeall and afterall hooks

--- src/lib/merkle_tree.test.ts
+++ src/lib/merkle_tree.test.ts
@@ -1,1 +1,11 @@
+import { Field, Poseidon, isReady, shutdown } from '../../dist/server';
+import { MerkleTree } from './merkle_tree';
 
+describe('Merkle Tree', () => {
+  beforeAll(async () => {
+    await isReady;
+  });
+  afterAll(async () => {
+    setTimeout(shutdown, 0);
+  });
+});
