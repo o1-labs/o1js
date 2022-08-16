90e682a6e42d2321f4b8fd88dbe6e482a007ab19
Florian <florian@technotro.com>
2022-08-16 12:13:46 +0200

    add member factory with account id

--- src/examples/zkapps/voting/member.ts
+++ src/examples/zkapps/voting/member.ts
@@ -1,11 +1,23 @@
-import { Bool, CircuitValue, prop, PublicKey, UInt64 } from 'snarkyjs';
+import { Bool, CircuitValue, Field, prop, PublicKey, UInt64 } from 'snarkyjs';
 
 export class Member extends CircuitValue {
+  static count = 0;
+
   @prop publicKey: PublicKey;
   @prop balance: UInt64;
   @prop hashVoted: Bool;
+  @prop accountId: Field;
 
-  constructor() {
+  private constructor(publicKey: PublicKey, balance: UInt64, accountId: Field) {
     super();
+    this.publicKey = publicKey;
+    this.balance = balance;
+    this.hashVoted = Bool(false);
+    this.accountId = accountId;
+  }
+
+  static from(publicKey: PublicKey, balance: UInt64) {
+    this.count++;
+    return new Member(publicKey, balance, Field.fromNumber(this.count));
   }
 }
