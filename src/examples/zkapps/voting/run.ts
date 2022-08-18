ba53529efab907cb5ad71dac72a744ea75049359
Florian <florian@technotro.com>
2022-08-18 17:09:02 +0200

    add comment

--- src/examples/zkapps/voting/run.ts
+++ src/examples/zkapps/voting/run.ts
@@ -7,4 +7,7 @@ Mina.setActiveInstance(Local);
 console.log('Running Voting script...');
 
 // I really hope this factory pattern works with SnarkyJS' contracts
+// one voting instance always consists of three contracts: two membership contracts and one voting contract
+// this pattern will hopefully help us deploy multiple sets of voting apps
+// with different preconditions efficiently for integration tests
 let VotingAppSet = VotingApp();
