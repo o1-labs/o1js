e611fa1c92ed60b66d2608a795647441582df53a
ymekuria <ymekuria@gmail.com>
2022-08-25 18:28:01 -0700

    feat: add new registerd voter to votersstore

--- src/examples/zkapps/voting/test.ts
+++ src/examples/zkapps/voting/test.ts
@@ -1,6 +1,6 @@
 import { Experimental, Mina, Party, Field, PrivateKey, UInt64 } from 'snarkyjs';
 import { VotingAppParams } from './factory';
-import { Member } from './member';
+import { Member, MerkleWitness } from './member';
 import { Membership_ } from './membership';
 import { OffchainStorage } from './off_chain_storage';
 import { Voting_ } from './voting';
@@ -35,9 +35,8 @@ export async function testSet(
 
   let tx;
 
-  let voterContract = contracts.voterContract;
-  let candidateContract = contracts.candidateContract;
-  let voting = contracts.voting;
+  let { votersStore, candidatesStore, votesStore } = storage;
+  let { voterContract, candidateContract, voting } = contracts;
 
   console.log('deploying set of 3 contracts');
   tx = await Mina.transaction(feePayer, () => {
@@ -50,9 +49,9 @@ export async function testSet(
     voterContract.deploy({ zkappKey: params.voterKey });
 
     // setting the merkle root
-    voterContract.committedMembers.set(storage.votersStore.getRoot());
-    candidateContract.committedMembers.set(storage.candidatesStore.getRoot());
-    voting.committedVotes.set(storage.votesStore.getRoot());
+    voterContract.committedMembers.set(votersStore.getRoot());
+    candidateContract.committedMembers.set(candidatesStore.getRoot());
+    voting.committedVotes.set(votesStore.getRoot());
 
     // setting the initial sequence events hash
     voterContract.accumulatedMembers.set(
@@ -66,262 +65,266 @@ export async function testSet(
   tx.send();
 
   console.log('all contracts deployed!');
-  
 
-  console.log('attempting to register a voter...')
+  console.log('attempting to register a voter...');
   let newVoter: Member;
   try {
     tx = await Mina.transaction(feePayer, () => {
-    newVoter = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
+      const newVoter1 = registerMember(
+        0n,
+        Member.from(
+          PrivateKey.random().toPublicKey(),
+          Field.zero,
+          UInt64.from(50)
+        ),
+        votersStore
+      );
 
-    // register new member
-    contracts.voting.voterRegistration(newVoter);
-    
-  });
-  if (params.doProofs) await tx.prove();
-  tx.send();
-} catch (err: any) {
-  throw Error(err)
-}
+      // register new member
+      contracts.voting.voterRegistration(newVoter1);
+    });
+    if (params.doProofs) await tx.prove();
+    tx.send();
+  } catch (err: any) {
+    throw Error(err);
+  }
 
-console.log('attempting to register the same voter twice...')
+  console.log('attempting to register the same voter twice...');
 
   try {
     tx = await Mina.transaction(feePayer, () => {
-    // register new member
-    contracts.voting.voterRegistration(newVoter);
-    contracts.voting.vote(candidate)
-    
-  });
-  if (params.doProofs) await tx.prove();
-  tx.send();
-} catch (err: any) {
-  // Todo: handle expected error and throw otherwise
-}
+      // register new member
+      contracts.voting.voterRegistration(newVoter);
+      contracts.voting.vote(candidate);
+    });
+    if (params.doProofs) await tx.prove();
+    tx.send();
+  } catch (err: any) {
+    // Todo: handle expected error and throw otherwise
+  }
 
-
- console.log('attempting to register a candidate...')
-let newCandidate: Member;
+  console.log('attempting to register a candidate...');
+  let newCandidate: Member;
   try {
     tx = await Mina.transaction(feePayer, () => {
-    newCandidate = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
+      newCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
 
-    // register new candidate
-    contracts.voting.candidateRegistration(newCandidate);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  throw Error(err)
-}
+      // register new candidate
+      contracts.voting.candidateRegistration(newCandidate);
+      contracts.voting.sign(votingKey);
+    });
 
- console.log('attempting to register a candidate before the time window ...')
- // 
- try {
-    tx = await Mina.transaction(feePayer, () => {
-   let earlyCandidate = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
+    tx.send();
+  } catch (err: any) {
+    throw Error(err);
+  }
 
-    // register late candidate
-    contracts.voting.candidateRegistration(earlyCandidate);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  // TODO: handle error when
-}
-
- // 
- try {
-    tx = await Mina.transaction(feePayer, () => {
-   let lateCandidate = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
-
-    // register late candidate
-    contracts.voting.candidateRegistration(lateCandidate);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  // TODO: handle error when
-}
-
- console.log('attempting to register a voter before the time window ...')
- // 
- try {
-    tx = await Mina.transaction(feePayer, () => {
-   let earlyVoter = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
-
-    // register early candidate
-    contracts.voting.voterRegistration(earlyVoter);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  // TODO: handle error when
-}
-
- console.log('attempting to register a voter after the time window ...')
- // 
- try {
-    tx = await Mina.transaction(feePayer, () => {
-   let lateVoter = Member.from(
-      PrivateKey.random().toPublicKey(),
-      Field.zero,
-      UInt64.from(50)
-    );
-
-    // register late candidate
-    contracts.voting.voterRegistration(lateVoter);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  // TODO: handle error when
-}
-
-// isMember test cases
-
-
-console.log('authrozing registrations...')
+  console.log('attempting to register a candidate before the time window ...');
+  //
   try {
     tx = await Mina.transaction(feePayer, () => {
-    // register new candidate
-    contracts.voting.authorizeRegistrations();
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  throw Error(err)
-}
+      let earlyCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
 
-console.log('attempting to vote for the new candidate...')
+      // register late candidate
+      contracts.voting.candidateRegistration(earlyCandidate);
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle error when
+  }
+
+  //
+  try {
+    tx = await Mina.transaction(feePayer, () => {
+      let lateCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+
+      // register late candidate
+      contracts.voting.candidateRegistration(lateCandidate);
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle error when
+  }
+
+  console.log('attempting to register a voter before the time window ...');
+  //
+  try {
+    tx = await Mina.transaction(feePayer, () => {
+      let earlyVoter = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+
+      // register early candidate
+      contracts.voting.voterRegistration(earlyVoter);
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle error when
+  }
+
+  console.log('attempting to register a voter after the time window ...');
+  //
+  try {
+    tx = await Mina.transaction(feePayer, () => {
+      let lateVoter = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+
+      // register late candidate
+      contracts.voting.voterRegistration(lateVoter);
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle error when
+  }
+
+  // isMember test cases
+
+  console.log('authrozing registrations...');
+  try {
+    tx = await Mina.transaction(feePayer, () => {
+      // register new candidate
+      contracts.voting.authorizeRegistrations();
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    throw Error(err);
+  }
+
+  console.log('attempting to vote for the new candidate...');
   let candidateChoice;
   try {
     tx = await Mina.transaction(feePayer, () => {
-  
-    contracts.voting.vote(candidateChoice);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  throw Error(err)
-}
-console.log('attempting to vote twice...')
+      contracts.voting.vote(candidateChoice);
+      contracts.voting.sign(votingKey);
+    });
+
+    tx.send();
+  } catch (err: any) {
+    throw Error(err);
+  }
+  console.log('attempting to vote twice...');
   try {
     tx = await Mina.transaction(feePayer, () => {
-  
-    contracts.voting.vote(candidateChoice);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-  // should throw error if double voting
-}
+      contracts.voting.vote(candidateChoice);
+      contracts.voting.sign(votingKey);
+    });
 
-console.log('attempting to vote for a fake candidate...')
+    tx.send();
+  } catch (err: any) {
+    // should throw error if double voting
+  }
+
+  console.log('attempting to vote for a fake candidate...');
   try {
     tx = await Mina.transaction(feePayer, () => {
+      let fakeCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+      contracts.voting.vote(fakeCandidate);
+      contracts.voting.sign(votingKey);
+    });
 
-  let fakeCandidate = Member.from(
-    PrivateKey.random().toPublicKey(),
-    Field.zero,
-    UInt64.from(50)
-  );
-    contracts.voting.vote(fakeCandidate);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-   // TODO: handle errors
-}
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle errors
+  }
 
-
-console.log('unregistered voter attempting to vote')
+  console.log('unregistered voter attempting to vote');
   try {
     tx = await Mina.transaction(feePayer, () => {
+      let fakeCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+      contracts.voting.vote(fakeCandidate);
+      contracts.voting.sign(votingKey);
+    });
 
-  let fakeCandidate = Member.from(
-    PrivateKey.random().toPublicKey(),
-    Field.zero,
-    UInt64.from(50)
-  );
-    contracts.voting.vote(fakeCandidate);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-   // TODO: handle errors
-}
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle errors
+  }
 
-console.log('candidate attempting to vote for voter...')
+  console.log('candidate attempting to vote for voter...');
   try {
     tx = await Mina.transaction(feePayer, () => {
+      let voter = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+      contracts.voting.vote(voter);
+      contracts.voting.sign(votingKey);
+    });
 
-  let voter = Member.from(
-    PrivateKey.random().toPublicKey(),
-    Field.zero,
-    UInt64.from(50)
-  );
-    contracts.voting.vote(voter);
-    contracts.voting.sign(votingKey);
-  });
-  
-  tx.send();
-} catch (err: any) {
-   // TODO: handle errors
-}
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle errors
+  }
 
-console.log('counting votes ...')
-let voteCount;
+  console.log('counting votes ...');
+  let voteCount;
   try {
     tx = await Mina.transaction(feePayer, () => {
+      let fakeCandidate = Member.from(
+        PrivateKey.random().toPublicKey(),
+        Field.zero,
+        UInt64.from(50)
+      );
+      voteCount = contracts.voting.countVotes();
+    });
 
-  let fakeCandidate = Member.from(
-    PrivateKey.random().toPublicKey(),
-    Field.zero,
-    UInt64.from(50)
-  );
-  voteCount  =  contracts.voting.countVotes();
-    
-  });
-  
-  tx.send();
-} catch (err: any) {
-   // TODO: handle errors
-   throw Error(error);
-}
+    tx.send();
+  } catch (err: any) {
+    // TODO: handle errors
+    throw Error(error);
+  }
 
-if(voteCount === '2') {
-  throw Error(`Vote count of ${voteCount} is incorrect`)
-}
+  if (voteCount === '2') {
+    throw Error(`Vote count of ${voteCount} is incorrect`);
+  }
   console.log('test successful!');
+}
+
+function registerMember(
+  i: bigint,
+  m: Member,
+  store: OffchainStorage<Member>
+): Member {
+  // we will also have to keep track of new voters and candidates within our off-chain merkle tree
+  store.set(i, m); // setting voter 0n
+  // setting the merkle witness
+  m.witness = new MerkleWitness(store.getWitness(i));
+
+  return m;
 }
