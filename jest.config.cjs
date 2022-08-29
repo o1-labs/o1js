598fab36c3c831c6ae88b388f27be1a4e690f808
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-29 16:37:16 +0200

    make jest use esm

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
+};
