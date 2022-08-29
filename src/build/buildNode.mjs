23bd31748658942eeeb0c101e44936f336116d04
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-29 16:57:45 +0200

    fixup cjs build

--- src/build/buildNode.mjs
+++ src/build/buildNode.mjs
@@ -1,5 +1,4 @@
 import fs from 'node:fs/promises';
-import fse from 'fs-extra';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { exec } from 'node:child_process';
@@ -18,72 +17,34 @@ let modulePath = path.resolve(fileURLToPath(import.meta.url));
 let isMain = nodePath === modulePath;
 
 if (isMain) {
-  console.log('building', entry);
+  console.log('building cjs version of', entry);
   console.log('using bindings from', bindings);
   await buildNode({ production: process.env.NODE_ENV === 'production' });
   console.log('finished build');
 }
 
 async function buildNode({ production }) {
-  let minify = false;
-
-  // copy over files not processed by TS
-  let copyPromise = copy({
-    [bindings]: './dist/server/node_bindings/',
-    './src/snarky.d.ts': './dist/server/snarky.d.ts',
-    ...Object.fromEntries(
-      (await fs.readdir('./src/snarky/'))
-        .filter(
-          (f) => f.endsWith('.js') || f.endsWith('.json') || f.endsWith('.d.ts')
-        )
-        .map((f) => [`./src/snarky/${f}`, `./dist/server/snarky/${f}`])
-    ),
-  });
-
-  if (minify) {
-    let snarkyJsPath = './dist/server/node_bindings/snarky_js_node.bc.js';
-    let snarkyJs = await fs.readFile(snarkyJsPath, 'utf8');
-    let { code } = await esbuild.transform(snarkyJs, {
-      target,
-      logLevel: 'error',
-      minify,
-    });
-    await fs.writeFile(snarkyJsPath, code);
-  }
-
   // invoke TS to recreate .ts files as .js + d.ts in /dist/server
-  let tscPromise = execPromise('npx tsc -p tsconfig.server.json');
-  await Promise.all([copyPromise, tscPromise]);
+  await execPromise('npx tsc -p tsconfig.server.json');
 
   // bundle the new index.js file with esbuild and create a new index.js file which conforms to CJS
   let jsEntry = path.resolve(
     'dist/server',
     path.basename(entry).replace('.ts', '.js')
   );
+  let outfile = jsEntry.replace('.js', '.cjs');
   await esbuild.build({
     entryPoints: [jsEntry],
     bundle: true,
     format: 'cjs',
     platform: 'node',
-    outfile: jsEntry,
+    outfile,
     target,
-    external: ['*.bc.js'],
     resolveExtensions: ['.node.js', '.ts', '.js'],
     allowOverwrite: true,
-    plugins: [makeNodeModulesExternal()],
-    minify,
+    plugins: [makeNodeModulesExternal(), makeJsooExternal()],
+    minify: false,
   });
-
-  // import the new index.js to get a list of its exports
-  // the list of exports is used to create an ESM entry-point index.mjs
-  let index = await import('../../dist/server/index.js');
-  let exportString = Object.keys(index)
-    .filter((x) => x !== 'default')
-    .join(', ');
-  let indexMjs = await fs.readFile('./src/index.mjs.template', 'utf8');
-  indexMjs = indexMjs.replace(/__EXPORTS__/g, exportString);
-  await fs.writeFile('./dist/server/index.mjs', indexMjs);
-  index.shutdown();
 }
 
 function makeNodeModulesExternal() {
@@ -99,19 +60,17 @@ function makeNodeModulesExternal() {
   };
 }
 
-function copy(copyMap) {
-  let promises = [];
-  for (let [source, target] of Object.entries(copyMap)) {
-    if (path.resolve(source) === path.resolve(target)) continue;
-    promises.push(
-      fse.copy(source, target, {
-        recursive: true,
-        overwrite: true,
-        dereference: true,
-      })
-    );
-  }
-  return Promise.all(promises);
+function makeJsooExternal() {
+  let isJsoo = /bc.cjs$/;
+  return {
+    name: 'plugin-external',
+    setup(build) {
+      build.onResolve({ filter: isJsoo }, ({ path }) => ({
+        path: path.replace('../', './'),
+        external: true,
+      }));
+    },
+  };
 }
 
 function execPromise(cmd) {
