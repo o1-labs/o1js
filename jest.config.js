e24aad572cda2ef1ac432138a931a7fa9467ff2f
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-29 19:01:44 +0200

    try more jest things

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ["node_modules/", "dist/node/"],
   globals: {
     'ts-jest': {
       useESM: true
