2b416b782fc3ab7e3abf6e9f97c599cb5b84c356
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-31 08:31:47 +0200

    add more examples to integration tests

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,3 +1,6 @@
 ./run src/examples/zkapps/hello_world/run.ts
 ./run src/examples/zkapps/voting/run.ts
+./run src/examples/simple_zkapp.ts
 ./run src/examples/zkapps/reducer/reducer_composite.ts
+./run src/examples/zkapps/composability.ts
+./run src/examples/zkapps/token_with_proofs.ts
