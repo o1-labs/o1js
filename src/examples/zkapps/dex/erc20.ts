07a47cb10c99764ca60a0830739da06ffd726d1e
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-31 14:57:19 +0200

    wip erc20 impl

--- src/examples/zkapps/dex/erc20.ts
+++ src/examples/zkapps/dex/erc20.ts
@@ -1,10 +1,19 @@
 import {
+  AsFieldElements,
   Bool,
   CircuitString,
+  circuitValue,
+  DeployArgs,
   Field,
+  method,
+  Party,
   PublicKey,
   SmartContract,
+  State,
+  state,
   UInt64,
+  Account,
+  Experimental,
 } from 'snarkyjs';
 
 /**
@@ -18,7 +27,7 @@ type Erc20 = {
   decimals?: () => Field; // TODO: should be UInt8 which doesn't exist yet
   totalSupply(): UInt64;
   balanceOf(owner: PublicKey): UInt64;
-  allowance(owner: PublicKey, value: UInt64): UInt64; // emits "Approve" event
+  allowance(owner: PublicKey, spender: PublicKey): UInt64;
 
   // mutations which need @method
   transfer(to: PublicKey, value: UInt64): Bool; // emits "Transfer" event; needs msg.sender
@@ -27,8 +36,16 @@ type Erc20 = {
 
   // events
   events: {
-    Transfer: { from: PublicKey; to: PublicKey; value: UInt64 };
-    Approval: { owner: PublicKey; spender: PublicKey; value: UInt64 };
+    Transfer: AsFieldElements<{
+      from: PublicKey;
+      to: PublicKey;
+      value: UInt64;
+    }>;
+    Approval: AsFieldElements<{
+      owner: PublicKey;
+      spender: PublicKey;
+      value: UInt64;
+    }>;
   };
 };
 
@@ -38,3 +55,60 @@ type Erc20 = {
 //    because the private key sits in a wallet and shouldn't be accessible to the website
 //    which creates the transaction.
 // => can keep mode where sender is unknown, but then methods relying on msg.sender have to FAIL
+
+class SomeCoin extends SmartContract implements Erc20 {
+  @state(UInt64) supply = State<UInt64>();
+
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+    this.tokenSymbol.set('SOM');
+  }
+
+  name(): CircuitString {
+    return CircuitString.fromString('SomeCoin');
+  }
+  symbol(): CircuitString {
+    return CircuitString.fromString('SOM');
+  }
+  decimals(): Field {
+    return Field(9);
+  }
+  totalSupply(): UInt64 {
+    return this.supply.get();
+  }
+  balanceOf(owner: PublicKey): UInt64 {
+    let account = Account(owner, this.experimental.token.id);
+    let balance = account.balance.get();
+    account.balance.assertEquals(balance);
+    return balance;
+  }
+  allowance(owner: PublicKey, spender: PublicKey): UInt64 {
+    // TODO: implement allowances
+    return UInt64.zero;
+  }
+
+  @method transfer(to: PublicKey, value: UInt64): Bool {
+    throw Error('TODO: reliably get msg.sender');
+  }
+  @method transferFrom(from: PublicKey, to: PublicKey, value: UInt64): Bool {
+    this.experimental.token.send({ from, to, amount: value });
+    return Bool(true);
+  }
+  @method approve(spender: PublicKey, value: UInt64): Bool {
+    // TODO: implement allowances
+    return Bool(false);
+  }
+
+  events = {
+    Transfer: circuitValue<{ from: PublicKey; to: PublicKey; value: UInt64 }>({
+      from: PublicKey,
+      to: PublicKey,
+      value: UInt64,
+    }),
+    Approval: circuitValue<{
+      owner: PublicKey;
+      spender: PublicKey;
+      value: UInt64;
+    }>({ owner: PublicKey, spender: PublicKey, value: UInt64 }),
+  };
+}
