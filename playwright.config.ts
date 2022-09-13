9258d94a8e4f8dd7941d798effe37c9a035ad0d9
Serhii Shymkiv <sergey@shimkiv.com>
2022-09-13 19:53:40 +0300

    Tests execution on CI fix.

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,6 +59,11 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run serve:web-e2e',
+    url: 'http://localhost:8000',
+  },
 };
 
 export default config;
