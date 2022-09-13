0613d1968baca8e8e7507658ff965d6b86fdf024
Serhii Shymkiv <sergey@shimkiv.com>
2022-09-13 22:14:37 +0300

    Change default test artifacts output folder.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { devices } from '@playwright/test';
  */
 const config: PlaywrightTestConfig = {
   testDir: './tests',
+  outputDir: './tests/report/test-artifacts',
   /* Maximum time one test can run for. */
   timeout: 25 * 60 * 1000,
   expect: {
