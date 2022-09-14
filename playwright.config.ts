c3a4fd6b6f1239c13f8055d9f56b403a548d2cb7
Serhii Shymkiv <sergey@shimkiv.com>
2022-09-14 19:12:17 +0300

    More tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,8 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests (on CI and locally for now). */
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
--- tests/on-chain-state-mgmt-zkapp-ui.spec.ts
+++ tests/on-chain-state-mgmt-zkapp-ui.spec.ts
@@ -1,13 +1,99 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import { OnChainStateMgmtZkAppPage } from './page/on-chain-state-mgmt-zkapp-ui.js';
 
 test.describe('On-Chain State Management zkApp UI', () => {
   test('should load page and initialize SnarkyJS', async ({ page }) => {
     const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
-    onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+  });
 
-    await expect(onChainStateMgmtZkApp.eventsContainer).toContainText(
-      'SnarkyJS initialized after'
+  test('should fail to update account state since zkApp was not yet deployed', async ({
+    page,
+  }) => {
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.updateZkAppState('3');
+    await onChainStateMgmtZkApp.checkZkAppStateUpdateFailureByUnknownAccount();
+  });
+
+  test('should compile and deploy zkApp', async ({ page }) => {
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+  });
+
+  test('should prove transaction and update zkApp account state', async ({
+    page,
+  }) => {
+    const currentAccountState = '2';
+    const newAccountState = '4';
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+    await onChainStateMgmtZkApp.updateZkAppState(newAccountState);
+    await onChainStateMgmtZkApp.checkUpdatedZkAppState(
+      currentAccountState,
+      newAccountState
+    );
+  });
+
+  test('should re-deploy zkApp', async ({ page }) => {
+    const currentAccountState = '2';
+    const newAccountState = '4';
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+    await onChainStateMgmtZkApp.updateZkAppState(newAccountState);
+    await onChainStateMgmtZkApp.checkUpdatedZkAppState(
+      currentAccountState,
+      newAccountState
+    );
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+  });
+
+  test('should fail to re-deploy zkApp by fee excess', async ({ page }) => {
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+    await onChainStateMgmtZkApp.clearEvents();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkZkAppDeploymentFailureByFeeExcess();
+  });
+
+  test('should fail to update account state by zkApp constraint', async ({
+    page,
+  }) => {
+    let currentAccountState = '2';
+    let newAccountState = '4';
+    const nextAccountState = '16';
+    const onChainStateMgmtZkApp = new OnChainStateMgmtZkAppPage(page);
+    await onChainStateMgmtZkApp.goto();
+    await onChainStateMgmtZkApp.checkSnarkyJsInitialization();
+    await onChainStateMgmtZkApp.compileAndDeployZkApp();
+    await onChainStateMgmtZkApp.checkDeployedZkApp();
+    await onChainStateMgmtZkApp.updateZkAppState(newAccountState);
+    await onChainStateMgmtZkApp.checkUpdatedZkAppState(
+      currentAccountState,
+      newAccountState
+    );
+    currentAccountState = newAccountState;
+    newAccountState = '1';
+    await onChainStateMgmtZkApp.updateZkAppState(newAccountState);
+    await onChainStateMgmtZkApp.checkZkAppStateUpdateFailureByStateConstraint(
+      currentAccountState,
+      nextAccountState,
+      newAccountState
     );
   });
 });
