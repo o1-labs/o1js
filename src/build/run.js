1af7709c30dedda93ca28147db4ff70756d57eab
Gregor <gregor.mitscha-baude@gmx.at>
2022-09-28 12:21:33 +0200

    restore old ./run behaviour if the new one fails

--- src/build/run.js
+++ src/build/run.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import { buildOne } from './buildExample.js';
+import { buildAndImport, buildOne } from './buildExample.js';
 
 let {
   _: [filePath],
   main,
   default: runDefault,
+  keep,
 } = minimist(process.argv.slice(2));
 
 if (!filePath) {
@@ -14,10 +15,19 @@ npx snarky-run [file]`);
   process.exit(0);
 }
 
-let absPath = await buildOne(filePath);
-console.log(`running ${absPath}`);
-let module = await import(absPath);
-if (main) await module.main();
-if (runDefault) await module.default();
-let { shutdown } = await import('../../dist/node/index.js');
-shutdown();
+try {
+  let absPath = await buildOne(filePath);
+  console.log(`running ${absPath}`);
+  let module = await import(absPath);
+  if (main) await module.main();
+  if (runDefault) await module.default();
+  let { shutdown } = await import('../../dist/node/index.js');
+  shutdown();
+} catch {
+  let { isReady, shutdown } = await import('../../dist/node/index.js');
+  await isReady;
+  let module = await buildAndImport(filePath, { keepFile: !!keep });
+  if (main) await module.main();
+  if (runDefault) await module.default();
+  shutdown();
+}
