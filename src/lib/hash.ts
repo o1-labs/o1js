fa87260d866bdac19d1a300bb61009cf50d9e568
Gareth Davies <hello@garethtdavies.com>
2022-09-30 18:44:00 +0000

    Add a check for token symbol length

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -170,6 +170,7 @@ const TokenSymbol = {
   },
 
   from(symbol: string): TokenSymbol {
+    if (symbol.length > 6) throw Error('Token symbol length should be less than 6');
     let field = prefixToField(symbol);
     return { symbol, field };
   },
