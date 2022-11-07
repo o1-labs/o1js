24bbfa107228bd7aef0b41538287f5a9e0eb07fc
Gregor <gregor.mitscha-baude@gmx.at>
2022-11-07 08:11:28 +0100

    abstract binable / base58 like ocaml

---
+++ src/provable/binable.ts
@@ -0,0 +1,95 @@
+// generic encoding infrastructure
+import { Ledger } from '../snarky.js';
+
+export { Binable, Base58, withVersionNumber, base58, fieldEncodings };
+
+type Binable<T> = {
+  toBytes(t: T): number[];
+  fromBytes(bytes: number[]): T;
+  sizeInBytes(): number;
+};
+
+function withVersionNumber<T>(
+  binable: Binable<T>,
+  versionNumber?: number
+): Binable<T> {
+  return {
+    toBytes(t) {
+      let bytes = binable.toBytes(t);
+      if (versionNumber !== undefined) bytes.unshift(versionNumber);
+      return bytes;
+    },
+    fromBytes(bytes) {
+      if (versionNumber !== undefined) bytes.shift();
+      return binable.fromBytes(bytes);
+    },
+    sizeInBytes() {
+      let size = binable.sizeInBytes();
+      return versionNumber !== undefined ? size + 1 : size;
+    },
+  };
+}
+
+type Base58<T> = {
+  toBase58(t: T): string;
+  fromBase58(base58: string): T;
+};
+
+function base58<T>(binable: Binable<T>, versionByte: () => number): Base58<T> {
+  return {
+    toBase58(t) {
+      let bytes = binable.toBytes(t);
+      let binaryString = String.fromCharCode(...bytes);
+      let ocamlBytes = { t: 9, c: binaryString, l: bytes.length };
+      return Ledger.encoding.toBase58(ocamlBytes, versionByte());
+    },
+    fromBase58(base58) {
+      let ocamlBytes = Ledger.encoding.ofBase58(base58, versionByte());
+      let bytes = [...ocamlBytes.c].map((_, i) => ocamlBytes.c.charCodeAt(i));
+      return binable.fromBytes(bytes);
+    },
+  };
+}
+
+// encoding of fields as base58, compatible with ocaml encodings (provided the versionByte and versionNumber are the same)
+
+function customEncoding<Field>(
+  Field: Binable<Field>,
+  versionByte: () => number,
+  versionNumber?: number
+) {
+  return base58(withVersionNumber(Field, versionNumber), versionByte);
+}
+
+const RECEIPT_CHAIN_HASH_VERSION = 1;
+const LEDGER_HASH_VERSION = 1;
+const EPOCH_SEED_VERSION = 1;
+const STATE_HASH_VERSION = 1;
+
+function fieldEncodings<Field>(Field: Binable<Field>) {
+  const TokenId = customEncoding(
+    Field,
+    () => Ledger.encoding.versionBytes.tokenIdKey
+  );
+  const ReceiptChainHash = customEncoding(
+    Field,
+    () => Ledger.encoding.versionBytes.receiptChainHash,
+    RECEIPT_CHAIN_HASH_VERSION
+  );
+  const LedgerHash = customEncoding(
+    Field,
+    () => Ledger.encoding.versionBytes.ledgerHash,
+    LEDGER_HASH_VERSION
+  );
+  const EpochSeed = customEncoding(
+    Field,
+    () => Ledger.encoding.versionBytes.epochSeed,
+    EPOCH_SEED_VERSION
+  );
+  const StateHash = customEncoding(
+    Field,
+    () => Ledger.encoding.versionBytes.stateHash,
+    STATE_HASH_VERSION
+  );
+  return { TokenId, ReceiptChainHash, LedgerHash, EpochSeed, StateHash };
+}
