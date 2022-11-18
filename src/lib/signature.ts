be06330d21adf5441f6111a2b6da14199af1cd61
Florian <florian@technotro.com>
2022-11-18 18:33:36 +0100

    Update signature.ts

--- src/lib/signature.ts
+++ src/lib/signature.ts
@@ -6,27 +6,27 @@ import { Poseidon } from './hash.js';
 export { PrivateKey, PublicKey, Signature };
 
 /**
- * A signing key. You can generate one via [[random]].
+ * A signing key. You can generate one via {@link PrivateKey.random}.
  */
 class PrivateKey extends CircuitValue {
   @prop s: Scalar;
 
   /**
    * You can use this method to generate a private key. You can then obtain
-   * the associated public key via [[toPublicKey]]. And generate signatures
-   * via [[Signature.create]].
+   * the associated public key via {@link toPublicKey}. And generate signatures
+   * via {@link Signature.create}.
    *
-   * @returns a new [[PrivateKey]].
+   * @returns a new {@link PrivateKey}.
    */
   static random(): PrivateKey {
     return new PrivateKey(Scalar.random());
   }
 
   /**
-   * Deserializes a list of bits into a [[PrivateKey]].
+   * Deserializes a list of bits into a {@link PrivateKey}.
    *
-   * @param bs a list of [[Bool]]s.
-   * @returns a [[PrivateKey]].
+   * @param bs a list of {@link Bool}.
+   * @returns a {@link PrivateKey}.
    */
   static fromBits(bs: Bool[]): PrivateKey {
     return new PrivateKey(Scalar.fromBits(bs));
@@ -35,31 +35,54 @@ class PrivateKey extends CircuitValue {
   /**
    * Derives the associated public key.
    *
-   * @returns a [[PublicKey]].
+   * @returns a {@link PublicKey}.
    */
   toPublicKey(): PublicKey {
     return PublicKey.fromPrivateKey(this);
   }
 
+  /**
+   * Decodes a base58 string into a {@link PrivateKey}.
+   *
+   * @returns a {@link PrivateKey}.
+   */
   static fromBase58(privateKeyBase58: string) {
     let scalar = Ledger.privateKeyOfString(privateKeyBase58);
     return new PrivateKey(scalar);
   }
+
+  /**
+   * Encodes a {@link PrivateKey} into a base58 string.
+   * @returns a base58 encoded string
+   */
   toBase58() {
     return PrivateKey.toBase58(this);
   }
+
   // static version, to operate on non-class versions of this type
+  /**
+   * Static method to encode a {@link PrivateKey} into a base58 string.
+   * @returns a base58 encoded string
+   */
   static toBase58(privateKey: { s: Scalar }) {
     return Ledger.privateKeyToString(privateKey);
   }
 }
 
 // TODO: this doesn't have a non-default check method yet. does it need one?
+/**
+ * A public key, which is also an address on the Mina network.
+ * You can derive a {@link PublicKey} directly from a {@link PrivateKey}.
+ */
 class PublicKey extends CircuitValue {
   // compressed representation of a curve point, where `isOdd` is the least significant bit of `y`
   @prop x: Field;
   @prop isOdd: Bool;
 
+  /**
+   * Returns the {@link Group} representation of this {@link PublicKey}.
+   * @returns A {@link Group}
+   */
   toGroup(): Group {
     // compute y from elliptic curve equation y^2 = x^3 + 5
     // TODO: we have to improve constraint efficiency by using range checks
@@ -74,51 +97,98 @@ class PublicKey extends CircuitValue {
     return new Group(x, y);
   }
 
+  /**
+   * Creates a {@link PublicKey} from a {@link Group} element.
+   * @returns a {@link PublicKey}.
+   */
   static fromGroup({ x, y }: Group): PublicKey {
     let isOdd = y.toBits()[0];
     return PublicKey.fromObject({ x, isOdd });
   }
 
+  /**
+   * Derives a {@link PublicKey} from a {@link PrivateKey}.
+   * @returns a {@link PublicKey}.
+   */
   static fromPrivateKey({ s }: PrivateKey): PublicKey {
     return PublicKey.fromGroup(Group.generator.scale(s));
   }
 
+  /**
+   * Creates a {@link PublicKey} from a JSON structure element.
+   * @returns a {@link PublicKey}.
+   */
   static from(g: { x: Field; isOdd: Bool }) {
     return PublicKey.fromObject(g);
   }
 
+  /**
+   * Creates an empty {@link PublicKey}.
+   * @returns an empty {@link PublicKey}
+   */
   static empty() {
     return PublicKey.from({ x: Field(0), isOdd: Bool(false) });
   }
 
+  /**
+   * Checks if a {@link PublicKey} is empty.
+   * @returns a {@link Bool}
+   */
   isEmpty() {
     // there are no curve points with x === 0
     return this.x.isZero();
   }
 
+  /**
+   * Decodes a base58 encoded {@link PublicKey} into a {@link PublicKey}.
+   * @returns a {@link PublicKey}
+   */
   static fromBase58(publicKeyBase58: string) {
     let pk = Ledger.publicKeyOfString(publicKeyBase58);
     return PublicKey.from(pk);
   }
+  /**
+   * Encodes a {@link PublicKey} in base58 format.
+   * @returns a base58 encoded {@link PublicKey}
+   */
   toBase58() {
     return PublicKey.toBase58(this);
   }
   // static version, to operate on non-class versions of this type
+  /**
+   * Static method to encode a {@link PublicKey} into base58 format.
+   * @returns a base58 encoded {@link PublicKey}
+   */
   static toBase58(publicKey: PublicKey) {
     return Ledger.publicKeyToString(publicKey);
   }
+  /**
+   * Serializes a {@link PublicKey} into its JSON representation.
+   * @returns a JSON string
+   */
   static toJSON(publicKey: PublicKey) {
     return publicKey.toBase58();
   }
+  /**
+   * Deserializes a JSON string into a {@link PublicKey}.
+   * @returns a JSON string
+   */
   static fromJSON<T extends AnyConstructor>(this: T, publicKey: string) {
     return PublicKey.fromBase58(publicKey) as InstanceType<T>;
   }
 }
 
+/**
+ * A Schnorr {@link Signature}  over the Paste Curves.
+ */
 class Signature extends CircuitValue {
   @prop r: Field;
   @prop s: Scalar;
 
+  /**
+   * Signs a message using a {@link PrivateKey}.
+   * @returns a {@link Signature}
+   */
   static create(privKey: PrivateKey, msg: Field[]): Signature {
     const publicKey = PublicKey.fromPrivateKey(privKey).toGroup();
     const d = privKey.s;
@@ -132,6 +202,10 @@ class Signature extends CircuitValue {
     return new Signature(r, s);
   }
 
+  /**
+   * Verifies the {@link Signature} using a message and the corresponding {@link PublicKey}.
+   * @returns a {@link Bool}
+   */
   verify(publicKey: PublicKey, msg: Field[]): Bool {
     const point = publicKey.toGroup();
     let e = Scalar.fromBits(
