ef3edb07a438beab74d82c679a09812472505667
Gregor <gregor.mitscha-baude@gmx.at>
2022-11-23 09:59:43 +0100

    fix a logical error

--- src/lib/zkapp.ts
+++ src/lib/zkapp.ts
@@ -720,7 +720,7 @@ class SmartContract {
     // init if this account is not yet deployed or has no verification key on it
     let shouldInit =
       !Mina.hasAccount(this.address) ||
-      Mina.getAccount(this.address).verificationKey !== undefined;
+      Mina.getAccount(this.address).verificationKey === undefined;
     if (!shouldInit) return;
     if (zkappKey) this.init(zkappKey);
     else this.init();
