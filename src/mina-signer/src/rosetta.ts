2628bee3170da86cafe37be0fc9d36ac6f4eb8f1
Gregor <gregor.mitscha-baude@gmx.at>
2023-01-09 17:45:50 +0100

    start to implement rosetta tx translation

--- src/mina-signer/src/rosetta.ts
+++ src/mina-signer/src/rosetta.ts
@@ -1,6 +1,10 @@
 import { Binable } from '../../provable/binable.js';
 import { PublicKey, Scalar } from '../../provable/curve-bigint.js';
-import { Field } from '../../provable/field-bigint.js';
+import { Field, UInt32, UInt64 } from '../../provable/field-bigint.js';
+import { Memo } from './memo.js';
+import { CommonJson, PaymentJson } from './sign-legacy.js';
+import { Signature } from './signature.js';
+import { Signed } from './transaction-hash.js';
 
 export { publicKeyToHex };
 
@@ -19,3 +23,91 @@ function fieldToHex<T extends Field | Scalar>(
   // map each byte to a hex string of length 2
   return bytes.map((byte) => byte.toString(16)).join('');
 }
+
+function rosettaTransactionToSignedCommand({
+  signature,
+  payment,
+  stake_delegation,
+}: RosettaTransactionJson) {
+  if (payment !== null && stake_delegation === null) {
+    let r = payment;
+    let command = {
+      receiver: r.to,
+      source: r.from,
+      kind: 'Payment',
+      fee_payer: r.from,
+      fee_token: r.token,
+      fee: UInt64.fromJSON(r.fee),
+      amount: UInt64.fromJSON(r.amount),
+      valid_until:
+        r.valid_until !== null ? UInt32.fromJSON(r.valid_until) : null,
+      memo: r.memo,
+    };
+  } else if (payment === null && stake_delegation !== null) {
+    let r = stake_delegation;
+    let command = {
+      receiver: r.new_delegate,
+      source: r.delegator,
+      kind: 'Delegation',
+      fee_payer: r.delegator,
+      fee_token: '1',
+      fee: UInt64.fromJSON(r.fee),
+      amount: null,
+      valid_until:
+        r.valid_until !== null ? UInt32.fromJSON(r.valid_until) : null,
+      memo: r.memo,
+    };
+  } else {
+    throw Error('rosettaTransactionToSignedCommand: Unsupported transaction');
+  }
+}
+
+function paymentToOcaml({
+  data: {
+    common,
+    body: { source, receiver, amount },
+  },
+  signature,
+}: Signed<PaymentJson>) {
+  return {
+    payload: {
+      common: commonToOcaml(common),
+      body: ['Payment', { source_pk: source, receiver_pk: receiver, amount }],
+    },
+    signer: source,
+    signature: Signature.toBase58(Signature.fromJSON(signature)),
+  };
+}
+
+function commonToOcaml({ fee, feePayer, nonce, validUntil, memo }: CommonJson) {
+  memo = Memo.toBase58(Memo.fromString(memo));
+  return {
+    fee: fee.slice(0, -9),
+    fee_payer_pk: feePayer,
+    nonce,
+    valid_until: validUntil,
+    memo,
+  };
+}
+
+type RosettaTransactionJson = {
+  signature: string;
+  payment: {
+    to: string;
+    from: string;
+    fee: string;
+    token: string;
+    nonce: string;
+    memo: string | null;
+    amount: string;
+    valid_until: string | null;
+  } | null;
+  stake_delegation: {
+    delegator: string;
+    new_delegate: string;
+    fee: string;
+    nonce: string;
+    memo: string | null;
+    valid_until: string | null;
+  } | null;
+};
