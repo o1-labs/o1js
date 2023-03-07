0098714515068498570b0baa6bd1df1435ddf3a4
Gregor <gregor.mitscha-baude@gmx.at>
2023-03-07 13:56:20 +0100

    fix unit test script

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
+shopt -s globstar # to expand '**' into nested directories./
 
 # run the build:test
 npm run build:test
 
 # find all unit tests in dist/node and run them
+# TODO it would be nice to make this work on Mac
+# here is a first attempt which has the problem of not failing if one of the tests fails
+# find ./dist/node -name "*.unit-test.js" -print -exec node {} \;
 for f in ./dist/node/**/*.unit-test.js; do
   echo "Running $f"
   node --enable-source-maps --stack-trace-limit=1000 $f;
