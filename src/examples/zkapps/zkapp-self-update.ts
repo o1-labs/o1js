4e38eb2d875e0a5cc65ef12a903abf22ef284beb
Gregor <gregor.mitscha-baude@gmx.at>
2023-03-28 09:39:16 +0200

    unify verification key type across APIs

--- src/examples/zkapps/zkapp-self-update.ts
+++ src/examples/zkapps/zkapp-self-update.ts
@@ -10,12 +10,12 @@ import {
   PrivateKey,
   Mina,
   AccountUpdate,
-  Field,
   Circuit,
 } from 'snarkyjs';
 
 class Foo extends SmartContract {
   init() {
+    super.init();
     this.account.permissions.set({
       ...Permissions.default(),
       setVerificationKey: Permissions.proof(),
@@ -33,12 +33,12 @@ class Bar extends SmartContract {
   }
 }
 
+// setup
+
 await isReady;
 
 const Local = Mina.LocalBlockchain({ proofsEnabled: true });
 Mina.setActiveInstance(Local);
-
-await Foo.compile();
 
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppAddress = zkAppPrivateKey.toPublicKey();
@@ -46,6 +46,10 @@ const zkApp = new Foo(zkAppAddress);
 
 const { privateKey: deployerKey, publicKey: deployerAccount } =
   Local.testAccounts[0];
+
+// deploy first verification key
+
+await Foo.compile();
 
 const tx = await Mina.transaction(deployerAccount, () => {
   AccountUpdate.fundNewAccount(deployerAccount);
@@ -55,27 +59,20 @@ await tx.prove();
 await tx.sign([deployerKey, zkAppPrivateKey]).send();
 
 const fooVerificationKey = Mina.getAccount(zkAppAddress).zkapp?.verificationKey;
-console.log('original verification key', {
-  data: fooVerificationKey?.data,
-  hash: fooVerificationKey?.hash.toString(),
-});
+Circuit.log('original verification key', fooVerificationKey);
+
+// update verification key
 
 const { verificationKey: barVerificationKey } = await Bar.compile();
 
-const replacement = {
-  data: barVerificationKey.data,
-  hash: Field(barVerificationKey.hash),
-};
 const tx2 = await Mina.transaction(deployerAccount, () => {
-  zkApp.replaceVerificationKey(replacement);
+  zkApp.replaceVerificationKey(barVerificationKey);
 });
 await tx2.prove();
 await tx2.sign([deployerKey]).send();
 
 const updatedVerificationKey =
   Mina.getAccount(zkAppAddress).zkapp?.verificationKey;
+
 // should be different from Foo
-console.log('updated verification key', {
-  data: updatedVerificationKey?.data,
-  hash: updatedVerificationKey?.hash.toString(),
-});
+Circuit.log('updated verification key', updatedVerificationKey);
