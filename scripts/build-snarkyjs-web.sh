dce718680d0d626969914165c15aaebbf5feeaf5
Gregor <gregor.mitscha-baude@gmx.at>
2023-04-03 14:16:35 +0200

    chrome -> web

--- scripts/build-snarkyjs-web.sh
+++ scripts/build-snarkyjs-web.sh
@@ -7,18 +7,18 @@ DUNE_PATH="$SNARKY_JS_PATH/src/snarkyjs-bindings/ocaml"
 BUILD_PATH="_build/default/$DUNE_PATH"
 KIMCHI_BINDINGS="$SNARKY_JS_PATH/src/snarkyjs-bindings/kimchi"
 
-dune b $DUNE_PATH/snarky_js_chrome.bc.js
-cp _build/default/$KIMCHI_BINDINGS/js/chrome/plonk_wasm* "$SNARKY_JS_PATH"/src/chrome_bindings/
-cp $BUILD_PATH/snarky_js_chrome*.js "$SNARKY_JS_PATH"/src/chrome_bindings/
+dune b $DUNE_PATH/snarky_js_web.bc.js
+cp _build/default/$KIMCHI_BINDINGS/js/web/plonk_wasm* "$SNARKY_JS_PATH"/src/web_bindings/
+cp $BUILD_PATH/snarky_js_web*.js "$SNARKY_JS_PATH"/src/web_bindings/
 
 # better error messages
 # `s` is the jsoo representation of the error message string, and `s.c` is the actual JS string
-sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "some error")}/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
+sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "some error")}/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
 
-pushd "$SNARKY_JS_PATH"/src/chrome_bindings
+pushd "$SNARKY_JS_PATH"/src/web_bindings
   wasm-opt --detect-features --enable-mutable-globals -O4 plonk_wasm_bg.wasm -o plonk_wasm_bg.wasm.opt
   mv plonk_wasm_bg.wasm.opt plonk_wasm_bg.wasm
 popd
--- scripts/update-snarkyjs-bindings.sh
+++ scripts/update-snarkyjs-bindings.sh
@@ -25,30 +25,30 @@ npm run build --prefix="$SNARKY_JS_PATH"
 # 2. web build
 
 cp "$BUILD_PATH/snarky_js_node.bc.map" "_build/snarky_js_node.bc.map"
-dune b $DUNE_PATH/snarky_js_chrome.bc.js
+dune b $DUNE_PATH/snarky_js_web.bc.js
 cp "_build/snarky_js_node.bc.map" "$BUILD_PATH/snarky_js_node.bc.map" 
 
-cp _build/default/$KIMCHI_BINDINGS/js/chrome/plonk_wasm* "$SNARKY_JS_PATH"/src/chrome_bindings/
-cp $BUILD_PATH/snarky_js_chrome*.js "$SNARKY_JS_PATH"/src/chrome_bindings/
+cp _build/default/$KIMCHI_BINDINGS/js/web/plonk_wasm* "$SNARKY_JS_PATH"/src/web_bindings/
+cp $BUILD_PATH/snarky_js_web*.js "$SNARKY_JS_PATH"/src/web_bindings/
 
 # better error messages
 # `s` is the jsoo representation of the error message string, and `s.c` is the actual JS string
-sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
-sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "Unknown error thrown by raise")}/' "$SNARKY_JS_PATH"/src/chrome_bindings/snarky_js_chrome.bc.js
+sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
+sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "Unknown error thrown by raise")}/' "$SNARKY_JS_PATH"/src/web_bindings/snarky_js_web.bc.js
 
 # optimize wasm / minify JS (we don't do this with jsoo to not break the error message fix above)
-pushd "$SNARKY_JS_PATH"/src/chrome_bindings
+pushd "$SNARKY_JS_PATH"/src/web_bindings
   wasm-opt --detect-features --enable-mutable-globals -O4 plonk_wasm_bg.wasm -o plonk_wasm_bg.wasm.opt
   mv plonk_wasm_bg.wasm.opt plonk_wasm_bg.wasm
-  npx esbuild --minify --log-level=error snarky_js_chrome.bc.js > snarky_js_chrome.bc.min.js
-  mv snarky_js_chrome.bc.min.js snarky_js_chrome.bc.js
+  npx esbuild --minify --log-level=error snarky_js_web.bc.js > snarky_js_web.bc.min.js
+  mv snarky_js_web.bc.min.js snarky_js_web.bc.js
 popd
 
 npm run build:web --prefix="$SNARKY_JS_PATH"
 
 # 3. update MINA_COMMIT file in snarkyjs
 
-echo "The mina commit used to generate the backends for node and chrome is
+echo "The mina commit used to generate the backends for node and web is
 $(git rev-parse HEAD)" > "$SNARKY_JS_PATH/MINA_COMMIT"
