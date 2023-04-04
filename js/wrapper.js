8594f397a954eff26cdd4759b3795a54ccbb2824
Gregor <gregor.mitscha-baude@gmx.at>
2023-04-04 12:00:56 +0200

    move down snarky/

---
+++ js/wrapper.js
@@ -0,0 +1,10 @@
+import { wasm, withThreadPool } from './node/node-backend.js';
+import { default as snarky } from '../../_node_bindings/snarky_js_node.bc.cjs';
+
+export { getSnarky, getWasm, withThreadPool };
+
+let getSnarky = () => snarky;
+
+function getWasm() {
+  return wasm;
+}
