1375c6d0b0d37ad43e7ab6b84b42ce08048b8a43
Gregor <gregor.mitscha-baude@gmx.at>
2023-04-10 11:57:24 +0200

    don't use run and check for tx callback

--- src/examples/zkapps/reducer/reducer.ts
+++ src/examples/zkapps/reducer/reducer.ts
@@ -79,6 +79,11 @@ let zkapp = new CounterZkapp(zkappAddress);
 if (doProofs) {
   console.log('compile');
   await CounterZkapp.compile();
+} else {
+  // TODO: if we don't do this, then `analyzeMethods()` will be called during `runUnchecked()` in the tx callback below,
+  // which currently fails due to `finalize_is_running` in snarky not resetting internal state, and instead setting is_running unconditionally to false,
+  // so we can't nest different snarky circuit runners
+  CounterZkapp.analyzeMethods();
 }
 
 console.log('deploy');
