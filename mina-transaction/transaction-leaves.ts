76b9e2373529dd33b60ce714c1ec06cba4fdb38f
Gregor <gregor.mitscha-baude@gmx.at>
2023-04-11 10:57:43 +0200

    actions rename

--- mina-transaction/transaction-leaves.ts
+++ mina-transaction/transaction-leaves.ts
@@ -15,10 +15,10 @@ export { PublicKey, Field, Bool, AuthRequired, UInt64, UInt32, Sign, TokenId };
 
 export {
   Events,
-  SequenceEvents,
+  Actions,
   ZkappUri,
   TokenSymbol,
-  SequenceState,
+  ActionState,
   ReceiptChainHash,
   StateHash,
 };
@@ -41,13 +41,13 @@ type Events = {
   hash: Field;
   data: Event[];
 };
-type SequenceEvents = Events;
-const { Events, SequenceEvents } = createEvents({ Field, Poseidon });
+type Actions = Events;
+const { Events, Actions } = createEvents({ Field, Poseidon });
 
-type SequenceState = Field;
-const SequenceState = {
+type ActionState = Field;
+const ActionState = {
   ...provable(Field),
-  emptyValue: SequenceEvents.emptySequenceState,
+  emptyValue: Actions.emptyActionState,
 };
 
 type ReceiptChainHash = Field;
