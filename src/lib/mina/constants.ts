17678779778a48e1e2ddcbf3153865365bc8a375
Martin Minkov <minkovlmartin@gmail.com>
2023-05-01 12:49:02 -0700

    refactor(constants.ts): change constant values to use 'as const' to improve type safety and prevent accidental reassignment

--- src/lib/mina/constants.ts
+++ src/lib/mina/constants.ts
@@ -6,21 +6,21 @@
 
 // ---- Constants used to calculate cost of a transaction ----
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#LL67C21-L67C21
-export const proofCost = 10.26;
+export const proofCost = 10.26 as const;
 
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#LL69C8-L69C8
-export const signedPairCost = 10.08;
+export const signedPairCost = 10.08 as const;
 
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#L71
-export const signedSingleCost = 9.14;
+export const signedSingleCost = 9.14 as const;
 
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#L73
-export const costLimit = 69.45;
+export const costLimit = 69.45 as const;
 // ----
 
 // --- Constants to define the maximum number of events and actions in a transaction ----
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#L75
-export const maxActionElements = 100;
+export const maxActionElements = 100 as const;
 // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#L77
-export const maxEventElements = 100;
+export const maxEventElements = 100 as const;
 // ----
