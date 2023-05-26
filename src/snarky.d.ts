836060e27c0d042abe1159a3dba24ec633a1a8cb
Yunus Gürlek <yunusgurlek@Yunus-MacBook-Pro.local>
2023-05-26 14:55:38 +0200

    Update reference docs on Provable and ProvablePure interface.

--- src/snarky.d.ts
+++ src/snarky.d.ts
@@ -22,31 +22,124 @@ export {
 export { Test };
 
 /**
- * `Provable<T>` is the general circuit type interface. Provable interface describes how a type `T` is made up of field elements and auxiliary (non-field element) data.
+ * `Provable<T>` is the general circuit type interface in SnarkyJS. `Provable<T>` interface describes how a type `T` is made up of {@link Field} elements and "auxiliary" (non-provable) data.
  *
  * You will find `Provable<T>` as the required input type in a few places in SnarkyJS. One convenient way to create a `Provable<T>` is using `Struct`.
  * 
  * The properties and methods on the provable type exist in all base SnarkyJS types as well (aka. {@link Field}, {@link Bool}, etc.). In most cases, a zkApp developer does not need these functions to create Dapps.
  */
 declare interface Provable<T> {
-  toFields: (x: T) => Field[];
-  toAuxiliary: (x?: T) => any[];
-  fromFields: (x: Field[], aux: any[]) => T;
+  /**
+   * A function that takes `value`, an element of type `T`, as argument and returns an array of {@link Field} elements that made up the provable data of `value`.
+   * 
+   * @param value - the element of type `T` to generate the {@link Field} array from.
+   * 
+   * @return A {@link Field} array describing how this `T` element is made up of {@link Field} elements.
+   */
+  toFields: (value: T) => Field[];
+
+   /**
+   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that made up the "auxilary" (non-provable) data of `value`.
+   * 
+   * @param value - the element of type `T` to generate the auxilary data array from, optional. If not provided, an empty array is returned.
+   * 
+   * @return An array of any type describing how this `T` element is made up of "auxilary" (non-provable) data.
+   */
+  toAuxiliary: (value?: T) => any[];
+
+  /**
+   * A function that return an element of type `T` from the given provable and "auxilary" data.
+   * 
+   * **Important**: For any element of type `T`, this function is the reverse operation of calling {@link toFields} and {@link toAuxilary} methods on an element of type `T`.
+   * 
+   * @param fields - an array of {@link Field} elements describing the provable data of the new `T` element.
+   * @param aux - an array of any type describing the "auxilary" data of the new `T` element, optional.
+   * 
+   * @return An element of type `T` generated from the given provable and "auxilary" data.
+   */
+  fromFields: (fields: Field[], aux: any[]) => T;
+
+  /**
+   * Return the size of the `T` type in terms of {@link Field} type, as {@link Field} is the primitive type.
+   * 
+   * **Warning**: This function returns a `number`, so you cannot use it to prove something on chain. You can use it during debugging or to understand the memory complexity of some type.
+   * 
+   * **Beware**: Calling the statement `x.sizeInFields()` is equivalent to `x.toFields().length`.
+   * 
+   * @return A `number` representing the size of the `T` type in terms of {@link Field} type.
+   */
   sizeInFields(): number;
-  check: (x: T) => void;
+
+  /**
+   * Assert some statements on the chain to check if `value` element is a valid member of type `T`.
+   * This function does not return anything, rather creates any number of assertions on the chain to prove `value` is a valid member of the type `T`.
+   * 
+   * For instance, calling check function on the type {@link Bool} asserts that the value of the element is either 1 or 0.
+   * 
+   * @param value - the element of type `T` to put assertions on.
+   */
+  check: (value: T) => void;
 }
+
 /**
- * `ProvablePure<T>` is a special kind of `Provable<T>`, where the auxiliary data is empty. This means the type only consists of field elements,
- * in that sense it is "pure".
- *
+ * `ProvablePure<T>` is a special kind of {@link Provable} interface, where the "auxiliary" (non-provable) data is empty. This means the type only consists of field elements, in that sense it is "pure".
+ * Any element on the interface `ProvablePure<T>` is also an element of the interface `Provable<T>` where the "auxilary" data is empty.
+ * 
  * Examples where `ProvablePure<T>` is required are types of on-chain state, events and actions.
+ * 
+ * It includes the same properties and methods as the {@link Provable} interface.
  */
 declare interface ProvablePure<T> extends Provable<T> {
-  toFields: (x: T) => Field[];
-  toAuxiliary: (x?: T) => [];
-  fromFields: (x: Field[]) => T;
+  /**
+   * A function that takes `value`, an element of type `T`, as argument and returns an array of {@link Field} elements that made up the provable data of `value`.
+   * 
+   * @param value - the element of type `T` to generate the {@link Field} array from.
+   * 
+   * @return A {@link Field} array describing how this `T` element is made up of {@link Field} elements.
+   */
+  toFields: (value: T) => Field[];
+
+   /**
+   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that made up the "auxilary" (non-provable) data of `value`.
+   * As any element of the interface `ProvablePure<T>` includes no "auxilary" data by definition, this function always return an empty array.
+   * 
+   * @param value - the element of type `T` to generate the auxilary data array from, optional. If not provided, an empty array is returned.
+   * 
+   * @return An empty array, as any element of the interface `ProvablePure<T>` includes no "auxilary" data by definition.
+   */
+  toAuxiliary: (value?: T) => any[];
+
+  /**
+   * A function that return an element of type `T` from the given provable data.
+   * 
+   * **Important**: For any element of type `T`, this function is the reverse operation of calling {@link toFields} method on an element of type `T`.
+   * 
+   * @param fields - an array of {@link Field} elements describing the provable data of the new `T` element.
+   * 
+   * @return An element of type `T` generated from the given provable data.
+   */
+  fromFields: (fields: Field[]) => T;
+
+  /**
+   * Return the size of the `T` type in terms of {@link Field} type, as {@link Field} is the primitive type.
+   * 
+   * **Warning**: This function returns a `number`, so you cannot use it to prove something on chain. You can use it during debugging or to understand the memory complexity of some type.
+   * 
+   * **Beware**: Calling the statement `x.sizeInFields()` is equivalent to `x.toFields().length`.
+   * 
+   * @return A `number` representing the size of the `T` type in terms of {@link Field} type.
+   */
   sizeInFields(): number;
-  check: (x: T) => void;
+
+  /**
+   * Assert some statements on the chain to check if `value` element is a valid member of type `T`.
+   * This function does not return anything, rather creates any number of assertions on the chain to prove `value` is a valid member of the type `T`.
+   * 
+   * For instance, calling check function on the type {@link Bool} asserts that the value of the element is either 1 or 0.
+   * 
+   * @param value - the element of type `T` to put assertions on.
+   */
+  check: (value: T) => void;
 }
 
 /**
@@ -105,7 +198,7 @@ declare class Field {
    * new Field("abc"); // ERROR: Invalid argument "abc"
    * ```
    * 
-Creating a {@link Field} from a negative number can result in unexpected behavior if you are not familiar with [modular arithmetic](https://en.wikipedia.org/wiki/Modular_arithmetic).```
+   * Creating a {@link Field} from a negative number can result in unexpected behavior if you are not familiar with [modular arithmetic](https://en.wikipedia.org/wiki/Modular_arithmetic).```
    * @example
    * ```
    * const x = new Field(-1); // Valid Field construction from negative number
@@ -716,7 +809,7 @@ Creating a {@link Field} from a negative number can result in unexpected behavio
   /**
    * This function is the implementation of {@link Provable.sizeInFields} in {@link Field} type.
    * Size of the {@link Field} type is always 1, as it is the primitive type.
-   * This function returns a reular number, so you cannot use it to prove something on chain. You can use it during debugging or to understand the memory complexity of some type.
+   * This function returns a `number`, so you cannot use it to prove something on chain. You can use it during debugging or to understand the memory complexity of some type.
    * 
    * This function has the same utility as the {@link Field.sizeInFields}.
    * 
@@ -725,7 +818,7 @@ Creating a {@link Field} from a negative number can result in unexpected behavio
    * console.log(Field.sizeInFields()); // Prints 1
    * ```
    * 
-   * @return A number representing the size of the {@link Field} type in terms of {@link Field} type itself.
+   * @return A `number` representing the size of the {@link Field} type in terms of {@link Field} type itself.
    */
   static sizeInFields(): number;
 
@@ -865,6 +958,7 @@ Creating a {@link Field} from a negative number can result in unexpected behavio
  * Use [[assertEquals]] to enforce the value of a Bool.
  */
 declare function Bool(x: Bool | boolean): Bool;
+
 declare class Bool {
   constructor(x: Bool | boolean);
 
