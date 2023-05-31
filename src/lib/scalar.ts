f4c232f71664dfc1abfa77484d96b41ffce6179a
Gregor <gregor.mitscha-baude@gmx.at>
2023-05-31 23:04:22 +0200

    remaining methods

--- src/lib/scalar.ts
+++ src/lib/scalar.ts
@@ -27,13 +27,40 @@ class Scalar {
     this.constantValue = constantValue ?? toConstantScalar(bits);
   }
 
-  static fromBigint(scalar: Fq) {
+  /**
+   * Create a constant {@link Scalar} from a bigint, number, string or Scalar.
+   *
+   * If the input is too large, it is reduced modulo the scalar field size.
+   */
+  static from(x: Scalar | bigint | number | string) {
+    if (x instanceof Scalar) return x;
+    let scalar = Fq(x);
     let bits = toBits(scalar);
     return new Scalar(bits, scalar);
   }
 
+  /**
+   * @deprecated use {@link Scalar.from}
+   */
+  static fromBigint(x: bigint) {
+    return Scalar.from(x);
+  }
+
+  /**
+   * Creates a data structure from an array of serialized {@link Bool}.
+   *
+   * **Warning**: The bits are interpreted as the bits of 2s + 1 + 2^255, where s is the Scalar.
+   */
   static fromBits(bits: Bool[]) {
     return Scalar.fromFields(bits.map((b) => b.toField()));
+  }
+
+  /**
+   * Returns a random {@link Scalar}.
+   * Randomness can not be proven inside a circuit!
+   */
+  static random() {
+    return Scalar.from(Fq.random());
   }
 
   // operations on constant scalars
@@ -55,7 +82,7 @@ That means it can't be called in a @method or similar environment, and there's n
   neg() {
     let x = this.#assertConstant('neg');
     let z = Fq.negate(x);
-    return Scalar.fromBigint(z);
+    return Scalar.from(z);
   }
 
   /**
@@ -67,7 +94,7 @@ That means it can't be called in a @method or similar environment, and there's n
     let x = this.#assertConstant('add');
     let y0 = y.#assertConstant('add');
     let z = Fq.add(x, y0);
-    return Scalar.fromBigint(z);
+    return Scalar.from(z);
   }
 
   /**
@@ -79,7 +106,7 @@ That means it can't be called in a @method or similar environment, and there's n
     let x = this.#assertConstant('sub');
     let y0 = y.#assertConstant('sub');
     let z = Fq.sub(x, y0);
-    return Scalar.fromBigint(z);
+    return Scalar.from(z);
   }
 
   /**
@@ -91,7 +118,7 @@ That means it can't be called in a @method or similar environment, and there's n
     let x = this.#assertConstant('mul');
     let y0 = y.#assertConstant('mul');
     let z = Fq.mul(x, y0);
-    return Scalar.fromBigint(z);
+    return Scalar.from(z);
   }
 
   /**
@@ -105,7 +132,7 @@ That means it can't be called in a @method or similar environment, and there's n
     let y0 = y.#assertConstant('div');
     let z = Fq.div(x, y0);
     if (z === undefined) throw Error('Scalar.div(): Division by zero');
-    return Scalar.fromBigint(z);
+    return Scalar.from(z);
   }
 
   /**
@@ -203,6 +230,32 @@ That means it can't be called in a @method or similar environment, and there's n
      The only one of these whose behavior depends on the bit values of the input scalars
      is Group.scale, and that function boolean constrains the scalar input itself.
      */
+  }
+
+  // ProvableExtended<Scalar>
+
+  /**
+   * Serialize a {@link Scalar} to a JSON string.
+   * This operation does _not_ affect the circuit and can't be used to prove anything about the string representation of the Scalar.
+   */
+  static toJSON(x: Scalar) {
+    return x.toJSON();
+  }
+
+  /**
+   * Serializes this Scalar to a string
+   */
+  toJSON() {
+    let s = this.#assertConstant('toJSON');
+    return s.toString();
+  }
+
+  /**
+   * Deserialize a JSON structure into a {@link Scalar}.
+   * This operation does _not_ affect the circuit and can't be used to prove anything about the string representation of the Scalar.
+   */
+  static fromJSON(x: string) {
+    return Scalar.from(Fq.fromJSON(x));
   }
 }
 
