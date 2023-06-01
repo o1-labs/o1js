ade4f6268d7aebe0d9152e9a48afd6020130dbca
Florian <florian@technotro.com>
2023-06-01 16:14:53 +0200

    comments

--- src/lib/group.ts
+++ src/lib/group.ts
@@ -1,9 +1,8 @@
 import { Scalar, withMessage } from './core.js';
-import { Field, FieldConst, FieldType, FieldVar, isField } from './field.js';
-import { Bool, Snarky, Group as SnarkyGroup } from '../snarky.js';
+import { Field, FieldVar, isField } from './field.js';
+import { Bool, Snarky } from '../snarky.js';
 import { Field as Fp } from '../provable/field-bigint.js';
 import { Pallas } from '../bindings/crypto/elliptic_curve.js';
-import { Provable } from './provable.js';
 
 export { Group as NewGroup };
 
@@ -14,20 +13,19 @@ class Group {
   x: Field;
   y: Field;
 
-  static generator: Group;
-
-  static #toTuple(g: Group): [0, FieldVar, FieldVar] {
-    return [0, g.x.value, g.y.value];
+  /**
+   * The generator `g` of the Group.
+   */
+  static get generator() {
+    return new Group({
+      x: Pallas.one.x,
+      y: Pallas.one.y,
+    });
   }
 
-  static #fromAffine({ x, y }: { x: bigint; y: bigint; infinity: boolean }) {
-    return new Group({ x, y });
-  }
-
-  #isConstant() {
-    return this.x.isConstant() && this.y.isConstant();
-  }
-
+  /**
+   * Coerces anything group-like to a {@link Group}.
+   */
   constructor({
     x,
     y,
@@ -52,6 +50,23 @@ class Group {
     */
   }
 
+  // helpers
+  static #toTuple(g: Group): [0, FieldVar, FieldVar] {
+    return [0, g.x.value, g.y.value];
+  }
+
+  static #fromAffine({ x, y }: { x: bigint; y: bigint; infinity: boolean }) {
+    return new Group({ x, y });
+  }
+
+  #isConstant() {
+    return this.x.isConstant() && this.y.isConstant();
+  }
+
+  /**
+   * Return a {@link Bool} if the Group element is on the Pallas curve.
+   * It checks that the Weierstrass equation `y^2 = x^3 + 5` is satisfied.
+   */
   onCurve() {
     if (this.x.isConstant() && this.y.isConstant()) {
       const { add, mul, square } = Fp;
@@ -68,34 +83,41 @@ class Group {
     }
   }
 
-  add(p: Group) {
+  /**
+   * Adds this {@link Group} element to another {@link Group} element.
+   *
+   * ```ts
+   * let g1 = Group({ x: 1, y: 1})
+   * let g2 = g1.add(g1)
+   * ```
+   */
+  add(g: Group) {
     let { x: x1, y: y1 } = this;
-    let { x: x2, y: y2 } = p;
+    let { x: x2, y: y2 } = g;
 
     if ([x1, y1, x2, y2].every((e) => e.isConstant())) {
       if (x1.toBigInt() === 0n) {
-        return p;
+        return g;
       } else if (x2.toBigInt() === 0n) {
         return this;
       } else {
-        // TODO: figure out if the conversion of infinite is correct like that
         let g_proj = Pallas.add(
           Pallas.fromAffine({
             x: x1.toBigInt(),
             y: y1.toBigInt(),
-            infinity: x1.toBigInt() === 0n && y1.toBigInt() === 0n,
+            infinity: false,
           }),
           Pallas.fromAffine({
             x: x2.toBigInt(),
             y: y2.toBigInt(),
-            infinity: x2.toBigInt() === 0n && y2.toBigInt() === 0n,
+            infinity: false,
           })
         );
 
         return Group.#fromAffine(Pallas.toAffine(g_proj));
       }
     } else {
-      let [, x, y] = Snarky.group.add(Group.#toTuple(this), Group.#toTuple(p));
+      let [, x, y] = Snarky.group.add(Group.#toTuple(this), Group.#toTuple(g));
       return new Group({
         x,
         y,
@@ -103,10 +125,16 @@ class Group {
     }
   }
 
+  /**
+   * Subtracts another {@link Group} element from this one.
+   */
   sub(y: Group) {
     return this.add(y.neg());
   }
 
+  /**
+   * Negates this {@link Group}. Under the hood, it simply negates the `y` coordinate and leaves the `x` coordinate as is.
+   */
   neg() {
     let { x, y } = this;
     return new Group({
@@ -115,8 +143,17 @@ class Group {
     });
   }
 
-  scale(s: Scalar) {
-    let fields = s.toFields();
+  /**
+   * Elliptic curve scalar multiplication. Scales the {@link Group} element `n`-times by itself, where `n` is the {@link Scalar}.
+   *
+   * ```typescript
+   * let s = Scalar(5);
+   * let 5g = g.scale(s);
+   * ```
+   */
+  scale(s: Scalar | number | bigint) {
+    let scalar = s instanceof Scalar ? s : Scalar.fromBigInt(BigInt(s));
+    let fields = scalar.toFields();
 
     if (
       this.x.isConstant() &&
@@ -131,7 +168,7 @@ class Group {
           y: y.toBigInt(),
           infinity: x.toBigInt() === 1n && y.toBigInt() === 1n,
         }),
-        BigInt(s.toJSON())
+        BigInt(scalar.toJSON())
       );
       return Group.#fromAffine(Pallas.toAffine(g_proj));
     } else {
@@ -143,6 +180,14 @@ class Group {
     }
   }
 
+  /**
+   * Assert that this {@link Group} element equals another {@link Group} element.
+   * Throws an error if the assertion fails.
+   *
+   * ```ts
+   * g1.assertEquals(g2);
+   * ```
+   */
   assertEquals(g: Group, message?: string) {
     let { x: x1, y: y1 } = this;
     let { x: x2, y: y2 } = g;
@@ -151,6 +196,14 @@ class Group {
     // y1.assertEquals(y2, message); need to enable this later on, but it breaks constraint backwards compatibility
   }
 
+  /**
+   * Check if this {@link Group} element equals another {@link Group} element.
+   * Returns a {@link Bool}.
+   *
+   * ```ts
+   * g1.equals(g1); // Bool(true)
+   * ```
+   */
   equals(g: Group) {
     let { x: x1, y: y1 } = this;
     let { x: x2, y: y2 } = g;
@@ -168,6 +221,11 @@ class Group {
     }
   }
 
+  /**
+   * Serializes this {@link Group} element to a JSON object.
+   *
+   * This operation does NOT affect the circuit and can't be used to prove anything about the representation of the element.
+   */
   toJSON(): {
     x: string;
     y: string;
@@ -178,58 +236,139 @@ class Group {
     };
   }
 
+  /**
+   * Part of the {@link Provable} interface.
+   *
+   * Returns an array containing this {@link Group} element as an array of {@link Field} elements.
+   */
   toFields() {
     return [this.x, this.y];
   }
 
-  static add(x: Group, y: Group) {
-    return x.add(y);
+  /**
+   * Adds a {@link Group} element to another one.
+   */
+  static add(g1: Group, g2: Group) {
+    return g1.add(g2);
   }
 
-  static sub(x: Group, y: Group) {
-    return x.sub(y);
+  /**
+   * Subtracts a {@link Group} element from another one.
+   */
+  static sub(g1: Group, g2: Group) {
+    return g1.sub(g2);
   }
 
-  static neg(x: Group) {
-    return x.neg();
+  /**
+   * Negates a {@link Group} element. Under the hood, it simply negates the `y` coordinate and leaves the `x` coordinate as is.
+   *
+   * ```typescript
+   * let gNeg = Group.neg(g);
+   * ```
+   */
+  static neg(g: Group) {
+    return g.neg();
   }
 
-  static scale(x: Group, y: Scalar) {
-    return x.scale(y);
+  /**
+   * Elliptic curve scalar multiplication. Scales a {@link Group} element `n`-times by itself, where `n` is the {@link Scalar}.
+   *
+   * ```typescript
+   * let s = Scalar(5);
+   * let 5g = Group.scale(g, s);
+   * ```
+   */
+  static scale(g: Group, s: Scalar) {
+    return g.scale(s);
   }
 
-  static assertEqual(x: Group, y: Group) {
-    x.assertEquals(y);
+  /**
+   * Assert that two {@link Group} elements are equal to another.
+   * Throws an error if the assertion fails.
+   *
+   * ```ts
+   * Group.assertEquals(g1, g2);
+   * ```
+   */
+  static assertEqual(g1: Group, g2: Group) {
+    g1.assertEquals(g2);
   }
 
-  static equal(x: Group, y: Group) {
-    return x.equals(y);
+  /**
+   * Checks if a {@link Group} element is equal to another {@link Group} element.
+   * Returns a {@link Bool}.
+   *
+   * ```ts
+   * Group.equal(g1, g2); // Bool(true)
+   * ```
+   */
+  static equal(g1: Group, g2: Group) {
+    return g1.equals(g2);
   }
 
-  static toFields(x: Group) {
-    return x.toFields();
+  /**
+   * Part of the {@link Provable} interface.
+   *
+   * Returns an array containing a {@link Group} element as an array of {@link Field} elements.
+   */
+  static toFields(g: Group) {
+    return g.toFields();
   }
 
-  static toAuxiliary(x?: Group) {
+  /**
+   * Part of the {@link Provable} interface.
+   *
+   * Returns an empty array.
+   */
+  static toAuxiliary() {
     return [];
   }
 
+  /**
+   * Part of the {@link Provable} interface.
+   *
+   * Deserializes a {@link Group} element from a list of field elements.
+   */
   static fromFields([x, y]: Field[]) {
     return new Group({ x, y });
   }
 
+  /**
+   * Part of the {@link Provable} interface.
+   *
+   * Returns 2.
+   */
   static sizeInFields() {
     return 2;
   }
 
-  static toJSON(x: Group) {
-    return x.toJSON();
+  /**
+   * Serializes a {@link Group} element to a JSON object.
+   *
+   * This operation does NOT affect the circuit and can't be used to prove anything about the representation of the element.
+   */
+  static toJSON(g: Group) {
+    return g.toJSON();
   }
 
-  static fromJSON({ x, y }: { x: string | number; y: string | number }) {
+  /**
+   * Deserializes a JSON-like structure to a {@link Group} element.
+   *
+   * This operation does NOT affect the circuit and can't be used to prove anything about the representation of the element.
+   */
+  static fromJSON({
+    x,
+    y,
+  }: {
+    x: string | number | bigint | Field | FieldVar;
+    y: string | number | bigint | Field | FieldVar;
+  }) {
     return new Group({ x, y });
   }
 
+  /**
+   * Checks that a {@link Group} element is constraint properly by checking that the element is on the curve.
+   */
   static check(g: Group) {
     console.log('its running check');
     try {
@@ -242,6 +381,10 @@ class Group {
     }
   }
 
+  /**
+   * Return a {@link Bool} if a Group element is on the Pallas curve.
+   * It checks that the Weierstrass equation `y^2 = x^3 + 5` is satisfied.
+   */
   static onCurve(g: Group) {
     return g.onCurve();
   }
