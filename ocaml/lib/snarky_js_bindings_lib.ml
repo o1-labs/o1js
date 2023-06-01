41c66c4fddd8118b7f39ee24b62ddf725eeab6ad
Florian <florian@technotro.com>
2023-06-01 11:41:34 +0200

    equals

--- ocaml/lib/snarky_js_bindings_lib.ml
+++ ocaml/lib/snarky_js_bindings_lib.ml
@@ -1357,6 +1357,11 @@ module Snarky = struct
     let scale p (scalar_bits : Boolean.var array) =
       Pickles.Step_main_inputs.Ops.scale_fast_msb_bits p
         (Shifted_value scalar_bits)
+
+    let equals
+        ((x1, y1) : Impl.field Snarky_backendless.Cvar.t Tuple_lib.Double.t)
+        ((x2, y2) : Impl.field Snarky_backendless.Cvar.t Tuple_lib.Double.t) =
+      Boolean.all [ Impl.Field.equal x1 x2; Impl.Field.equal y1 y2 ]
   end
 
   module Circuit = struct
@@ -1453,6 +1458,8 @@ let snarky =
         method onCurve = Snarky.Group.on_curve
 
         method scale = Snarky.Group.scale
+
+        method equals = Snarky.Group.equals
       end
 
     val circuit =
