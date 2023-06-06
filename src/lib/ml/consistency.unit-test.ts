4f4705ffc6a8dcf923456390d51abcbf23a80e53
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-06 21:28:23 +0200

    add pk base58 tests

--- src/lib/ml/consistency.unit-test.ts
+++ src/lib/ml/consistency.unit-test.ts
@@ -1,6 +1,7 @@
 import { Test } from '../../snarky.js';
 import { Random, test } from '../testing/property.js';
-import { PrivateKey } from '../signature.js';
+import { Field, Bool } from '../core.js';
+import { PrivateKey, PublicKey } from '../signature.js';
 import { Ml } from './conversion.js';
 import { expect } from 'expect';
 
@@ -21,4 +22,21 @@ test(Random.privateKey, (s) => {
   // fromBase58 - check consistency with where we started
   expect(PrivateKey.fromBase58(js)).toEqual(sk);
   expect(Test.encoding.privateKeyOfBase58(ml)).toEqual(skMl);
+});
+
+// PublicKey.toBase58, fromBase58
+
+test(Random.publicKey, (pk0) => {
+  // public key from bigint
+  let pk = PublicKey.from({ x: Field(pk0.x), isOdd: Bool(!!pk0.isOdd) });
+  let pkMl = Ml.fromPublicKey(pk);
+
+  // toBase58 - check consistency with ml
+  let ml = Test.encoding.publicKeyToBase58(pkMl);
+  let js = pk.toBase58();
+  expect(js).toEqual(ml);
+
+  // fromBase58 - check consistency with where we started
+  expect(PublicKey.fromBase58(js)).toEqual(pk);
+  expect(Test.encoding.publicKeyOfBase58(ml)).toEqual(pkMl);
 });
