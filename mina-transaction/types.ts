e3537085ea02548f2350b78761c56e7c0f18de8a
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-08 12:42:34 +0200

    expose bigint transaction types

--- mina-transaction/types.ts
+++ mina-transaction/types.ts
@@ -1,2 +1,3 @@
 export { provableFromLayout, toJSONEssential } from './gen/transaction.js';
 export * as Types from './gen/transaction.js';
+export * as TypesBigint from './gen/transaction-bigint.js';
