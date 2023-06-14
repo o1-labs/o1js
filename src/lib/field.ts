c4e239b0c443270fa4778329901fcb548987da8c
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-14 13:09:38 +0200

    factor out helper

--- src/lib/field.ts
+++ src/lib/field.ts
@@ -9,7 +9,15 @@ import { Bool } from './bool.js';
 export { Field };
 
 // internal API
-export { ConstantField, FieldType, FieldVar, FieldConst, isField, withMessage };
+export {
+  ConstantField,
+  FieldType,
+  FieldVar,
+  FieldConst,
+  isField,
+  withMessage,
+  checkBitLength,
+};
 
 type FieldConst = Uint8Array;
 
@@ -886,15 +894,6 @@ class Field {
     }
   }
 
-  static #checkBitLength(name: string, length: number) {
-    if (length > Fp.sizeInBits)
-      throw Error(
-        `${name}: bit length must be ${Fp.sizeInBits} or less, got ${length}`
-      );
-    if (length <= 0)
-      throw Error(`${name}: bit length must be positive, got ${length}`);
-  }
-
   /**
    * Returns an array of {@link Bool} elements representing [little endian binary representation](https://en.wikipedia.org/wiki/Endianness) of this {@link Field} element.
    *
@@ -909,7 +908,7 @@ class Field {
    * @return An array of {@link Bool} element representing little endian binary representation of this {@link Field}.
    */
   toBits(length?: number) {
-    if (length !== undefined) Field.#checkBitLength('Field.toBits()', length);
+    if (length !== undefined) checkBitLength('Field.toBits()', length);
     if (this.isConstant()) {
       let bits = Fp.toBits(this.toBigInt());
       if (length !== undefined) {
@@ -936,7 +935,7 @@ class Field {
    */
   static fromBits(bits: (Bool | boolean)[]) {
     let length = bits.length;
-    Field.#checkBitLength('Field.fromBits()', length);
+    checkBitLength('Field.fromBits()', length);
     if (bits.every((b) => typeof b === 'boolean' || b.toField().isConstant())) {
       let bits_ = bits
         .map((b) => (typeof b === 'boolean' ? b : b.toBoolean()))
@@ -964,7 +963,7 @@ class Field {
    * @return A {@link Field} element that is equal to the `length` of this {@link Field} element.
    */
   rangeCheckHelper(length: number) {
-    Field.#checkBitLength('Field.rangeCheckHelper()', length);
+    checkBitLength('Field.rangeCheckHelper()', length);
     if (length % 16 !== 0)
       throw Error(
         'Field.rangeCheckHelper(): `length` has to be a multiple of 16.'
@@ -1219,6 +1218,8 @@ class Field {
   static sizeInBytes() {
     return Fp.sizeInBytes();
   }
+
+  static sizeInBits = Fp.sizeInBits;
 }
 
 const FieldBinable = defineBinable({
@@ -1263,4 +1264,17 @@ function withMessage(error: unknown, message?: string) {
   if (message === undefined || !(error instanceof Error)) return error;
   error.message = `${message}\n${error.message}`;
   return error;
+}
+
+function checkBitLength(
+  name: string,
+  length: number,
+  maxLength = Fp.sizeInBits
+) {
+  if (length > maxLength)
+    throw Error(
+      `${name}: bit length must be ${maxLength} or less, got ${length}`
+    );
+  if (length < 0)
+    throw Error(`${name}: bit length must be non-negative, got ${length}`);
 }
