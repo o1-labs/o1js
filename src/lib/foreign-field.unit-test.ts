4212c9d6f8fe04407518a5f5010bcd0a6e37d050
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-14 12:26:30 +0200

    add arithemtic tests

--- src/lib/foreign-field.unit-test.ts
+++ src/lib/foreign-field.unit-test.ts
@@ -1,7 +1,11 @@
 import { ProvablePure } from '../snarky.js';
-import { ForeignField, createForeignField } from './foreign-field.js';
+import { FieldVar } from './field.js';
+import { ForeignField, createForeignField, limbBits } from './foreign-field.js';
+import { Scalar as Fq } from '../provable/curve-bigint.js';
 import { Scalar } from './scalar.js';
 import { expect } from 'expect';
+import { createEquivalenceTesters, throwError } from './testing/equivalent.js';
+import { Random } from './testing/random.js';
 
 let ForeignScalar = createForeignField(Scalar.ORDER);
 
@@ -9,9 +13,30 @@ let ForeignScalar = createForeignField(Scalar.ORDER);
 ForeignScalar satisfies ProvablePure<ForeignField>;
 
 // basic constructor / IO
-let scalar = new ForeignScalar(1n << 88n);
+let s0 = 1n + ((1n + (1n << limbBits)) << limbBits);
+let scalar = new ForeignScalar(s0);
 
-console.dir(scalar, { depth: Infinity });
-console.dir(ForeignScalar.modulus, { depth: Infinity });
+expect(scalar.value).toEqual([0, FieldVar[1], FieldVar[1], FieldVar[1]]);
+expect(scalar.toBigInt()).toEqual(s0);
 
-expect(scalar.toBigInt()).toEqual(1n << 88n);
+let { equivalent1, equivalent2, equivalentVoid1, equivalentVoid2 } =
+  createEquivalenceTesters(ForeignScalar, (x) => new ForeignScalar(x));
+
+equivalent2((x, y) => x.add(y), Fq.add, Random.scalar);
+equivalent1((x) => x.neg(), Fq.negate, Random.scalar);
+equivalent2((x, y) => x.sub(y), Fq.sub, Random.scalar);
+equivalent2((x, y) => x.mul(y), Fq.mul, Random.scalar);
+equivalent1(
+  (x) => x.inv(),
+  (x) => Fq.inverse(x) ?? throwError('division by 0'),
+  Random.scalar
+);
+
+// equivalent2(
+//   (x, y) => x.equals(y).toField(),
+//   (x, y) => BigInt(x === y)
+// );
+// equivalentVoid2(
+//   (x, y) => x.assertEquals(y),
+//   (x, y) => x === y || throwError('not equal')
+// );
