cc90fb54f93d331e150c22fcbe73d55c85dd04ec
Martin Minkov <minkovlmartin@gmail.com>
2023-06-15 15:30:57 -0700

    docs(snarky.d.ts): fix typos in comments, change "auxilary" to "auxiliary" for consistency and clarity

--- src/snarky.d.ts
+++ src/snarky.d.ts
@@ -35,23 +35,23 @@ declare interface Provable<T> {
   toFields: (value: T) => Field[];
 
    /**
-   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that make up the "auxilary" (non-provable) data of `value`.
+   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that make up the "auxiliary" (non-provable) data of `value`.
    * 
    * @param value - the element of type `T` to generate the auxiliary data array from, optional. If not provided, a default value for auxiliary data is returned.
    * 
-   * @return An array of any type describing how this `T` element is made up of "auxilary" (non-provable) data.
+   * @return An array of any type describing how this `T` element is made up of "auxiliary" (non-provable) data.
    */
   toAuxiliary: (value?: T) => any[];
 
   /**
-   * A function that returns an element of type `T` from the given provable and "auxilary" data.
+   * A function that returns an element of type `T` from the given provable and "auxiliary" data.
    * 
    * **Important**: For any element of type `T`, this function is the reverse operation of calling {@link toFields} and {@link toAuxilary} methods on an element of type `T`.
    * 
    * @param fields - an array of {@link Field} elements describing the provable data of the new `T` element.
-   * @param aux - an array of any type describing the "auxilary" data of the new `T` element, optional.
+   * @param aux - an array of any type describing the "auxiliary" data of the new `T` element, optional.
    * 
-   * @return An element of type `T` generated from the given provable and "auxilary" data.
+   * @return An element of type `T` generated from the given provable and "auxiliary" data.
    */
   fromFields: (fields: Field[], aux: any[]) => T;
 
@@ -77,7 +77,7 @@ declare interface Provable<T> {
 
 /**
  * `ProvablePure<T>` is a special kind of {@link Provable} interface, where the "auxiliary" (non-provable) data is empty. This means the type consists only of field elements, in that sense it is "pure".
- * Any element on the interface `ProvablePure<T>` is also an element of the interface `Provable<T>` where the "auxilary" data is empty.
+ * Any element on the interface `ProvablePure<T>` is also an element of the interface `Provable<T>` where the "auxiliary" data is empty.
  * 
  * Examples where `ProvablePure<T>` is required are types of on-chain state, events and actions.
  * 
@@ -94,12 +94,12 @@ declare interface ProvablePure<T> extends Provable<T> {
   toFields: (value: T) => Field[];
 
    /**
-   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that make up the "auxilary" (non-provable) data of `value`.
-   * As any element of the interface `ProvablePure<T>` includes no "auxilary" data by definition, this function always returns a default value.
+   * A function that takes `value` (optional), an element of type `T`, as argument and returns an array of any type that make up the "auxiliary" (non-provable) data of `value`.
+   * As any element of the interface `ProvablePure<T>` includes no "auxiliary" data by definition, this function always returns a default value.
    * 
-   * @param value - the element of type `T` to generate the auxilary data array from, optional. If not provided, a default value for auxiliary data is returned.
+   * @param value - the element of type `T` to generate the auxiliary data array from, optional. If not provided, a default value for auxiliary data is returned.
    * 
-   * @return An empty array, as any element of the interface `ProvablePure<T>` includes no "auxilary" data by definition.
+   * @return An empty array, as any element of the interface `ProvablePure<T>` includes no "auxiliary" data by definition.
    */
   toAuxiliary: (value?: T) => any[];
 
