5177ecd481ba309cc3933a83089768f6111310de
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-22 15:01:37 +0200

    clean up some build logic

--- src/build/utils.js
+++ src/build/utils.js
@@ -2,17 +2,15 @@ import fse from 'fs-extra';
 
 export { copyFromTo };
 
-async function copyFromTo(files, srcDir = undefined, targetDir = undefined) {
-  let promises = [];
-  for (let source of files) {
-    let target = source.replace(`${srcDir}/`, `${targetDir}/`);
-    promises.push(
-      fse.copy(source, target, {
+function copyFromTo(files, srcDir = undefined, targetDir = undefined) {
+  return Promise.all(
+    files.map((source) => {
+      let target = source.replace(srcDir, targetDir);
+      return fse.copy(source, target, {
         recursive: true,
         overwrite: true,
         dereference: true,
-      })
-    );
-  }
-  await Promise.all(promises);
+      });
+    })
+  );
 }
