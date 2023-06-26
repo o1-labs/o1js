40fe626566b013faf5989fbf62bbfa1597cbcd5a
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-26 16:22:10 +0200

    fix invalid code in bindings.js

--- kimchi/js/bindings.js
+++ kimchi/js/bindings.js
@@ -999,8 +999,7 @@ var caml_fp_srs_batch_accumulator_check = function (srs, comms, chals) {
 var caml_fp_srs_batch_accumulator_generate = function (srs, comms, chals) {
     var rust_chals = caml_fp_vector_to_rust(chals);
     var rust_comms = plonk_wasm.caml_fp_srs_batch_accumulator_generate(srs, comms, rust_chals);
-    var rust_comms = caml_array_of_rust_vector(rust_comms, plonk_wasm.WasmGVesta, rust_affine_to_caml_affine, false);
-    return ok;
+    return caml_array_of_rust_vector(rust_comms, plonk_wasm.WasmGVesta, rust_affine_to_caml_affine, false);
 };
 
 // Provides: caml_fp_srs_h
@@ -1081,8 +1080,7 @@ var caml_fq_srs_batch_accumulator_check = function (srs, comms, chals) {
 var caml_fq_srs_batch_accumulator_generate = function (srs, comms, chals) {
     var rust_chals = caml_fq_vector_to_rust(chals);
     var rust_comms = plonk_wasm.caml_fq_srs_batch_accumulator_generate(srs, comms, rust_chals);
-    var rust_comms = caml_array_of_rust_vector(rust_comms, plonk_wasm.WasmGPallas, rust_affine_to_caml_affine, false);
-    return ok;
+    return caml_array_of_rust_vector(rust_comms, plonk_wasm.WasmGPallas, rust_affine_to_caml_affine, false);
 };
 
 // Provides: caml_fq_srs_h
