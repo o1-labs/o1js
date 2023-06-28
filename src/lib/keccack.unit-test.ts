05550d970d01fad74a8ff3d0bfe35bef2352c58e
Martin Minkov <minkovlmartin@gmail.com>
2023-06-28 14:38:38 -0700

    feat(keccack.unit-test.ts): add tests for digest to hex and hex to digest conversions

--- src/lib/keccack.unit-test.ts
+++ src/lib/keccack.unit-test.ts
@@ -1,5 +1,8 @@
 import { test, Random } from './testing/property.js';
 import { UInt8 } from './int.js';
+import { Hash } from './hash.js';
+import { Provable } from './provable.js';
+import { expect } from 'expect';
 
 // Test constructor
 test(Random.uint8, Random.json.uint8, (x, y, assert) => {
@@ -29,3 +32,47 @@ test.negative(Random.int(-10, -1), (x) => new UInt8(x));
 
 // throws on numbers >= 2^8
 test.negative(Random.uint8.invalid, (x) => new UInt8(x));
+
+// test digest->hex and hex->digest conversions
+checkHashConversions();
+console.log('hashing digest conversions matches! 🎉');
+
+function checkHashConversions() {
+  for (let i = 0; i < 2; i++) {
+    const data = Random.array(Random.uint8, Random.nat(20))
+      .create()()
+      .map((x) => new UInt8(x));
+
+    Provable.runAndCheck(() => {
+      let digest = Hash.SHA224.hash(data);
+      expectDigestToEqualHex(digest);
+
+      digest = Hash.SHA256.hash(data);
+      expectDigestToEqualHex(digest);
+
+      digest = Hash.SHA384.hash(data);
+      expectDigestToEqualHex(digest);
+
+      digest = Hash.SHA512.hash(data);
+      expectDigestToEqualHex(digest);
+
+      digest = Hash.Keccack256.hash(data);
+      expectDigestToEqualHex(digest);
+    });
+  }
+}
+
+function expectDigestToEqualHex(digest: UInt8[]) {
+  Provable.asProver(() => {
+    const hex = UInt8.toHex(digest);
+    expect(equals(digest, UInt8.fromHex(hex))).toBe(true);
+  });
+}
+
+function equals(a: UInt8[], b: UInt8[]): boolean {
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++)
+    if (a[i].value.toConstant() === b[i].value.toConstant()) return false;
+
+  return true;
+}
