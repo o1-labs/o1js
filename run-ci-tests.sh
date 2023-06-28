2d2af219c6d8fb62dba6e7e8c97ade67f3d11e5d
Gregor <gregor.mitscha-baude@gmx.at>
2023-06-28 09:20:28 +0200

    test cjs in ci

--- run-ci-tests.sh
+++ run-ci-tests.sh
@@ -39,5 +39,10 @@ case $TEST_TYPE in
       echo "Running Regression checks"
       ./run ./src/examples/vk_regression.ts --bundle ;;
 
+    "CommonJS test" )
+      echo "Testing CommonJS version";
+      node src/example/commonjs
+      ;;
+
     * ) echo "ERROR: Invalid enviroment variable, not clear what tests to run! $CI_NODE_INDEX"; exit 1 ;;
 esac
