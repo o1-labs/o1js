4360afeef2b011e347d239370f18f9d450f77e6f
Martin Minkov <minkovlmartin@gmail.com>
2023-06-29 16:12:50 -0700

    feat(examples): start debugging hashing example

---
+++ src/examples/zkapps/hashing/hash.ts
@@ -0,0 +1,96 @@
+import {
+  Hash,
+  UInt8,
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  PrivateKey,
+  Permissions,
+  Struct,
+} from 'snarkyjs';
+
+export const adminPrivateKey = PrivateKey.random();
+export const adminPublicKey = adminPrivateKey.toPublicKey();
+
+let initialCommitment: Field = Field(0);
+
+// 32 UInts
+export class HashInput extends Struct({
+  data: [
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+    UInt8,
+  ],
+}) {}
+
+export class HashStorage extends SmartContract {
+  @state(Field) commitment = State<Field>();
+
+  init() {
+    super.init();
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+    });
+    this.commitment.set(initialCommitment);
+  }
+
+  @method SHA224(xs: HashInput) {
+    const shaHash = Hash.SHA224.hash(xs.data);
+    const commitment = Hash.hash(shaHash.map((f) => f.toField()));
+    this.commitment.set(commitment);
+  }
+
+  @method SHA256(xs: HashInput) {
+    const shaHash = Hash.SHA256.hash(xs.data);
+    const commitment = Hash.hash(shaHash.map((f) => f.toField()));
+    this.commitment.set(commitment);
+  }
+
+  @method SHA384(xs: HashInput) {
+    const shaHash = Hash.SHA384.hash(xs.data);
+    const commitment = Hash.hash(shaHash.map((f) => f.toField()));
+    this.commitment.set(commitment);
+  }
+
+  @method SHA512(xs: HashInput) {
+    const shaHash = Hash.SHA512.hash(xs.data);
+    const commitment = Hash.hash(shaHash.map((f) => f.toField()));
+    this.commitment.set(commitment);
+  }
+
+  @method Keccak256(xs: HashInput) {
+    const shaHash = Hash.Keccak256.hash(xs.data);
+    const commitment = Hash.hash(shaHash.map((f) => f.toField()));
+    this.commitment.set(commitment);
+  }
+}
