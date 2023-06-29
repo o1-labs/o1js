88c687fdda42fbc05bb30bddc545bc84b219e0a4
Martin Minkov <minkovlmartin@gmail.com>
2023-06-29 12:35:47 -0700

    feat(keccack.unit-test.ts): add support for provable testing

--- src/lib/keccack.unit-test.ts
+++ src/lib/keccack.unit-test.ts
@@ -4,6 +4,8 @@ import { Hash } from './hash.js';
 import { Provable } from './provable.js';
 import { expect } from 'expect';
 
+let RandomUInt8 = Random.map(Random.uint8, (x) => UInt8.from(x));
+
 // Test constructor
 test(Random.uint8, Random.uint8, (x, y, assert) => {
   let z = new UInt8(x);
@@ -34,45 +36,67 @@ test.negative(Random.int(-10, -1), (x) => new UInt8(x));
 test.negative(Random.uint8.invalid, (x) => new UInt8(x));
 
 // test digest->hex and hex->digest conversions
-checkHashConversions();
+checkHashInCircuit();
+checkHashOutCircuit();
 console.log('hashing digest conversions matches! 🎉');
 
-function checkHashConversions() {
-  for (let i = 0; i < 2; i++) {
-    const data = Random.array(Random.uint8, Random.nat(20))
-      .create()()
-      .map((x) => new UInt8(x));
+// check in-circuit
+function checkHashInCircuit() {
+  Provable.runAndCheck(() => {
+    let d0 = Provable.witness(UInt8, () => new UInt8(RandomUInt8.create()()));
+    let d1 = Provable.witness(UInt8, () => new UInt8(RandomUInt8.create()()));
+    let d2 = Provable.witness(UInt8, () => new UInt8(RandomUInt8.create()()));
+    let d3 = Provable.witness(UInt8, () => new UInt8(RandomUInt8.create()()));
+    let d4 = Provable.witness(UInt8, () => new UInt8(RandomUInt8.create()()));
 
-    Provable.runAndCheck(() => {
-      let digest = Hash.SHA224.hash(data);
-      expectDigestToEqualHex(digest);
-
-      digest = Hash.SHA256.hash(data);
-      expectDigestToEqualHex(digest);
-
-      digest = Hash.SHA384.hash(data);
-      expectDigestToEqualHex(digest);
-
-      digest = Hash.SHA512.hash(data);
-      expectDigestToEqualHex(digest);
-
-      digest = Hash.Keccack256.hash(data);
-      expectDigestToEqualHex(digest);
-    });
-  }
-}
-
-function expectDigestToEqualHex(digest: UInt8[]) {
-  Provable.asProver(() => {
-    const hex = UInt8.toHex(digest);
-    expect(equals(digest, UInt8.fromHex(hex))).toBe(true);
+    let data = [d0, d1, d2, d3, d4];
+    checkHashConversions(data, true);
   });
 }
 
-function equals(a: UInt8[], b: UInt8[]): boolean {
+// check out-of-circuit
+function checkHashOutCircuit() {
+  let r = Random.array(RandomUInt8, Random.nat(20)).create()();
+  checkHashConversions(r, false);
+}
+
+function checkHashConversions(data: UInt8[], provable: boolean) {
+  let digest = Hash.SHA224.hash(data);
+  expectDigestToEqualHex(digest, provable);
+
+  digest = Hash.SHA256.hash(data);
+  expectDigestToEqualHex(digest, provable);
+
+  digest = Hash.SHA384.hash(data);
+  expectDigestToEqualHex(digest, provable);
+
+  digest = Hash.SHA512.hash(data);
+  expectDigestToEqualHex(digest, provable);
+
+  digest = Hash.Keccack256.hash(data);
+  expectDigestToEqualHex(digest, provable);
+}
+
+function expectDigestToEqualHex(digest: UInt8[], provable: boolean) {
+  if (provable) {
+    Provable.asProver(() => {
+      const hex = UInt8.toHex(digest);
+      expect(equals(digest, UInt8.fromHex(hex), provable)).toBe(true);
+    });
+  } else {
+    const hex = UInt8.toHex(digest);
+    expect(equals(digest, UInt8.fromHex(hex), provable)).toBe(true);
+  }
+}
+
+function equals(a: UInt8[], b: UInt8[], provable: boolean): boolean {
   if (a.length !== b.length) return false;
   for (let i = 0; i < a.length; i++)
-    if (a[i].value.toConstant() === b[i].value.toConstant()) return false;
+    if (provable) {
+      a[i].assertEquals(b[i]);
+    } else {
+      if (a[i].value.toConstant() === b[i].value.toConstant()) return false;
+    }
 
   return true;
 }
