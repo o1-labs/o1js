3b150d1cdd588f36188a1e550eca01d881fa5788
Gregor <gregor.mitscha-baude@gmx.at>
2023-08-24 21:30:07 +0200

    use ts bindings for bigint and field

--- kimchi/js/bindings.js
+++ kimchi/js/bindings.js
@@ -37,76 +37,62 @@ var caml_bytes_to_uint8array = function (ocaml_bytes) {
     return bytes;
 };
 
+// TODO
 // Provides: caml_bigint_256_of_numeral
-// Requires: plonk_wasm, caml_jsstring_of_string
+// Requires: tsBindings, caml_jsstring_of_string
 var caml_bigint_256_of_numeral = function (s, len, base) {
-    return plonk_wasm.caml_bigint_256_of_numeral(caml_jsstring_of_string(s), len, base);
+    return tsBindings.caml_bigint_256_of_numeral(caml_jsstring_of_string(s), len, base);
 };
 
 // Provides: caml_bigint_256_of_decimal_string
-// Requires: plonk_wasm, caml_jsstring_of_string
-var caml_bigint_256_of_decimal_string = function (s) {
-    return plonk_wasm.caml_bigint_256_of_decimal_string(caml_jsstring_of_string(s));
-};
+// Requires: tsBindings
+var caml_bigint_256_of_decimal_string = tsBindings.caml_bigint_256_of_decimal_string;
 
+// TODO
 // Provides: caml_bigint_256_num_limbs
-// Requires: plonk_wasm
-var caml_bigint_256_num_limbs = plonk_wasm.caml_bigint_256_num_limbs
+// Requires: tsBindings
+var caml_bigint_256_num_limbs = tsBindings.caml_bigint_256_num_limbs;
 
+// TODO
 // Provides: caml_bigint_256_bytes_per_limb
-// Requires: plonk_wasm
-var caml_bigint_256_bytes_per_limb = plonk_wasm.caml_bigint_256_bytes_per_limb
+// Requires: tsBindings
+var caml_bigint_256_bytes_per_limb = tsBindings.caml_bigint_256_bytes_per_limb;
 
+// TODO
 // Provides: caml_bigint_256_div
-// Requires: plonk_wasm
-var caml_bigint_256_div = plonk_wasm.caml_bigint_256_div
+// Requires: tsBindings
+var caml_bigint_256_div = tsBindings.caml_bigint_256_div;
 
 // Provides: caml_bigint_256_compare
-// Requires: plonk_wasm
-var caml_bigint_256_compare = plonk_wasm.caml_bigint_256_compare
+// Requires: tsBindings
+var caml_bigint_256_compare = tsBindings.caml_bigint_256_compare;
 
 // Provides: caml_bigint_256_print
-// Requires: plonk_wasm
-var caml_bigint_256_print = plonk_wasm.caml_bigint_256_print
+// Requires: tsBindings
+var caml_bigint_256_print = tsBindings.caml_bigint_256_print;
 
 // Provides: caml_bigint_256_to_string
-// Requires: plonk_wasm, caml_string_of_jsstring
-var caml_bigint_256_to_string = function (x) {
-    return caml_string_of_jsstring(plonk_wasm.caml_bigint_256_to_string(x));
-};
+// Requires: tsBindings
+var caml_bigint_256_to_string = tsBindings.caml_bigint_256_to_string;
 
 // Provides: caml_bigint_256_test_bit
-// Requires: plonk_wasm, caml_js_to_bool
-var caml_bigint_256_test_bit = function (x, i) {
-    return caml_js_to_bool(plonk_wasm.caml_bigint_256_test_bit(x, i));
-};
+// Requires: tsBindings
+var caml_bigint_256_test_bit = tsBindings.caml_bigint_256_test_bit;
 
 // Provides: caml_bigint_256_to_bytes
-// Requires: plonk_wasm, caml_bytes_of_uint8array
-var caml_bigint_256_to_bytes = function (x) {
-    return caml_bytes_of_uint8array(plonk_wasm.caml_bigint_256_to_bytes(x));
-};
+// Requires: tsBindings
+var caml_bigint_256_to_bytes = tsBindings.caml_bigint_256_to_bytes;
 
 // Provides: caml_bigint_256_of_bytes
-// Requires: plonk_wasm, caml_bytes_to_uint8array
-var caml_bigint_256_of_bytes = function (ocaml_bytes) {
-    return plonk_wasm.caml_bigint_256_of_bytes(caml_bytes_to_uint8array(ocaml_bytes));
-};
+// Requires: tsBindings
+var caml_bigint_256_of_bytes = tsBindings.caml_bigint_256_of_bytes;
 
+// TODO
 // Provides: caml_bigint_256_deep_copy
-// Requires: plonk_wasm
-var caml_bigint_256_deep_copy = plonk_wasm.caml_bigint_256_deep_copy
+// Requires: tsBindings
+var caml_bigint_256_deep_copy = tsBindings.caml_bigint_256_deep_copy
 
 
-
-
-
-// Provides: caml_pasta_fp_copy
-var caml_pasta_fp_copy = function (x, y) {
-    for (var i = 0, l = x.length; i < l; i++) {
-        x[i] = y[i];
-    }
-};
 
 // Provides: caml_option_of_maybe_undefined
 var caml_option_of_maybe_undefined = function (x) {
@@ -126,302 +112,270 @@ var caml_option_to_maybe_undefined = function (x) {
     }
 };
 
+
+
+
+// Provides: caml_pasta_fp_copy
+// Requires: tsBindings
+var caml_pasta_fp_copy = tsBindings.caml_pasta_fp_copy;
+
 // Provides: caml_pasta_fp_size_in_bits
-// Requires: plonk_wasm
-var caml_pasta_fp_size_in_bits = plonk_wasm.caml_pasta_fp_size_in_bits
+// Requires: tsBindings
+var caml_pasta_fp_size_in_bits = tsBindings.caml_pasta_fp_size_in_bits;
 
 // Provides: caml_pasta_fp_size
-// Requires: plonk_wasm
-var caml_pasta_fp_size = plonk_wasm.caml_pasta_fp_size
+// Requires: tsBindings
+var caml_pasta_fp_size = tsBindings.caml_pasta_fp_size;
 
 // Provides: caml_pasta_fp_add
-// Requires: plonk_wasm
-var caml_pasta_fp_add = plonk_wasm.caml_pasta_fp_add
+// Requires: tsBindings
+var caml_pasta_fp_add = tsBindings.caml_pasta_fp_add;
 
 // Provides: caml_pasta_fp_sub
-// Requires: plonk_wasm
-var caml_pasta_fp_sub = plonk_wasm.caml_pasta_fp_sub
+// Requires: tsBindings
+var caml_pasta_fp_sub = tsBindings.caml_pasta_fp_sub;
 
 // Provides: caml_pasta_fp_negate
-// Requires: plonk_wasm
-var caml_pasta_fp_negate = plonk_wasm.caml_pasta_fp_negate
+// Requires: tsBindings
+var caml_pasta_fp_negate = tsBindings.caml_pasta_fp_negate;
 
 // Provides: caml_pasta_fp_mul
-// Requires: plonk_wasm
-var caml_pasta_fp_mul = plonk_wasm.caml_pasta_fp_mul
+// Requires: tsBindings
+var caml_pasta_fp_mul = tsBindings.caml_pasta_fp_mul;
 
 // Provides: caml_pasta_fp_div
-// Requires: plonk_wasm
-var caml_pasta_fp_div = plonk_wasm.caml_pasta_fp_div
+// Requires: tsBindings
+var caml_pasta_fp_div = tsBindings.caml_pasta_fp_div;
 
 // Provides: caml_pasta_fp_inv
-// Requires: plonk_wasm, caml_option_of_maybe_undefined
-var caml_pasta_fp_inv = function (x) {
-    return caml_option_of_maybe_undefined(plonk_wasm.caml_pasta_fp_inv(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_inv = tsBindings.caml_pasta_fp_inv;
 
 // Provides: caml_pasta_fp_square
-// Requires: plonk_wasm
-var caml_pasta_fp_square = plonk_wasm.caml_pasta_fp_square
+// Requires: tsBindings
+var caml_pasta_fp_square = tsBindings.caml_pasta_fp_square
 
 // Provides: caml_pasta_fp_is_square
-// Requires: plonk_wasm, caml_js_to_bool
-var caml_pasta_fp_is_square = function (x) {
-    return caml_js_to_bool(plonk_wasm.caml_pasta_fp_is_square(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_is_square = tsBindings.caml_pasta_fp_is_square;
 
 // Provides: caml_pasta_fp_sqrt
-// Requires: plonk_wasm, caml_option_of_maybe_undefined
-var caml_pasta_fp_sqrt = function (x) {
-    return caml_option_of_maybe_undefined(plonk_wasm.caml_pasta_fp_sqrt(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_sqrt = tsBindings.caml_pasta_fp_sqrt;
 
 // Provides: caml_pasta_fp_of_int
-// Requires: plonk_wasm
-var caml_pasta_fp_of_int = plonk_wasm.caml_pasta_fp_of_int
+// Requires: tsBindings
+var caml_pasta_fp_of_int = tsBindings.caml_pasta_fp_of_int
 
 // Provides: caml_pasta_fp_to_string
-// Requires: plonk_wasm, caml_string_of_jsstring
-var caml_pasta_fp_to_string = function (x) {
-    return caml_string_of_jsstring(plonk_wasm.caml_pasta_fp_to_string(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_to_string = tsBindings.caml_pasta_fp_to_string;
 
 // Provides: caml_pasta_fp_of_string
-// Requires: plonk_wasm, caml_jsstring_of_string
-var caml_pasta_fp_of_string = function (x) {
-    return plonk_wasm.caml_pasta_fp_of_string(caml_jsstring_of_string(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_of_string = tsBindings.caml_pasta_fp_of_string;
 
 // Provides: caml_pasta_fp_print
-// Requires: plonk_wasm
-var caml_pasta_fp_print = plonk_wasm.caml_pasta_fp_print
+// Requires: tsBindings
+var caml_pasta_fp_print = tsBindings.caml_pasta_fp_print;
 
 // Provides: caml_pasta_fp_mut_add
-// Requires: caml_pasta_fp_copy, caml_pasta_fp_add
-var caml_pasta_fp_mut_add = function (x, y) {
-    caml_pasta_fp_copy(x, caml_pasta_fp_add(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fp_mut_add = tsBindings.caml_pasta_fp_mut_add;
 
 // Provides: caml_pasta_fp_mut_sub
-// Requires: caml_pasta_fp_copy, caml_pasta_fp_sub
-var caml_pasta_fp_mut_sub = function (x, y) {
-    caml_pasta_fp_copy(x, caml_pasta_fp_sub(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fp_mut_sub = tsBindings.caml_pasta_fp_mut_sub;
 
 // Provides: caml_pasta_fp_mut_mul
-// Requires: caml_pasta_fp_copy, caml_pasta_fp_mul
-var caml_pasta_fp_mut_mul = function (x, y) {
-    caml_pasta_fp_copy(x, caml_pasta_fp_mul(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fp_mut_mul = tsBindings.caml_pasta_fp_mut_mul;
 
 // Provides: caml_pasta_fp_mut_square
-// Requires: caml_pasta_fp_copy, caml_pasta_fp_square
-var caml_pasta_fp_mut_square = function (x) {
-    caml_pasta_fp_copy(x, caml_pasta_fp_square(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_mut_square = tsBindings.caml_pasta_fp_mut_square;
 
 // Provides: caml_pasta_fp_compare
-// Requires: plonk_wasm
-var caml_pasta_fp_compare = plonk_wasm.caml_pasta_fp_compare
+// Requires: tsBindings
+var caml_pasta_fp_compare = tsBindings.caml_pasta_fp_compare;
 
 // Provides: caml_pasta_fp_equal
-// Requires: plonk_wasm
-var caml_pasta_fp_equal = plonk_wasm.caml_pasta_fp_equal
+// Requires: tsBindings
+var caml_pasta_fp_equal = tsBindings.caml_pasta_fp_equal;
 
 // Provides: caml_pasta_fp_random
-// Requires: plonk_wasm
-var caml_pasta_fp_random = plonk_wasm.caml_pasta_fp_random
+// Requires: tsBindings
+var caml_pasta_fp_random = tsBindings.caml_pasta_fp_random;
 
+// TODO
 // Provides: caml_pasta_fp_rng
-// Requires: plonk_wasm
-var caml_pasta_fp_rng = plonk_wasm.caml_pasta_fp_rng
+// Requires: tsBindings
+var caml_pasta_fp_rng = tsBindings.caml_pasta_fp_rng;
 
 // Provides: caml_pasta_fp_to_bigint
-// Requires: plonk_wasm
-var caml_pasta_fp_to_bigint = plonk_wasm.caml_pasta_fp_to_bigint
+// Requires: tsBindings
+var caml_pasta_fp_to_bigint = tsBindings.caml_pasta_fp_to_bigint;
 
 // Provides: caml_pasta_fp_of_bigint
-// Requires: plonk_wasm
-var caml_pasta_fp_of_bigint = plonk_wasm.caml_pasta_fp_of_bigint
+// Requires: tsBindings
+var caml_pasta_fp_of_bigint = tsBindings.caml_pasta_fp_of_bigint;
 
+// TODO
 // Provides: caml_pasta_fp_two_adic_root_of_unity
-// Requires: plonk_wasm
-var caml_pasta_fp_two_adic_root_of_unity = plonk_wasm.caml_pasta_fp_two_adic_root_of_unity
+// Requires: tsBindings
+var caml_pasta_fp_two_adic_root_of_unity = tsBindings.caml_pasta_fp_two_adic_root_of_unity;
 
 // Provides: caml_pasta_fp_domain_generator
-// Requires: plonk_wasm
-var caml_pasta_fp_domain_generator = plonk_wasm.caml_pasta_fp_domain_generator
+// Requires: tsBindings
+var caml_pasta_fp_domain_generator = tsBindings.caml_pasta_fp_domain_generator;
 
+// TODO
 // Provides: caml_pasta_fp_to_bytes
-// Requires: plonk_wasm, caml_bytes_of_uint8array
-var caml_pasta_fp_to_bytes = function (x) {
-    var res = plonk_wasm.caml_pasta_fp_to_bytes(x);
-    return caml_bytes_of_uint8array(plonk_wasm.caml_pasta_fp_to_bytes(x));
-};
+// Requires: tsBindings
+var caml_pasta_fp_to_bytes = tsBindings.caml_pasta_fp_to_bytes;
 
+// TODO
 // Provides: caml_pasta_fp_of_bytes
-// Requires: plonk_wasm, caml_bytes_to_uint8array
-var caml_pasta_fp_of_bytes = function (ocaml_bytes) {
-    return plonk_wasm.caml_pasta_fp_of_bytes(caml_bytes_to_uint8array(ocaml_bytes));
-};
+// Requires: tsBindings
+var caml_pasta_fp_of_bytes = tsBindings.caml_pasta_fp_of_bytes;
 
+// TODO
 // Provides: caml_pasta_fp_deep_copy
-// Requires: plonk_wasm
-var caml_pasta_fp_deep_copy = plonk_wasm.caml_pasta_fp_deep_copy
+// Requires: tsBindings
+var caml_pasta_fp_deep_copy = tsBindings.caml_pasta_fp_deep_copy;
 
 
 
 
 
 // Provides: caml_pasta_fq_copy
-var caml_pasta_fq_copy = function (x, y) {
-    for (var i = 0, l = x.length; i < l; i++) {
-        x[i] = y[i];
-    }
-};
+// Requires: tsBindings
+var caml_pasta_fq_copy = tsBindings.caml_pasta_fq_copy;
 
 // Provides: caml_pasta_fq_size_in_bits
-// Requires: plonk_wasm
-var caml_pasta_fq_size_in_bits = plonk_wasm.caml_pasta_fq_size_in_bits
+// Requires: tsBindings
+var caml_pasta_fq_size_in_bits = tsBindings.caml_pasta_fq_size_in_bits;
 
 // Provides: caml_pasta_fq_size
 // Requires: plonk_wasm
-var caml_pasta_fq_size = plonk_wasm.caml_pasta_fq_size
+var caml_pasta_fq_size = tsBindings.caml_pasta_fq_size;
 
 // Provides: caml_pasta_fq_add
-// Requires: plonk_wasm
-var caml_pasta_fq_add = plonk_wasm.caml_pasta_fq_add
+// Requires: tsBindings
+var caml_pasta_fq_add = tsBindings.caml_pasta_fq_add;
 
 // Provides: caml_pasta_fq_sub
-// Requires: plonk_wasm
-var caml_pasta_fq_sub = plonk_wasm.caml_pasta_fq_sub
+// Requires: tsBindings
+var caml_pasta_fq_sub = tsBindings.caml_pasta_fq_sub;
 
 // Provides: caml_pasta_fq_negate
-// Requires: plonk_wasm
-var caml_pasta_fq_negate = plonk_wasm.caml_pasta_fq_negate
+// Requires: tsBindings
+var caml_pasta_fq_negate = tsBindings.caml_pasta_fq_negate;
 
 // Provides: caml_pasta_fq_mul
-// Requires: plonk_wasm
-var caml_pasta_fq_mul = plonk_wasm.caml_pasta_fq_mul
+// Requires: tsBindings
+var caml_pasta_fq_mul = tsBindings.caml_pasta_fq_mul;
 
 // Provides: caml_pasta_fq_div
-// Requires: plonk_wasm
-var caml_pasta_fq_div = plonk_wasm.caml_pasta_fq_div
+// Requires: tsBindings
+var caml_pasta_fq_div = tsBindings.caml_pasta_fq_div;
 
 // Provides: caml_pasta_fq_inv
-// Requires: plonk_wasm, caml_option_of_maybe_undefined
-var caml_pasta_fq_inv = function (x) {
-    return caml_option_of_maybe_undefined(plonk_wasm.caml_pasta_fq_inv(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_inv = tsBindings.caml_pasta_fq_inv; 
 
 // Provides: caml_pasta_fq_square
-// Requires: plonk_wasm
-var caml_pasta_fq_square = plonk_wasm.caml_pasta_fq_square
+// Requires: tsBindings
+var caml_pasta_fq_square = tsBindings.caml_pasta_fq_square
 
 // Provides: caml_pasta_fq_is_square
-// Requires: plonk_wasm, caml_js_to_bool
-var caml_pasta_fq_is_square = function (x) {
-    return caml_js_to_bool(plonk_wasm.caml_pasta_fq_is_square(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_is_square = tsBindings.caml_pasta_fq_is_square;
 
 // Provides: caml_pasta_fq_sqrt
-// Requires: plonk_wasm, caml_option_of_maybe_undefined
-var caml_pasta_fq_sqrt = function (x) {
-    return caml_option_of_maybe_undefined(plonk_wasm.caml_pasta_fq_sqrt(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_sqrt = tsBindings.caml_pasta_fq_sqrt;
 
 // Provides: caml_pasta_fq_of_int
-// Requires: plonk_wasm
-var caml_pasta_fq_of_int = plonk_wasm.caml_pasta_fq_of_int
+// Requires: tsBindings
+var caml_pasta_fq_of_int = tsBindings.caml_pasta_fq_of_int;
 
 // Provides: caml_pasta_fq_to_string
-// Requires: plonk_wasm, caml_string_of_jsstring
-var caml_pasta_fq_to_string = function (x) {
-    return caml_string_of_jsstring(plonk_wasm.caml_pasta_fq_to_string(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_to_string = tsBindings.caml_pasta_fq_to_string;
 
 // Provides: caml_pasta_fq_of_string
-// Requires: plonk_wasm, caml_jsstring_of_string
-var caml_pasta_fq_of_string = function (x) {
-    return plonk_wasm.caml_pasta_fq_of_string(caml_jsstring_of_string(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_of_string = tsBindings.caml_pasta_fq_of_string;
 
 // Provides: caml_pasta_fq_print
-// Requires: plonk_wasm
-var caml_pasta_fq_print = plonk_wasm.caml_pasta_fq_print
+// Requires: tsBindings
+var caml_pasta_fq_print = tsBindings.caml_pasta_fq_print;
 
 // Provides: caml_pasta_fq_mut_add
-// Requires: caml_pasta_fq_copy, caml_pasta_fq_add
-var caml_pasta_fq_mut_add = function (x, y) {
-    caml_pasta_fq_copy(x, caml_pasta_fq_add(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fq_mut_add = tsBindings.caml_pasta_fq_mut_add;
 
 // Provides: caml_pasta_fq_mut_sub
-// Requires: caml_pasta_fq_copy, caml_pasta_fq_sub
-var caml_pasta_fq_mut_sub = function (x, y) {
-    caml_pasta_fq_copy(x, caml_pasta_fq_sub(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fq_mut_sub = tsBindings.caml_pasta_fq_mut_sub;
 
 // Provides: caml_pasta_fq_mut_mul
-// Requires: caml_pasta_fq_copy, caml_pasta_fq_mul
-var caml_pasta_fq_mut_mul = function (x, y) {
-    caml_pasta_fq_copy(x, caml_pasta_fq_mul(x, y));
-};
+// Requires: tsBindings
+var caml_pasta_fq_mut_mul = tsBindings.caml_pasta_fq_mut_mul;
 
 // Provides: caml_pasta_fq_mut_square
-// Requires: caml_pasta_fq_copy, caml_pasta_fq_square
-var caml_pasta_fq_mut_square = function (x) {
-    caml_pasta_fq_copy(x, caml_pasta_fq_square(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_mut_square = tsBindings.caml_pasta_fq_mut_square;
 
 // Provides: caml_pasta_fq_compare
-// Requires: plonk_wasm
-var caml_pasta_fq_compare = plonk_wasm.caml_pasta_fq_compare
+// Requires: tsBindings
+var caml_pasta_fq_compare = tsBindings.caml_pasta_fq_compare;
 
 // Provides: caml_pasta_fq_equal
-// Requires: plonk_wasm
-var caml_pasta_fq_equal = plonk_wasm.caml_pasta_fq_equal
+// Requires: tsBindings
+var caml_pasta_fq_equal = tsBindings.caml_pasta_fq_equal;
 
 // Provides: caml_pasta_fq_random
-// Requires: plonk_wasm
-var caml_pasta_fq_random = plonk_wasm.caml_pasta_fq_random
+// Requires: tsBindings
+var caml_pasta_fq_random = tsBindings.caml_pasta_fq_random;
 
+// TODO
 // Provides: caml_pasta_fq_rng
-// Requires: plonk_wasm
-var caml_pasta_fq_rng = plonk_wasm.caml_pasta_fq_rng
+// Requires: tsBindings
+var caml_pasta_fq_rng = tsBindings.caml_pasta_fq_rng;
 
 // Provides: caml_pasta_fq_to_bigint
-// Requires: plonk_wasm
-var caml_pasta_fq_to_bigint = plonk_wasm.caml_pasta_fq_to_bigint
+// Requires: tsBindings
+var caml_pasta_fq_to_bigint = tsBindings.caml_pasta_fq_to_bigint;
 
 // Provides: caml_pasta_fq_of_bigint
-// Requires: plonk_wasm
-var caml_pasta_fq_of_bigint = plonk_wasm.caml_pasta_fq_of_bigint
+// Requires: tsBindings
+var caml_pasta_fq_of_bigint = tsBindings.caml_pasta_fq_of_bigint;
 
+// TODO
 // Provides: caml_pasta_fq_two_adic_root_of_unity
-// Requires: plonk_wasm
-var caml_pasta_fq_two_adic_root_of_unity = plonk_wasm.caml_pasta_fq_two_adic_root_of_unity
+// Requires: tsBindings
+var caml_pasta_fq_two_adic_root_of_unity = tsBindings.caml_pasta_fq_two_adic_root_of_unity;
 
 // Provides: caml_pasta_fq_domain_generator
-// Requires: plonk_wasm
-var caml_pasta_fq_domain_generator = plonk_wasm.caml_pasta_fq_domain_generator
+// Requires: tsBindings
+var caml_pasta_fq_domain_generator = tsBindings.caml_pasta_fq_domain_generator;
 
+// TODO
 // Provides: caml_pasta_fq_to_bytes
-// Requires: plonk_wasm, caml_bytes_of_uint8array
-var caml_pasta_fq_to_bytes = function (x) {
-    var res = plonk_wasm.caml_pasta_fq_to_bytes(x);
-    return caml_bytes_of_uint8array(plonk_wasm.caml_pasta_fq_to_bytes(x));
-};
+// Requires: tsBindings
+var caml_pasta_fq_to_bytes = tsBindings.caml_pasta_fq_to_bytes;
 
+// TODO
 // Provides: caml_pasta_fq_of_bytes
-// Requires: plonk_wasm, caml_bytes_to_uint8array
-var caml_pasta_fq_of_bytes = function (ocaml_bytes) {
-    return plonk_wasm.caml_pasta_fq_of_bytes(caml_bytes_to_uint8array(ocaml_bytes));
-};
+// Requires: tsBindings
+var caml_pasta_fq_of_bytes = tsBindings.caml_pasta_fq_of_bytes;
 
+// TODO
 // Provides: caml_pasta_fq_deep_copy
-// Requires: plonk_wasm
-var caml_pasta_fq_deep_copy = plonk_wasm.caml_pasta_fq_deep_copy
+// Requires: tsBindings
+var caml_pasta_fq_deep_copy = tsBindings.caml_pasta_fq_deep_copy;
 
 
 
@@ -513,7 +467,7 @@ var caml_fp_vector_get = function (v, i) {
     if (value === undefined) {
       throw Error('caml_fp_vector_get: Index out of bounds, got ' + i + '/' + (v.length - 1));
     }
-    return new joo_global_object.Uint8Array(value);
+    return value;
 }
 
 // Provides: caml_fp_vector_to_rust
@@ -554,7 +508,7 @@ var caml_fq_vector_get = function (v, i) {
     if (value === undefined) {
       throw Error('caml_fq_vector_get: Index out of bounds, got ' + i + '/' + (v.length - 1));
     }
-    return new joo_global_object.Uint8Array(value);
+    return value;
 }
 
 // Provides: caml_fq_vector_to_rust
