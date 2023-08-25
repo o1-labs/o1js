a3012b5fd7c3ba176c49dc7a399c8792e27d5117
Gregor <gregor.mitscha-baude@gmx.at>
2023-08-25 11:52:42 +0200

    extract field vector bindings

--- kimchi/js/bindings.js
+++ kimchi/js/bindings.js
@@ -59,63 +59,6 @@ var caml_option_to_maybe_undefined = function (x) {
 };
 
 
-
-// Provides: caml_fp_vector_create
-var caml_fp_vector_create = function () {
-    return [0]; // OCaml tag for arrays, so that we can use the same utility fns on both
-};
-
-// Provides: caml_fp_vector_length
-var caml_fp_vector_length = function (v) {
-    return v.length - 1;
-};
-
-// Provides: caml_fp_vector_emplace_back
-var caml_fp_vector_emplace_back = function (v, x) {
-    v.push(x);
-}
-
-// Provides: caml_fp_vector_get
-var caml_fp_vector_get = function (v, i) {
-    var value = v[i + 1];
-    if (value === undefined) {
-      throw Error('caml_fp_vector_get: Index out of bounds, got ' + i + '/' + (v.length - 1));
-    }
-    return value;
-}
-
-
-
-
-
-// Provides: caml_fq_vector_create
-var caml_fq_vector_create = function () {
-    return [0]; // OCaml tag for arrays, so that we can use the same utility fns on both
-};
-
-// Provides: caml_fq_vector_length
-var caml_fq_vector_length = function (v) {
-    return v.length - 1;
-};
-
-// Provides: caml_fq_vector_emplace_back
-var caml_fq_vector_emplace_back = function (v, x) {
-    v.push(x);
-}
-
-// Provides: caml_fq_vector_get
-var caml_fq_vector_get = function (v, i) {
-    var value = v[i + 1];
-    if (value === undefined) {
-      throw Error('caml_fq_vector_get: Index out of bounds, got ' + i + '/' + (v.length - 1));
-    }
-    return value;
-}
-
-
-
-
-
 // Provides: free_finalization_registry
 var free_finalization_registry =
     new joo_global_object.FinalizationRegistry(function (instance_representative) {
