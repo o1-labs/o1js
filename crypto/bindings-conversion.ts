833bf2d2433f1f767befb65f55fd30367d5cc862
Gregor <gregor.mitscha-baude@gmx.at>
2023-08-28 21:13:30 +0200

    oracles conversion

--- crypto/bindings-conversion.ts
+++ crypto/bindings-conversion.ts
@@ -160,6 +160,8 @@ function createRustConversion(wasm: wasm) {
     VerificationEvals,
     Shifts,
     VerifierIndex,
+    RandomOracles,
+    Oracles,
   }: WasmClasses) {
     let self = {
       vectorToRust: fieldsToRustFlat,
@@ -266,6 +268,30 @@ function createRustConversion(wasm: wasm) {
         vk.free();
         return mlVk;
       },
+
+      oraclesToRust(oracles: Oracles): WasmOracles {
+        let [, o, pEval, openingPrechallenges, digestBeforeEvaluations] =
+          oracles;
+        return new Oracles(
+          randomOraclesToRust(o),
+          fieldToRust(pEval[1]),
+          fieldToRust(pEval[2]),
+          fieldsToRustFlat(openingPrechallenges),
+          fieldToRust(digestBeforeEvaluations)
+        );
+      },
+      oraclesFromRust(oracles: WasmOracles): Oracles {
+        let mlOracles: Oracles = [
+          0,
+          randomOraclesFromRust(oracles.o),
+          [0, fieldFromRust(oracles.p_eval0), fieldFromRust(oracles.p_eval1)],
+          fieldsFromRustFlat(oracles.opening_prechallenges),
+          fieldFromRust(oracles.digest_before_evaluations),
+        ];
+        // TODO: do we not want to free?
+        // oracles.free();
+        return mlOracles;
+      },
     };
 
     function domainToRust([, logSizeOfGroup, groupGen]: Domain): WasmDomain {
@@ -317,6 +343,66 @@ function createRustConversion(wasm: wasm) {
       evals.free();
       return mlEvals;
     }
+
+    function randomOraclesToRust(ro: RandomOracles): WasmRandomOracles {
+      let jointCombinerMl = MlOption.from(ro[1]);
+      let jointCombinerChal = maybeFieldToRust(jointCombinerMl?.[1][1]);
+      let jointCombiner = maybeFieldToRust(jointCombinerMl?.[2]);
+      let beta = fieldToRust(ro[2]);
+      let gamma = fieldToRust(ro[3]);
+      let alphaChal = fieldToRust(ro[4][1]);
+      let alpha = fieldToRust(ro[5]);
+      let zeta = fieldToRust(ro[6]);
+      let v = fieldToRust(ro[7]);
+      let u = fieldToRust(ro[8]);
+      let zetaChal = fieldToRust(ro[9][1]);
+      let vChal = fieldToRust(ro[10][1]);
+      let uChal = fieldToRust(ro[11][1]);
+      return new RandomOracles(
+        jointCombinerChal,
+        jointCombiner,
+        beta,
+        gamma,
+        alphaChal,
+        alpha,
+        zeta,
+        v,
+        u,
+        zetaChal,
+        vChal,
+        uChal
+      );
+    }
+    function randomOraclesFromRust(ro: WasmRandomOracles): RandomOracles {
+      let jointCombinerChal = ro.joint_combiner_chal;
+      let jointCombiner = ro.joint_combiner;
+      let jointCombinerOption = MlOption<[0, ScalarChallenge, Field]>(
+        jointCombinerChal &&
+          jointCombiner && [
+            0,
+            [0, fieldFromRust(jointCombinerChal)],
+            fieldFromRust(jointCombiner),
+          ]
+      );
+      let mlRo: RandomOracles = [
+        0,
+        jointCombinerOption,
+        fieldFromRust(ro.beta),
+        fieldFromRust(ro.gamma),
+        [0, fieldFromRust(ro.alpha_chal)],
+        fieldFromRust(ro.alpha),
+        fieldFromRust(ro.zeta),
+        fieldFromRust(ro.v),
+        fieldFromRust(ro.u),
+        [0, fieldFromRust(ro.zeta_chal)],
+        [0, fieldFromRust(ro.v_chal)],
+        [0, fieldFromRust(ro.u_chal)],
+      ];
+      // TODO: do we not want to free?
+      // ro.free();
+      return mlRo;
+    }
+
     return self;
   }
 
@@ -395,6 +481,10 @@ function fieldsFromRustFlat(fieldBytes: Uint8Array): MlArray<Field> {
     fields[i] = fieldFromRust(fieldView);
   }
   return [0, ...fields];
+}
+
+function maybeFieldToRust(x?: Field): Uint8Array | undefined {
+  return x && fieldToRust(x);
 }
 
 // affine
