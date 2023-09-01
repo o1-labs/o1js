effa5698595c80e0cb6e2079237abf00cec517f7
Gregor <gregor.mitscha-baude@gmx.at>
2023-09-01 11:44:48 +0200

    minor integration test fixes

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -3,9 +3,9 @@ set -e
 
 ./run src/examples/zkapps/hello_world/run.ts --bundle
 ./run src/examples/zkapps/voting/run.ts --bundle
-./run src/examples/simple_zkapp.ts
-./run src/examples/zkapps/reducer/reducer_composite.ts
-./run src/examples/zkapps/composability.ts
+./run src/examples/simple_zkapp.ts --bundle
+./run src/examples/zkapps/reducer/reducer_composite.ts --bundle
+./run src/examples/zkapps/composability.ts --bundle
 ./run src/examples/zkapps/dex/run.ts --bundle
-./run src/examples/zkapps/dex/happy-path-with-proofs.ts --bundle
+./run src/examples/zkapps/dex/happy-path-with-actions.ts --bundle
 ./run src/examples/zkapps/dex/upgradability.ts --bundle
