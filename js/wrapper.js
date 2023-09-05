f64b0db23d092a0434e9b0b4bc2fe7838bbec6ec
Gregor <gregor.mitscha-baude@gmx.at>
2023-09-05 10:16:31 +0200

    get jest to work with new bindings

--- js/wrapper.js
+++ js/wrapper.js
@@ -1,9 +1,12 @@
 import { wasm, withThreadPool } from './node/node-backend.js';
-import { default as snarky } from '../compiled/_node_bindings/snarky_js_node.bc.cjs';
+
+// this dynamic import makes jest respect the import order
+// otherwise the cjs file gets imported before its implicit esm dependencies and fails
+let snarky = await import('../compiled/_node_bindings/snarky_js_node.bc.cjs');
 
 export { getSnarky, getWasm, withThreadPool };
 
-let getSnarky = () => snarky;
+let getSnarky = () => snarky.default;
 
 function getWasm() {
   return wasm;
