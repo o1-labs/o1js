d99bb43cb5f24e0cf4a4a2c6d996048a4e4481e2
Gregor <gregor.mitscha-baude@gmx.at>
2023-09-05 11:12:58 +0200

    handle commonjs case using esbuild label

--- js/wrapper.js
+++ js/wrapper.js
@@ -1,12 +1,20 @@
 import { wasm, withThreadPool } from './node/node-backend.js';
 
+let snarky;
+
 // this dynamic import makes jest respect the import order
 // otherwise the cjs file gets imported before its implicit esm dependencies and fails
-let snarky = await import('../compiled/_node_bindings/snarky_js_node.bc.cjs');
+if (typeof require !== 'undefined') {
+  snarky = require('../compiled/_node_bindings/snarky_js_node.bc.cjs');
+} else {
+  ESM: snarky = (
+    await import('../compiled/_node_bindings/snarky_js_node.bc.cjs')
+  ).default;
+}
 
 export { getSnarky, getWasm, withThreadPool };
 
-let getSnarky = () => snarky.default;
+let getSnarky = () => snarky;
 
 function getWasm() {
   return wasm;
