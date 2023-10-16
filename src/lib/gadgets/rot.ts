b8cc337dab014b4beb24673cb792a99b3f1ccec6
Martin Minkov <minkovlmartin@gmail.com>
2023-10-16 15:46:51 -0700

    refactor(rot.ts): replace hardcoded value 64 with constant MAX_BITS

--- src/lib/gadgets/rot.ts
+++ src/lib/gadgets/rot.ts
@@ -8,7 +8,7 @@ const MAX_BITS = 64 as const;
 
 function rot(word: Field, bits: number, direction: 'left' | 'right' = 'left') {
   // Check that the rotation bits are in range
-  if (bits < 0 || bits > 64) {
+  if (bits < 0 || bits > MAX_BITS) {
     throw Error(`rot64: expected bits to be between 0 and 64, got ${bits}`);
   }
 
