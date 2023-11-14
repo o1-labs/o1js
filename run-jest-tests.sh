7e9529c0685098347ee0f51233cda205b4e8e3c4
Martin Minkov <minkovlmartin@gmail.com>
2023-11-14 12:55:13 -0800

    refactor(run-jest-tests.sh): remove condition excluding 'src/mina' from jest tests This change was made because the 'snarkyjs' inside the mina repo has been removed, making the condition unnecessary.

--- run-jest-tests.sh
+++ run-jest-tests.sh
@@ -3,8 +3,5 @@ set -e
 shopt -s globstar # to expand '**' into nested directories
 
 for f in ./src/**/*.test.ts; do
-  # TODO: Remove this once we remove the `snarkyjs` inside the mina repo
-  if [[ $f != *"src/mina"* ]]; then
-    NODE_OPTIONS=--experimental-vm-modules npx jest $f;
-  fi
+  NODE_OPTIONS=--experimental-vm-modules npx jest $f;
 done
