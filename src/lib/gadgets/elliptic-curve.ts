6c34c9f4074da828003e2595c778def85c7a320e
Gregor <gregor.mitscha-baude@gmx.at>
2023-11-15 16:37:33 +0100

    double

--- src/lib/gadgets/elliptic-curve.ts
+++ src/lib/gadgets/elliptic-curve.ts
@@ -5,6 +5,7 @@ import { Provable } from '../provable.js';
 import { exists } from './common.js';
 import {
   Field3,
+  ForeignField,
   Sum,
   assertRank1,
   bigint3,
@@ -40,9 +41,9 @@ function add({ x: x1, y: y1 }: Point, { x: x2, y: y2 }: Point, f: bigint) {
   multiRangeCheck(m);
   multiRangeCheck(x3);
   multiRangeCheck(y3);
-  let m2Bound = weakBound(m[2], f);
+  let mBound = weakBound(m[2], f);
   let x3Bound = weakBound(x3[2], f);
-  // we dont need to bounds-check y3[2] because it's never one of the inputs to a multiplication
+  // we dont need to bound y3[2] because it's never one of the inputs to a multiplication
 
   // (x1 - x2)*m = y1 - y2
   let deltaX = new Sum(x1).sub(x2);
@@ -59,11 +60,61 @@ function add({ x: x1, y: y1 }: Point, { x: x2, y: y2 }: Point, f: bigint) {
   let qBound3 = assertRank1(deltaX1X3, m, ySum, f);
 
   // bounds checks
-  multiRangeCheck([m2Bound, x3Bound, qBound1]);
+  multiRangeCheck([mBound, x3Bound, qBound1]);
   multiRangeCheck([qBound2, qBound3, Field.from(0n)]);
 }
 
-let cs = Provable.constraintSystem(() => {
+function double({ x: x1, y: y1 }: Point, f: bigint) {
+  // TODO constant case
+
+  // witness and range-check slope, x3, y3
+  let witnesses = exists(9, () => {
+    let [x1_, y1_] = Field3.toBigints(x1, y1);
+    let denom = inverse(mod(2n * y1_, f), f);
+
+    let m = denom !== undefined ? mod(3n * mod(x1_ ** 2n, f) * denom, f) : 0n;
+    let m2 = mod(m * m, f);
+    let x3 = mod(m2 - 2n * x1_, f);
+    let y3 = mod(m * (x1_ - x3) - y1_, f);
+
+    return [...split(m), ...split(x3), ...split(y3)];
+  });
+  let [m0, m1, m2, x30, x31, x32, y30, y31, y32] = witnesses;
+  let m: Field3 = [m0, m1, m2];
+  let x3: Field3 = [x30, x31, x32];
+  let y3: Field3 = [y30, y31, y32];
+
+  multiRangeCheck(m);
+  multiRangeCheck(x3);
+  multiRangeCheck(y3);
+  let mBound = weakBound(m[2], f);
+  let x3Bound = weakBound(x3[2], f);
+  // we dont need to bound y3[2] because it's never one of the inputs to a multiplication
+
+  // x1^2 = x1x1
+  let x1x1 = ForeignField.mul(x1, x1, f);
+
+  // 2*y1*m = 3*x1x1
+  // TODO this assumes the curve has a == 0
+  let y1Times2 = new Sum(y1).add(y1);
+  let x1x1Times3 = new Sum(x1x1).add(x1x1).add(x1x1);
+  let qBound1 = assertRank1(y1Times2, m, x1x1Times3, f);
+
+  // m^2 = 2*x1 + x3
+  let xSum = new Sum(x1).add(x1).add(x3);
+  let qBound2 = assertRank1(m, m, xSum, f);
+
+  // (x1 - x3)*m = y1 + y3
+  let deltaX1X3 = new Sum(x1).sub(x3);
+  let ySum = new Sum(y1).add(y3);
+  let qBound3 = assertRank1(deltaX1X3, m, ySum, f);
+
+  // bounds checks
+  multiRangeCheck([mBound, x3Bound, qBound1]);
+  multiRangeCheck([qBound2, qBound3, Field.from(0n)]);
+}
+
+let csAdd = Provable.constraintSystem(() => {
   let x1 = Provable.witness(Field3.provable, () => Field3.from(0n));
   let x2 = Provable.witness(Field3.provable, () => Field3.from(0n));
   let y1 = Provable.witness(Field3.provable, () => Field3.from(0n));
@@ -75,5 +126,17 @@ let cs = Provable.constraintSystem(() => {
   add(g, h, exampleFields.secp256k1.modulus);
 });
 
-printGates(cs.gates);
-console.log({ digest: cs.digest, rows: cs.rows });
+let csDouble = Provable.constraintSystem(() => {
+  let x1 = Provable.witness(Field3.provable, () => Field3.from(0n));
+  let y1 = Provable.witness(Field3.provable, () => Field3.from(0n));
+
+  let g = { x: x1, y: y1 };
+
+  double(g, exampleFields.secp256k1.modulus);
+});
+
+printGates(csAdd.gates);
+console.log({ digest: csAdd.digest, rows: csAdd.rows });
+
+printGates(csDouble.gates);
+console.log({ digest: csDouble.digest, rows: csDouble.rows });
