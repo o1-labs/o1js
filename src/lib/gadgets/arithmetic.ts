8c524b678b1cc3eccd3c4dde0b17a0d5d81c8949
Florian <florian@technotro.com>
2023-11-21 12:49:25 +0100

    add divMod32, addMod32

---
+++ src/lib/gadgets/arithmetic.ts
@@ -0,0 +1,40 @@
+import { Field } from '../core.js';
+import { Provable } from '../provable.js';
+import { rangeCheck32 } from './range-check.js';
+
+export { divMod32, addMod32 };
+
+function divMod32(n: Field) {
+  if (n.isConstant()) {
+    let nBigInt = n.toBigInt();
+    let q = nBigInt / (1n << 32n);
+    let r = nBigInt - q * (1n << 32n);
+    return {
+      remainder: new Field(r),
+      quotient: new Field(q),
+    };
+  }
+
+  let qr = Provable.witness(Provable.Array(Field, 2), () => {
+    let nBigInt = n.toBigInt();
+    let q = nBigInt / (1n << 32n);
+    let r = nBigInt - q * (1n << 32n);
+    return [new Field(q), new Field(r)];
+  });
+  let [q, r] = qr;
+
+  // I think we can "skip" this here and do it in the caller - see rotate32
+  // rangeCheck32(q);
+  // rangeCheck32(r);
+
+  n.assertEquals(q.mul(1n << 32n).add(r));
+
+  return {
+    remainder: r,
+    quotient: q,
+  };
+}
+
+function addMod32(x: Field, y: Field) {
+  return divMod32(x.add(y)).remainder;
+}
