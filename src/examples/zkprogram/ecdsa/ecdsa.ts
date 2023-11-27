89f7d01050c2cbdcf2c23183caac4ec5201914cc
Gregor <gregor.mitscha-baude@gmx.at>
2023-11-27 13:21:28 +0100

    finish example

--- src/examples/zkprogram/ecdsa/ecdsa.ts
+++ src/examples/zkprogram/ecdsa/ecdsa.ts
@@ -1,30 +1,39 @@
-import { Gadgets, ZkProgram, Struct } from 'o1js';
+import { Gadgets, ZkProgram, Struct, Crypto } from 'o1js';
 
-export { ecdsaProgram };
+export { ecdsaProgram, Point, Secp256k1 };
 
 let { ForeignField, Field3, Ecdsa } = Gadgets;
 
-type PublicKey = { x: Gadgets.Field3; y: Gadgets.Field3 };
-const PublicKey = Struct({ x: Field3.provable, y: Field3.provable });
+// TODO expose this as part of Gadgets.Curve
+
+class Point extends Struct({ x: Field3.provable, y: Field3.provable }) {
+  // point from bigints
+  static from({ x, y }: { x: bigint; y: bigint }) {
+    return new Point({ x: Field3.from(x), y: Field3.from(y) });
+  }
+}
+
+const Secp256k1 = Crypto.createCurve(Crypto.CurveParams.Secp256k1);
 
 const ecdsaProgram = ZkProgram({
   name: 'ecdsa',
-  publicInput: PublicKey,
+  publicInput: Point,
 
   methods: {
     verifyEcdsa: {
       privateInputs: [Ecdsa.Signature.provable, Field3.provable],
       method(
-        publicKey: PublicKey,
+        publicKey: Point,
         signature: Gadgets.Ecdsa.Signature,
         msgHash: Gadgets.Field3
       ) {
         // assert that private inputs are valid
         ForeignField.assertAlmostFieldElements(
           [signature.r, signature.s, msgHash],
-          0n
+          Secp256k1.order
         );
 
+        // verify signature
         Ecdsa.verify(Secp256k1, signature, msgHash, publicKey);
       },
     },
---
+++ src/examples/zkprogram/ecdsa/run.ts
@@ -0,0 +1,38 @@
+import { Gadgets } from 'o1js';
+import { Point, Secp256k1, ecdsaProgram } from './ecdsa.js';
+import assert from 'assert';
+
+// create an example ecdsa signature
+let privateKey = Secp256k1.Scalar.random();
+let publicKey = Secp256k1.scale(Secp256k1.one, privateKey);
+
+// TODO make this use an actual keccak hash
+let messageHash = Secp256k1.Scalar.random();
+
+let signature = Gadgets.Ecdsa.sign(Secp256k1, messageHash, privateKey);
+
+console.time('ecdsa verify (build constraint system)');
+let cs = ecdsaProgram.analyzeMethods().verifyEcdsa;
+console.timeEnd('ecdsa verify (build constraint system)');
+
+let gateTypes: Record<string, number> = {};
+gateTypes['Total rows'] = cs.rows;
+for (let gate of cs.gates) {
+  gateTypes[gate.type] ??= 0;
+  gateTypes[gate.type]++;
+}
+console.log(gateTypes);
+
+console.time('ecdsa verify (compile)');
+await ecdsaProgram.compile();
+console.timeEnd('ecdsa verify (compile)');
+
+console.time('ecdsa verify (prove)');
+let proof = await ecdsaProgram.verifyEcdsa(
+  Point.from(publicKey),
+  Gadgets.Ecdsa.Signature.from(signature),
+  Gadgets.Field3.from(messageHash)
+);
+console.timeEnd('ecdsa verify (prove)');
+
+assert(await ecdsaProgram.verify(proof), 'proof verifies');
