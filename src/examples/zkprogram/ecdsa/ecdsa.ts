b35cc616019f1fdbb365c70c423006d1ab4f737c
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-04 16:20:21 +0100

    rewrite ecdsa example using high level API

--- src/examples/zkprogram/ecdsa/ecdsa.ts
+++ src/examples/zkprogram/ecdsa/ecdsa.ts
@@ -1,40 +1,21 @@
-import { Gadgets, ZkProgram, Struct, Crypto } from 'o1js';
+import { ZkProgram, Crypto, createEcdsa, createForeignCurve, Bool } from 'o1js';
 
-export { ecdsaProgram, Point, Secp256k1 };
+export { ecdsaProgram, Secp256k1, Ecdsa };
 
-let { ForeignField, Field3, Ecdsa } = Gadgets;
-
-// TODO expose this as part of Gadgets.Curve
-
-class Point extends Struct({ x: Field3.provable, y: Field3.provable }) {
-  // point from bigints
-  static from({ x, y }: { x: bigint; y: bigint }) {
-    return new Point({ x: Field3.from(x), y: Field3.from(y) });
-  }
-}
-
-const Secp256k1 = Crypto.createCurve(Crypto.CurveParams.Secp256k1);
+class Secp256k1 extends createForeignCurve(Crypto.CurveParams.Secp256k1) {}
+class Scalar extends Secp256k1.Scalar {}
+class Ecdsa extends createEcdsa(Secp256k1) {}
 
 const ecdsaProgram = ZkProgram({
   name: 'ecdsa',
-  publicInput: Point,
+  publicInput: Scalar.provable,
+  publicOutput: Bool,
 
   methods: {
     verifyEcdsa: {
-      privateInputs: [Ecdsa.Signature.provable, Field3.provable],
-      method(
-        publicKey: Point,
-        signature: Gadgets.Ecdsa.Signature,
-        msgHash: Gadgets.Field3
-      ) {
-        // assert that private inputs are valid
-        ForeignField.assertAlmostReduced(
-          [signature.r, signature.s, msgHash],
-          Secp256k1.order
-        );
-
-        // verify signature
-        Ecdsa.verify(Secp256k1, signature, msgHash, publicKey);
+      privateInputs: [Ecdsa.provable, Secp256k1.provable],
+      method(msgHash: Scalar, signature: Ecdsa, publicKey: Secp256k1) {
+        return signature.verify(msgHash, publicKey);
       },
     },
   },
