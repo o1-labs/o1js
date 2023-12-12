25bd83bc2a8053efe3c09dc2cbef119d84de1019
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-12 15:22:16 +0100

    add keccak to ecdsa circuit

--- src/lib/foreign-ecdsa.ts
+++ src/lib/foreign-ecdsa.ts
@@ -8,9 +8,12 @@ import {
   toPoint,
 } from './foreign-curve.js';
 import { AlmostForeignField } from './foreign-field.js';
-import { assert } from './gadgets/common.js';
+import { assert, witnessSlice } from './gadgets/common.js';
 import { Field3 } from './gadgets/foreign-field.js';
 import { Ecdsa } from './gadgets/elliptic-curve.js';
+import { Field } from './field.js';
+import { TupleN } from './util/types.js';
+import { rangeCheck64 } from './gadgets/range-check.js';
 
 // external API
 export { createEcdsa, EcdsaSignature };
@@ -90,11 +93,14 @@ class EcdsaSignature {
    * let sig = Provable.witness(Ecdsa.provable, () => signature);
    *
    * // verify signature
-   * let isValid = sig.verify(msgHash, pk);
+   * let isValid = sig.verifySignedHash(msgHash, pk);
    * isValid.assertTrue('signature verifies');
    * ```
    */
-  verify(msgHash: AlmostForeignField | bigint, publicKey: FlexiblePoint) {
+  verifySignedHash(
+    msgHash: AlmostForeignField | bigint,
+    publicKey: FlexiblePoint
+  ) {
     let msgHash_ = this.Constructor.Curve.Scalar.from(msgHash);
     let publicKey_ = this.Constructor.Curve.from(publicKey);
     return Ecdsa.verify(
@@ -169,4 +175,72 @@ function createEcdsa(
 
 function toObject(signature: EcdsaSignature) {
   return { r: signature.r.value, s: signature.s.value };
+}
+
+/**
+ * Provable method to convert keccak256 hash output to ECDSA scalar = "message hash"
+ *
+ * Spec from [Wikipedia](https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm):
+ *
+ * > Let z be the L_n leftmost bits of e, where L_{n} is the bit length of the group order n.
+ * > (Note that z can be greater than n but not longer.)
+ *
+ * The output z is used as input to a multiplication:
+ *
+ * > Calculate u_1 = z s^(-1) mod n ...
+ *
+ * That means we don't need to reduce z mod n: The fact that it has bitlength <= n makes it
+ * almost reduced which is enough for the multiplication to be correct.
+ * (using a weaker notion of "almost reduced" than what we usually prove, but sufficient for all uses of it: `z < 2^ceil(log(n))`)
+ *
+ * In summary, this method:
+ * - takes 256 bits in 4x64 form
+ * - converts them to 3 limbs which collectively have L_n <= 256 bits,
+ *    by dropping the higher bits.
+ */
+function keccakOutputToLimbs(
+  hash: TupleN<Field, 4>,
+  Curve: typeof ForeignCurve
+) {
+  const L_n = Curve.Scalar.sizeInBits;
+  // keep it simple, avoid dealing with bits dropped from words other than the highest
+  assert(L_n > 3 * 64, `Scalar sizes ${L_n} <= ${3 * 64} not supported`);
+
+  // TODO confirm endianness
+  let [w0, w1, w2, w3] = hash;
+
+  // split w1 and w2 along the 88 bit boundaries
+  let [w10, w11] = split64(w1, 24); // 24 = 88 - 64; 40 = 64 - 24
+  let [w20, w21] = split64(w2, 48); // 48 = 88 - 40; 16 = 64 - 48
+
+  // if L_n < 256, drop higher part of w3 so that the total length is L_n
+  if (L_n < 256) {
+    let [w30] = split64(w3, L_n - 3 * 64);
+    w3 = w30;
+  }
+
+  // piece together into limbs
+  let x0 = w0.add(w10.mul(1n << 64n));
+  let x1 = w11.add(w20.mul(1n << 40n));
+  let x2 = w21.add(w3.mul(1n << 16n));
+
+  return new Curve.Scalar.AlmostReduced([x0, x1, x2]);
+}
+
+// split 64-bit field into two pieces of lengths n and 64-n
+function split64(x: Field, n: number) {
+  let x0 = witnessSlice(x, 0, n);
+  let x1 = witnessSlice(x, n, 64);
+
+  // prove decomposition
+  let nn = BigInt(n);
+  x0.add(x1.mul(1n << nn)).assertEquals(x);
+
+  // prove ranges: x0 in [0, 2^n), x1 in [0, 2^(64-n))
+  rangeCheck64(x0);
+  rangeCheck64(x0.mul(1n << (64n - nn)));
+  rangeCheck64(x1);
+  // note: x1 < 2^(64-n) is implied by x0 + x1 * 2^n = x < 2^64
+
+  return [x0, x1];
 }
