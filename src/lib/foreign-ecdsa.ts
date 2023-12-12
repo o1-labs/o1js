a5ca0b396174f0e3b6647880706c74adc3f81f80
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-12 16:32:54 +0100

    change conversion to take bytes

--- src/lib/foreign-ecdsa.ts
+++ src/lib/foreign-ecdsa.ts
@@ -8,12 +8,11 @@ import {
   toPoint,
 } from './foreign-curve.js';
 import { AlmostForeignField } from './foreign-field.js';
-import { assert, witnessSlice } from './gadgets/common.js';
+import { assert } from './gadgets/common.js';
 import { Field3 } from './gadgets/foreign-field.js';
 import { Ecdsa } from './gadgets/elliptic-curve.js';
 import { Field } from './field.js';
-import { TupleN } from './util/types.js';
-import { rangeCheck64 } from './gadgets/range-check.js';
+import { l } from './gadgets/range-check.js';
 
 // external API
 export { createEcdsa, EcdsaSignature };
@@ -193,54 +192,34 @@ function toObject(signature: EcdsaSignature) {
  * almost reduced which is enough for the multiplication to be correct.
  * (using a weaker notion of "almost reduced" than what we usually prove, but sufficient for all uses of it: `z < 2^ceil(log(n))`)
  *
- * In summary, this method:
- * - takes 256 bits in 4x64 form
- * - converts them to 3 limbs which collectively have L_n <= 256 bits,
- *    by dropping the higher bits.
+ * In summary, this method just:
+ * - takes a 32 bytes hash
+ * - converts them to 3 limbs which collectively have L_n <= 256 bits
  */
-function keccakOutputToLimbs(
-  hash: TupleN<Field, 4>,
-  Curve: typeof ForeignCurve
-) {
+function keccakOutputToLimbs(hash: Field[], Curve: typeof ForeignCurve) {
   const L_n = Curve.Scalar.sizeInBits;
-  // keep it simple, avoid dealing with bits dropped from words other than the highest
-  assert(L_n > 3 * 64, `Scalar sizes ${L_n} <= ${3 * 64} not supported`);
-
-  // TODO confirm endianness
-  let [w0, w1, w2, w3] = hash;
-
-  // split w1 and w2 along the 88 bit boundaries
-  let [w10, w11] = split64(w1, 24); // 24 = 88 - 64; 40 = 64 - 24
-  let [w20, w21] = split64(w2, 48); // 48 = 88 - 40; 16 = 64 - 48
-
-  // if L_n < 256, drop higher part of w3 so that the total length is L_n
-  if (L_n < 256) {
-    let [w30] = split64(w3, L_n - 3 * 64);
-    w3 = w30;
-  }
+  // keep it simple for now, avoid dealing with dropping bits
+  // TODO: what does "leftmost bits" mean? big-endian or little-endian?
+  // @noble/curves uses a right shift, dropping the least significant bits:
+  // https://github.com/paulmillr/noble-curves/blob/4007ee975bcc6410c2e7b504febc1d5d625ed1a4/src/abstract/weierstrass.ts#L933
+  assert(L_n === 256, `Scalar sizes ${L_n} !== 256 not supported`);
+  assert(hash.length === 32, `hash length ${hash.length} !== 32 not supported`);
 
   // piece together into limbs
-  let x0 = w0.add(w10.mul(1n << 64n));
-  let x1 = w11.add(w20.mul(1n << 40n));
-  let x2 = w21.add(w3.mul(1n << 16n));
+  // bytes are big-endian, so the first byte is the most significant
+  assert(l === 88n);
+  let x2 = bytesToLimbBE(hash.slice(0, 10));
+  let x1 = bytesToLimbBE(hash.slice(10, 21));
+  let x0 = bytesToLimbBE(hash.slice(21, 32));
 
   return new Curve.Scalar.AlmostReduced([x0, x1, x2]);
 }
 
-// split 64-bit field into two pieces of lengths n and 64-n
-function split64(x: Field, n: number) {
-  let x0 = witnessSlice(x, 0, n);
-  let x1 = witnessSlice(x, n, 64);
-
-  // prove decomposition
-  let nn = BigInt(n);
-  x0.add(x1.mul(1n << nn)).assertEquals(x);
-
-  // prove ranges: x0 in [0, 2^n), x1 in [0, 2^(64-n))
-  rangeCheck64(x0);
-  rangeCheck64(x0.mul(1n << (64n - nn)));
-  rangeCheck64(x1);
-  // note: x1 < 2^(64-n) is implied by x0 + x1 * 2^n = x < 2^64
-
-  return [x0, x1];
+function bytesToLimbBE(bytes: Field[]) {
+  let n = bytes.length;
+  let limb = bytes[0];
+  for (let i = 1; i < n; i++) {
+    limb = limb.mul(1n << 8n).add(bytes[i]);
+  }
+  return limb.seal();
 }
