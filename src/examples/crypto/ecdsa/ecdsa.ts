b7ecd5be709c9f06d3cec91bb718286439202ce7
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-12 17:12:23 +0100

    use keccak in example

--- src/examples/crypto/ecdsa/ecdsa.ts
+++ src/examples/crypto/ecdsa/ecdsa.ts
@@ -1,21 +1,54 @@
-import { ZkProgram, Crypto, createEcdsa, createForeignCurve, Bool } from 'o1js';
+import assert from 'assert';
+import {
+  ZkProgram,
+  Crypto,
+  createEcdsa,
+  createForeignCurve,
+  Bool,
+  Struct,
+  Provable,
+  Field,
+} from 'o1js';
 
-export { ecdsaProgram, Secp256k1, Ecdsa };
+export { keccakAndEcdsa, ecdsa, Secp256k1, Ecdsa, Message };
 
 class Secp256k1 extends createForeignCurve(Crypto.CurveParams.Secp256k1) {}
 class Scalar extends Secp256k1.Scalar {}
 class Ecdsa extends createEcdsa(Secp256k1) {}
 
-const ecdsaProgram = ZkProgram({
+// a message of 8 bytes
+class Message extends Struct({ array: Provable.Array(Field, 8) }) {
+  static from(message: Uint8Array) {
+    assert(message.length === 8, 'message must be 8 bytes');
+    return new Message({ array: [...message].map(Field) });
+  }
+}
+
+const keccakAndEcdsa = ZkProgram({
   name: 'ecdsa',
+  publicInput: Message,
+  publicOutput: Bool,
+
+  methods: {
+    verify: {
+      privateInputs: [Ecdsa.provable, Secp256k1.provable],
+      method(message: Message, signature: Ecdsa, publicKey: Secp256k1) {
+        return signature.verify(message.array, publicKey);
+      },
+    },
+  },
+});
+
+const ecdsa = ZkProgram({
+  name: 'ecdsa-only',
   publicInput: Scalar.provable,
   publicOutput: Bool,
 
   methods: {
-    verifyEcdsa: {
+    verifySignedHash: {
       privateInputs: [Ecdsa.provable, Secp256k1.provable],
-      method(msgHash: Scalar, signature: Ecdsa, publicKey: Secp256k1) {
-        return signature.verify(msgHash, publicKey);
+      method(message: Scalar, signature: Ecdsa, publicKey: Secp256k1) {
+        return signature.verifySignedHash(message, publicKey);
       },
     },
   },
