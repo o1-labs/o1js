6ea442bd364cd81cec17946c77efcea97e757a88
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-18 10:42:45 +0100

    further tweaks to packed

--- src/lib/provable-types/packed.ts
+++ src/lib/provable-types/packed.ts
@@ -1,43 +1,39 @@
+import { provableFromClass } from '../../bindings/lib/provable-snarky.js';
 import {
   HashInput,
-  InferProvable,
   ProvableExtended,
   Unconstrained,
-  provable,
 } from '../circuit_value.js';
 import { Field } from '../field.js';
+import { assert } from '../gadgets/common.js';
 import { Poseidon, packToFields } from '../hash.js';
 import { Provable } from '../provable.js';
 import { fields } from './fields.js';
 
-export { provablePacked, Packed };
+export { Packed };
 
 class Packed<T> {
   packed: Field[];
   value: Unconstrained<T>;
-  type: ProvableExtended<T>;
 
-  private constructor(
-    packed: Field[],
-    value: Unconstrained<T>,
-    type: ProvableExtended<T>
-  ) {
+  constructor(packed: Field[], value: Unconstrained<T>) {
     this.packed = packed;
     this.value = value;
-    this.type = type;
   }
 
-  static pack<T>(type: ProvableExtended<T>, x: T): Packed<T> {
-    let input = type.toInput(x);
+  static pack<T>(x: T): Packed<T> {
+    let input = this.innerProvable.toInput(x);
     let packed = packToFields(input);
-    return new Packed(packed, Unconstrained.from(x), type);
+    return new this(packed, Unconstrained.from(x));
   }
 
   unpack(): T {
-    let value = Provable.witness(this.type, () => this.value.get());
+    let value = Provable.witness(this.Constructor.innerProvable, () =>
+      this.value.get()
+    );
 
     // prove that the value packs to the packed fields
-    let input = this.type.toInput(value);
+    let input = this.Constructor.innerProvable.toInput(value);
     let packed = packToFields(input);
     for (let i = 0; i < this.packed.length; i++) {
       this.packed[i].assertEquals(packed[i]);
@@ -53,24 +49,44 @@ class Packed<T> {
   hash() {
     return Poseidon.hash(this.packed);
   }
-}
 
-type PackedBase<T> = { packed: Field[]; value: Unconstrained<T> };
+  static createProvable<T>(
+    type: ProvableExtended<T>
+  ): ProvableHashable<Packed<T>> {
+    let input = type.toInput(type.empty());
+    let packedSize = countFields(input);
+
+    return provableFromClass(this, {
+      packed: fields(packedSize),
+      value: Unconstrained.provable,
+    }) as ProvableHashable<Packed<T>>;
+  }
+
+  static _provable: ProvableHashable<Packed<any>> | undefined;
+  static _innerProvable: ProvableExtended<any> | undefined;
+
+  get Constructor(): typeof Packed {
+    return this.constructor as typeof Packed;
+  }
+
+  static get provable(): ProvableHashable<Packed<any>> {
+    assert(this._provable !== undefined, 'Packed not initialized');
+    return this._provable;
+  }
+  static get innerProvable(): ProvableExtended<any> {
+    assert(this._innerProvable !== undefined, 'Packed not initialized');
+    return this._innerProvable;
+  }
+
+  static create<T>(type: ProvableExtended<T>): typeof Packed<T> {
+    return class Packed_ extends Packed<T> {
+      static _provable = Packed_.createProvable(type);
+      static _innerProvable = type;
+    };
+  }
+}
 
 type ProvableHashable<T> = Provable<T> & { toInput: (x: T) => HashInput };
-
-function provablePacked<A extends ProvableExtended<any>>(
-  type: A
-): ProvableHashable<PackedBase<InferProvable<A>>> {
-  // compute packed size
-  let input = type.toInput(type.empty());
-  let packedSize = countFields(input);
-
-  return provable({
-    packed: fields(packedSize),
-    value: Unconstrained.provable,
-  });
-}
 
 function countFields(input: HashInput) {
   let n = input.fields?.length ?? 0;
