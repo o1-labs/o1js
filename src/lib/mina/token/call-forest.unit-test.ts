cde3325184c3d3c8c02b1cf37d84c3a46c2323eb
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-23 17:39:58 +0100

    compute stack hash that is equivalent between provable & mins-aigner, but not yet the new implementation

--- src/lib/mina/token/call-forest.unit-test.ts
+++ src/lib/mina/token/call-forest.unit-test.ts
@@ -1,40 +1,85 @@
 import { Random, test } from '../../testing/property.js';
 import { RandomTransaction } from '../../../mina-signer/src/random-transaction.js';
 import { CallForest, PartialCallForest } from './call-forest.js';
-import { AccountUpdate, ZkappCommand } from '../../account_update.js';
+import {
+  AccountUpdate,
+  CallForest as ProvableCallForest,
+} from '../../account_update.js';
 import { TypesBigint } from '../../../bindings/mina-transaction/types.js';
 import { Pickles } from '../../../snarky.js';
-import { Field } from '../../../lib/core.js';
+import {
+  accountUpdatesToCallForest,
+  callForestHash,
+  CallForest as SimpleCallForest,
+} from '../../../mina-signer/src/sign-zkapp-command.js';
 
 // rng for account updates
 
 let [, data, hashMl] = Pickles.dummyVerificationKey();
-let verificationKey = { data, hash: Field(hashMl) };
+let verificationKey = { data, hash: hashMl[1] };
 
-const accountUpdates = Random.map(
+const callForest: Random<SimpleCallForest> = Random.map(
   RandomTransaction.zkappCommand,
   (txBigint) => {
-    // bigint to json, then to provable
-    let txJson = TypesBigint.ZkappCommand.toJSON(txBigint);
-
-    let accountUpdates = txJson.accountUpdates.map((aJson) => {
-      let a = AccountUpdate.fromJSON(aJson);
-
+    let flatUpdates = txBigint.accountUpdates.map((a) => {
       // fix verification key
       if (a.body.update.verificationKey.isSome) {
         a.body.update.verificationKey.value = verificationKey;
       }
       return a;
     });
-
-    return accountUpdates;
+    return accountUpdatesToCallForest(flatUpdates);
   }
 );
 
 // tests begin here
 
-test.custom({ timeBudget: 10000 })(accountUpdates, (updates) => {
+test.custom({ timeBudget: 10000 })(callForest, (forestBigint) => {
+  // reference: bigint callforest hash from mina-signer
+  let stackHash = callForestHash(forestBigint);
+
+  let updates = callForestToNestedArray(
+    mapCallForest(forestBigint, accountUpdateFromBigint)
+  );
+
   console.log({ length: updates.length });
 
-  CallForest.fromAccountUpdates(updates);
+  let dummyParent = AccountUpdate.dummy();
+  dummyParent.children.accountUpdates = updates;
+
+  let hash = ProvableCallForest.hashChildren(dummyParent);
+  hash.assertEquals(stackHash);
+
+  let forest = CallForest.fromAccountUpdates(updates);
 });
+
+// call forest helpers
+
+type AbstractSimpleCallForest<A> = {
+  accountUpdate: A;
+  children: AbstractSimpleCallForest<A>;
+}[];
+
+function mapCallForest<A, B>(
+  forest: AbstractSimpleCallForest<A>,
+  mapOne: (a: A) => B
+): AbstractSimpleCallForest<B> {
+  return forest.map(({ accountUpdate, children }) => ({
+    accountUpdate: mapOne(accountUpdate),
+    children: mapCallForest(children, mapOne),
+  }));
+}
+
+function accountUpdateFromBigint(a: TypesBigint.AccountUpdate): AccountUpdate {
+  // bigint to json, then to provable
+  return AccountUpdate.fromJSON(TypesBigint.AccountUpdate.toJSON(a));
+}
+
+function callForestToNestedArray(
+  forest: AbstractSimpleCallForest<AccountUpdate>
+): AccountUpdate[] {
+  return forest.map(({ accountUpdate, children }) => {
+    accountUpdate.children.accountUpdates = callForestToNestedArray(children);
+    return accountUpdate;
+  });
+}
--- src/mina-signer/src/sign-zkapp-command.ts
+++ src/mina-signer/src/sign-zkapp-command.ts
@@ -33,6 +33,7 @@ export {
   createFeePayer,
   accountUpdateFromFeePayer,
   isCallDepthValid,
+  CallForest,
 };
 
 function signZkappCommand(
