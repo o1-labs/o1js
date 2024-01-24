2471c1006e47bf36bb4077074fc52701df82f2b4
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-24 16:35:01 +0100

    finish unit tests for call forest iteration

--- src/lib/mina/token/call-forest.unit-test.ts
+++ src/lib/mina/token/call-forest.unit-test.ts
@@ -2,8 +2,7 @@ import { Random, test } from '../../testing/property.js';
 import { RandomTransaction } from '../../../mina-signer/src/random-transaction.js';
 import {
   CallForest,
-  HashedAccountUpdate,
-  PartialCallForest,
+  PartialCallForest as CallForestIterator,
   hashAccountUpdate,
 } from './call-forest.js';
 import {
@@ -19,7 +18,9 @@ import {
   CallForest as SimpleCallForest,
 } from '../../../mina-signer/src/sign-zkapp-command.js';
 import assert from 'assert';
-import { Field } from '../../field.js';
+import { Field, Bool } from '../../core.js';
+import { Bool as BoolB } from '../../../provable/field-bigint.js';
+import { PublicKey } from '../../signature.js';
 
 // RANDOM NUMBER GENERATORS for account updates
 
@@ -32,6 +33,12 @@ const accountUpdateBigint = Random.map(
     // fix verification key
     if (a.body.update.verificationKey.isSome)
       a.body.update.verificationKey.value = dummyVerificationKey;
+
+    // ensure that, by default, all account updates are token-accessible
+    a.body.mayUseToken =
+      a.body.callDepth === 0
+        ? { parentsOwnToken: BoolB(true), inheritFromParent: BoolB(false) }
+        : { parentsOwnToken: BoolB(false), inheritFromParent: BoolB(true) };
     return a;
   }
 );
@@ -48,7 +55,7 @@ const flatAccountUpdates = arrayOf(accountUpdate);
 
 // correctly hashes a call forest
 
-test.custom({ timeBudget: 1000, logFailures: false })(
+test.custom({ timeBudget: 1000 })(
   flatAccountUpdatesBigint,
   (flatUpdatesBigint) => {
     // reference: bigint callforest hash from mina-signer
@@ -66,114 +73,163 @@ test.custom({ timeBudget: 1000, logFailures: false })(
   }
 );
 
+// can recover flat account updates from nested updates
+// this is here to assert that we compute `updates` correctly in the other tests
+
+test(flatAccountUpdates, (flatUpdates) => {
+  let updates = callForestToNestedArray(
+    accountUpdatesToCallForest(flatUpdates)
+  );
+  let flatUpdates2 = ProvableCallForest.toFlatList(updates, false);
+  let n = flatUpdates.length;
+  for (let i = 0; i < n; i++) {
+    assert.deepStrictEqual(flatUpdates2[i], flatUpdates[i]);
+  }
+});
+
 // traverses the top level of a call forest in correct order
 // i.e., CallForest.next() works
 
-test.custom({ timeBudget: 1000, logFailures: false })(
-  flatAccountUpdates,
-  (flatUpdates) => {
-    // convert to o1js-style list of nested `AccountUpdate`s
-    let updates = callForestToNestedArray(
-      accountUpdatesToCallForest(flatUpdates)
-    );
-    let forest = CallForest.fromAccountUpdates(updates);
+test.custom({ timeBudget: 1000 })(flatAccountUpdates, (flatUpdates) => {
+  // prepare call forest from flat account updates
+  let updates = callForestToNestedArray(
+    accountUpdatesToCallForest(flatUpdates)
+  );
+  let forest = CallForest.fromAccountUpdates(updates);
 
-    let n = updates.length;
-    for (let i = 0; i < n; i++) {
-      let expected = updates[i];
-      let actual = forest.next().accountUpdate.value.get();
-      assertEqual(actual, expected);
-    }
-
-    // doing next() again should return a dummy
+  // step through top-level by calling forest.next() repeatedly
+  let n = updates.length;
+  for (let i = 0; i < n; i++) {
+    let expected = updates[i];
     let actual = forest.next().accountUpdate.value.get();
-    assertEqual(actual, AccountUpdate.dummy());
+    assertEqual(actual, expected);
   }
-);
+
+  // doing next() again should return a dummy
+  let actual = forest.next().accountUpdate.value.get();
+  assertEqual(actual, AccountUpdate.dummy());
+});
 
 // traverses a call forest in correct depth-first order,
-// assuming we don't skip any subtrees
+// when no subtrees are skipped
 
-test.custom({ timeBudget: 10000, logFailures: false })(
+test.custom({ timeBudget: 5000 })(flatAccountUpdates, (flatUpdates) => {
+  // with default token id, no subtrees will be skipped
+  let tokenId = TokenId.default;
+
+  // prepare forest iterator from flat account updates
+  let updates = callForestToNestedArray(
+    accountUpdatesToCallForest(flatUpdates)
+  );
+  let forest = CallForest.fromAccountUpdates(updates);
+  let forestIterator = CallForestIterator.create(forest, tokenId);
+
+  // step through forest iterator and compare against expected updates
+  let expectedUpdates = flatUpdates;
+
+  let n = flatUpdates.length;
+  for (let i = 0; i < n; i++) {
+    let expected = expectedUpdates[i];
+    let actual = forestIterator.next().accountUpdate;
+    assertEqual(actual, expected);
+  }
+
+  // doing next() again should return a dummy
+  let actual = forestIterator.next().accountUpdate;
+  assertEqual(actual, AccountUpdate.dummy());
+});
+
+// correctly skips subtrees for various reasons
+
+// in this test, we make all updates inaccessible by setting the top level to `no` or `inherit`, or to the token owner
+// this means we wouldn't need to traverse any update in the whole tree
+// but we only notice inaccessibility when we have already traversed the inaccessible update
+// so, the result should be that we traverse the top level and nothing else
+test.custom({ timeBudget: 5000 })(
   flatAccountUpdates,
-  (flatUpdates) => {
-    // convert to o1js-style list of nested `AccountUpdate`s
+  Random.publicKey,
+  (flatUpdates, publicKey) => {
+    // create token owner and derive token id
+    let tokenOwner = PublicKey.fromObject({
+      x: Field.from(publicKey.x),
+      isOdd: Bool(!!publicKey.isOdd),
+    });
+    let tokenId = TokenId.derive(tokenOwner);
+
+    // prepare forest iterator from flat account updates
     let updates = callForestToNestedArray(
       accountUpdatesToCallForest(flatUpdates)
     );
 
-    let forest = CallForest.fromAccountUpdates(updates);
-    let tokenId = Field.random();
-    let partialForest = PartialCallForest.create(forest, tokenId);
+    // make all top-level updates inaccessible
+    updates.forEach((u, i) => {
+      if (i % 3 === 0) {
+        u.body.mayUseToken = AccountUpdate.MayUseToken.No;
+      } else if (i % 3 === 1) {
+        u.body.mayUseToken = AccountUpdate.MayUseToken.InheritFromParent;
+      } else {
+        u.body.publicKey = tokenOwner;
+        u.body.tokenId = TokenId.default;
+      }
+    });
 
-    let flatUpdates2 = ProvableCallForest.toFlatList(updates, false);
+    let forest = CallForest.fromAccountUpdates(updates);
+    let forestIterator = CallForestIterator.create(forest, tokenId);
+
+    // step through forest iterator and compare against expected updates
+    let expectedUpdates = updates;
 
     let n = flatUpdates.length;
     for (let i = 0; i < n; i++) {
-      assert.deepStrictEqual(flatUpdates2[i], flatUpdates[i]);
-
-      let expected = flatUpdates[i];
-      let actual = partialForest.next({ skipSubtrees: false }).accountUpdate;
+      let expected = expectedUpdates[i] ?? AccountUpdate.dummy();
+      let actual = forestIterator.next().accountUpdate;
       assertEqual(actual, expected);
     }
-
-    // doing next() again should return a dummy
-    let actual = partialForest.next({ skipSubtrees: false }).accountUpdate;
-    assertEqual(actual, AccountUpdate.dummy());
   }
 );
 
-// TODO
-// traverses a call forest in correct depth-first order, when skipping subtrees
-
-test.custom({ timeBudget: 10000, logFailures: false, minRuns: 1, maxRuns: 1 })(
+// similar to the test before, but now we make all updates in the second layer inaccessible
+// so the iteration should walk through the first and second layer
+test.custom({ timeBudget: 5000 })(
   flatAccountUpdates,
-  (flatUpdates) => {
-    // convert to o1js-style list of nested `AccountUpdate`s
+  Random.publicKey,
+  (flatUpdates, publicKey) => {
+    // create token owner and derive token id
+    let tokenOwner = PublicKey.fromObject({
+      x: Field.from(publicKey.x),
+      isOdd: Bool(!!publicKey.isOdd),
+    });
+    let tokenId = TokenId.derive(tokenOwner);
+
+    // make all second-level updates inaccessible
+    flatUpdates
+      .filter((u) => u.body.callDepth === 1)
+      .forEach((u, i) => {
+        if (i % 3 === 0) {
+          u.body.mayUseToken = AccountUpdate.MayUseToken.No;
+        } else if (i % 3 === 1) {
+          u.body.mayUseToken = AccountUpdate.MayUseToken.ParentsOwnToken;
+        } else {
+          u.body.publicKey = tokenOwner;
+          u.body.tokenId = TokenId.default;
+        }
+      });
+
+    // prepare forest iterator from flat account updates
     let updates = callForestToNestedArray(
       accountUpdatesToCallForest(flatUpdates)
     );
-
-    let dummyParent = AccountUpdate.dummy();
-    dummyParent.children.accountUpdates = updates;
-    console.log(dummyParent.toPrettyLayout());
-
     let forest = CallForest.fromAccountUpdates(updates);
-    let tokenId = Field.random();
-    let partialForest = PartialCallForest.create(forest, tokenId);
+    let forestIterator = CallForestIterator.create(forest, tokenId);
 
-    let flatUpdates2 = ProvableCallForest.toFlatList(updates, false);
+    // step through forest iterator and compare against expected updates
+    let expectedUpdates = flatUpdates.filter((u) => u.body.callDepth <= 1);
 
     let n = flatUpdates.length;
     for (let i = 0; i < n; i++) {
-      assert.deepStrictEqual(flatUpdates2[i], flatUpdates[i]);
-
-      let expected = flatUpdates[i];
-      let expectedHash = hashAccountUpdate(expected).toBigInt();
-      let actual = partialForest.next({ skipSubtrees: false }).accountUpdate;
-      let actualHash = hashAccountUpdate(actual).toBigInt();
-
-      let isCorrect = String(expectedHash === actualHash).padStart(5, ' ');
-
-      console.log(
-        'actual:   ',
-        actual.body.callDepth,
-        isCorrect,
-        actual.body.publicKey.toBase58(),
-        actualHash
-      );
-    }
-    console.log();
-
-    for (let i = 0; i < n; i++) {
-      let expected = flatUpdates[i];
-      console.log(
-        'expected: ',
-        expected.body.callDepth,
-        ' true',
-        expected.body.publicKey.toBase58(),
-        hashAccountUpdate(expected).toBigInt()
-      );
+      let expected = expectedUpdates[i] ?? AccountUpdate.dummy();
+      let actual = forestIterator.next().accountUpdate;
+      assertEqual(actual, expected);
     }
   }
 );
