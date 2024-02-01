1435bb5b18a5200f41ef4a3c9802f9b0bf983fd0
Gregor <gregor.mitscha-baude@gmx.at>
2024-02-01 15:59:07 +0100

    remove unused code

--- src/examples/zkapps/dex/dex.ts
+++ src/examples/zkapps/dex/dex.ts
@@ -418,51 +418,6 @@ class TokenContract extends BaseTokenContract {
     zkapp.requireSignature();
   }
 
-  // @method _approveUpdate(zkappUpdate: AccountUpdate) {
-  //   this.approve(zkappUpdate);
-  //   let balanceChange = Int64.fromObject(zkappUpdate.body.balanceChange);
-  //   balanceChange.assertEquals(Int64.from(0));
-  // }
-
-  // // FIXME: remove this
-  // @method _approveAny(zkappUpdate: AccountUpdate) {
-  //   this.approve(zkappUpdate, AccountUpdate.Layout.AnyChildren);
-  // }
-
-  // // let a zkapp send tokens to someone, provided the token supply stays constant
-  // @method _approveUpdateAndSend(
-  //   zkappUpdate: AccountUpdate,
-  //   to: PublicKey,
-  //   amount: UInt64
-  // ) {
-  //   // approve a layout of two grandchildren, both of which can't inherit the token permission
-  //   let { StaticChildren, AnyChildren } = AccountUpdate.Layout;
-  //   this.approve(zkappUpdate, StaticChildren(AnyChildren, AnyChildren));
-  //   zkappUpdate.body.mayUseToken.parentsOwnToken.assertTrue();
-  //   let [grandchild1, grandchild2] = zkappUpdate.children.accountUpdates;
-  //   grandchild1.body.mayUseToken.inheritFromParent.assertFalse();
-  //   grandchild2.body.mayUseToken.inheritFromParent.assertFalse();
-
-  //   // see if balance change cancels the amount sent
-  //   let balanceChange = Int64.fromObject(zkappUpdate.body.balanceChange);
-  //   balanceChange.assertEquals(Int64.from(amount).neg());
-  //   // add same amount of tokens to the receiving address
-  //   this.token.mint({ address: to, amount });
-  // }
-
-  // _transfer(from: PublicKey, to: PublicKey | AccountUpdate, amount: UInt64) {
-  //   if (to instanceof PublicKey)
-  //     return this._transferToAddress(from, to, amount);
-  //   if (to instanceof AccountUpdate)
-  //     return this._transferToUpdate(from, to, amount);
-  // }
-  // @method _transferToAddress(from: PublicKey, to: PublicKey, value: UInt64) {
-  //   this.token.send({ from, to, amount: value });
-  // }
-  // @method _transferToUpdate(from: PublicKey, to: AccountUpdate, value: UInt64) {
-  //   this.token.send({ from, to, amount: value });
-  // }
-
   @method getBalance(publicKey: PublicKey): UInt64 {
     let accountUpdate = AccountUpdate.create(publicKey, this.token.id);
     let balance = accountUpdate.account.balance.get();
