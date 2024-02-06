d69c73a3fe0752ef43816d84a7c9cd67abee347c
Gregor <gregor.mitscha-baude@gmx.at>
2024-02-06 15:33:42 +0100

    add failing unit test

---
+++ src/lib/mina/account-update-layout.unit-test.ts
@@ -0,0 +1,42 @@
+import { Mina } from '../../index.js';
+import { AccountUpdate } from '../account_update.js';
+import { UInt64 } from '../int.js';
+import { SmartContract, method } from '../zkapp.js';
+
+// smart contract which creates an account update that has a child of its own
+
+class NestedCall extends SmartContract {
+  @method deposit() {
+    const payerUpdate = AccountUpdate.createSigned(this.sender);
+    payerUpdate.send({ to: this.address, amount: UInt64.one });
+  }
+}
+
+// setup
+
+let Local = Mina.LocalBlockchain({ proofsEnabled: true });
+Mina.setActiveInstance(Local);
+
+let [
+  { publicKey: sender, privateKey: senderKey },
+  { publicKey: zkappAddress, privateKey: zkappKey },
+] = Local.testAccounts;
+
+await NestedCall.compile();
+let zkapp = new NestedCall(zkappAddress);
+
+// deploy zkapp
+
+await (await Mina.transaction(sender, () => zkapp.deploy()))
+  .sign([zkappKey, senderKey])
+  .send();
+
+// deposit call
+let balanceBefore = Mina.getBalance(zkappAddress);
+
+let depositTx = await Mina.transaction(sender, () => zkapp.deposit());
+console.log(depositTx.toPretty());
+await depositTx.prove();
+await depositTx.sign([senderKey]).send();
+
+Mina.getBalance(zkappAddress).assertEquals(balanceBefore.add(1));
