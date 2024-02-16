f1ec38f40cbb77d19948462752572614686e922b
Martin Minkov <minkovlmartin@gmail.com>
2024-02-16 09:59:03 -0800

    refactor(graphql.ts): modify import statements

--- src/lib/mina/graphql.ts
+++ src/lib/mina/graphql.ts
@@ -1,6 +1,6 @@
-import { type ActionStatesStringified, removeJsonQuotes } from '../fetch.js';
 import { UInt32 } from '../int.js';
-import { type ZkappCommand } from '../account-update.js';
+import type { ZkappCommand } from '../account-update.js';
+import type { ActionStatesStringified } from '../fetch.js';
 import { Types } from '../../bindings/mina-transaction/types.js';
 
 export {
