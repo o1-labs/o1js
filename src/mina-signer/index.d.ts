b7b5f14f057b2f87f4c19211ba114ba03e6d3cd4
Serhii Shymkiv <sergey@shimkiv.com>
2024-02-17 10:48:47 +0200

    Fix for Mina-Signer exports.

--- src/mina-signer/index.d.ts
+++ src/mina-signer/index.d.ts
@@ -1,6 +1,6 @@
 // this file is a wrapper for supporting types in both commonjs and esm projects
 
 import Client from './mina-signer.ts';
-export { type NetworkId } from './src/types.ts';
+import { NetworkId } from './src/types.ts';
 
-export = Client;
+export { Client, NetworkId };
