9ce4ea69007bb2876e155d5d99fe9dc5a9c8d494
Martin Minkov <minkovlmartin@gmail.com>
2024-02-22 12:19:52 -0800

    fix(scalar.ts): correct parameter type in toInput() method documentation from Field to Scalar for accuracy

--- src/lib/scalar.ts
+++ src/lib/scalar.ts
@@ -236,9 +236,9 @@ class Scalar {
   /**
    * **Warning**: This function is mainly for internal use. Normally it is not intended to be used by a zkApp developer.
    *
-   * This function is the implementation of `ProvableExtended.toInput()` for the {@link Field} type.
+   * This function is the implementation of `ProvableExtended.toInput()` for the {@link Scalar} type.
    *
-   * @param value - The {@link Field} element to get the `input` array.
+   * @param value - The {@link Scalar} element to get the `input` array.
    *
    * @return An object where the `fields` key is a {@link Field} array of length 1 created from this {@link Field}.
    *
