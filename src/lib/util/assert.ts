acceaac5678de446f9ea4ecfd78c18809c4b4651
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-02-27 09:56:14 +0100

    assert promise helper

--- src/lib/util/assert.ts
+++ src/lib/util/assert.ts
@@ -1,7 +1,12 @@
-export { assert };
+export { assert, assertPromise };
 
 function assert(stmt: boolean, message?: string): asserts stmt {
   if (!stmt) {
     throw Error(message ?? 'Assertion failed');
   }
+}
+
+function assertPromise<T extends Promise<any>>(value: T): T {
+  assert(value instanceof Promise, 'Expected a promise');
+  return value;
 }
