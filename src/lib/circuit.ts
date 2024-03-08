102217012c7bc529e3ad9f71a4634cdd467412ef
gabrielbosio <gabrielbosio95@gmail.com>
2024-03-08 17:13:25 -0300

    Remove sort in input types

--- src/lib/circuit.ts
+++ src/lib/circuit.ts
@@ -261,10 +261,10 @@ function circuitMain(
       return (target as any)[propertyName].apply(target, args);
     },
     publicInputType: provableFromTuple(
-      Array.from(publicIndexSet).sort().map((i) => paramTypes[i])
+      Array.from(publicIndexSet).map((i) => paramTypes[i])
     ),
     privateInputType: provableFromTuple(
-      Array.from(witnessIndexSet).sort().map((i) => paramTypes[i])
+      Array.from(witnessIndexSet).map((i) => paramTypes[i])
     ),
   };
 }
