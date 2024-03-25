559364dc47d2d91d695bd7fb1e3cc9ffd9e8bd56
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-25 18:13:36 +0100

    fixup

--- src/lib/provable-types/merkle-tree.unit-test.ts
+++ src/lib/provable-types/merkle-tree.unit-test.ts
@@ -1,15 +1,10 @@
 import { Bool, Field } from '../core.js';
-import { maybeSwap, maybeSwapBad } from './merkle-tree.js';
+import { maybeSwap } from './merkle-tree.js';
 import { Random, test } from '../testing/property.js';
 import { expect } from 'expect';
 
 test(Random.bool, Random.field, Random.field, (b, x, y) => {
   let [x0, y0] = maybeSwap(Bool(!!b), Field(x), Field(y));
-  let [x1, y1] = maybeSwapBad(Bool(!!b), Field(x), Field(y));
-
-  // both versions of `maybeSwap` should behave the same
-  expect(x0).toEqual(x1);
-  expect(y0).toEqual(y1);
 
   // if the boolean is true, it shouldn't swap the fields; otherwise, it should
   if (b) {
