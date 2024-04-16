9d8eb95610daac57e332117720c53c079cea0ce2
Gregor <gregor.mitscha-baude@gmx.at>
2024-04-16 21:29:11 +0200

    feedback: better error message

--- src/lib/provable/crypto/poseidon.ts
+++ src/lib/provable/crypto/poseidon.ts
@@ -36,6 +36,10 @@ class Sponge {
   // TODO: implement constant version in TS. currently, you need to call `initializeBindings()` before successfully calling this
   constructor() {
     let isChecked = Provable.inCheckedComputation();
+    assert(
+      Snarky !== undefined,
+      'Poseidon.Sponge(): bindings are not initialized, try calling `await initializeBindings()` first.'
+    );
     this.#sponge = Snarky.poseidon.sponge.create(isChecked);
   }
 
