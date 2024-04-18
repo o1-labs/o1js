65f75e49484aed7f373562e44a215c1c768060de
Gregor <gregor.mitscha-baude@gmx.at>
2024-04-18 12:03:42 +0200

    wip debugging

--- src/mina-signer/src/sign-zkapp-command.ts
+++ src/mina-signer/src/sign-zkapp-command.ts
@@ -101,6 +101,7 @@ function verifyAccountUpdateSignature(
   transactionCommitments: { commitment: bigint; fullCommitment: bigint },
   networkId: NetworkId
 ) {
+  console.log('verifyAccountUpdateSignature', update.authorization.signature);
   if (update.authorization.signature === undefined) return false;
 
   let { publicKey, useFullCommitment } = update.body;
@@ -108,7 +109,12 @@ function verifyAccountUpdateSignature(
   let usedCommitment = useFullCommitment ? fullCommitment : commitment;
   let signature = Signature.fromBase58(update.authorization.signature);
 
-  return verifyFieldElement(signature, usedCommitment, publicKey, networkId);
+  let ok = verifyFieldElement(signature, usedCommitment, publicKey, networkId);
+  console.dir(
+    { signature, usedCommitment, publicKey, networkId, ok },
+    { depth: null }
+  );
+  return ok;
 }
 
 function transactionCommitments(
