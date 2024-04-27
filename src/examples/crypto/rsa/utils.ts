4b6f0bdbfd8667d4f88d41bfae22184a5c250538
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-04-27 21:29:49 +0200

    replace modinv

--- src/examples/crypto/rsa/utils.ts
+++ src/examples/crypto/rsa/utils.ts
@@ -10,6 +10,18 @@ export {
 };
 
 /**
+ * Generates an RSA signature for the given message using the private key and modulus.
+ * @param message - The message to be signed.
+ * @param privateKey - The private exponent used for signing.
+ * @param modulus - The modulus used for signing.
+ * @returns The RSA signature of the message.
+ */
+function rsaSign(message: bigint, privateKey: bigint, modulus: bigint): bigint {
+  // Calculate the signature using modular exponentiation
+  return power(message, privateKey, modulus);
+}
+
+/**
  * Generates a SHA-256 digest of the input message and returns the hash as a native bigint.
  * @param  message - The input message to be hashed.
  * @returns The SHA-256 hash of the input message as a native bigint.
@@ -26,29 +38,6 @@ function generateDigestBigint(message: string) {
  */
 function toBigInt(x: bigInt.BigInteger): bigint {
   return BigInt('0x' + x.toString(16));
-}
-
-/**
- * Generates a random prime number with the specified bit length.
- * @param bitLength - The desired bit length of the prime number. Default is 1024.
- * @returns A random prime number with the specified bit length.
- */
-function generateRandomPrime(bitLength: number): bigint {
-  let primeCandidate;
-  do {
-    // Generate a random number with the desired bit length
-    primeCandidate = bigInt.randBetween(
-      bigInt(2).pow(bitLength - 1), // Lower bound
-      bigInt(2).pow(bitLength).minus(1) // Upper bound
-    );
-
-    // Ensure the number is odd
-    if (!primeCandidate.isOdd()) {
-      primeCandidate = primeCandidate.add(1);
-    }
-  } while (!primeCandidate.isPrime());
-
-  return toBigInt(primeCandidate);
 }
 
 /**
@@ -74,22 +63,35 @@ function generateRsaParams(primeSize: number) {
   const phiN = (p - 1n) * (q - 1n);
 
   // Private exponent
-  const d = toBigInt(bigInt(e).modInv(phiN));
+  const d = inverse(e, phiN);
 
   return { p, q, n: p * q, phiN, e, d };
 }
 
 /**
- * Generates an RSA signature for the given message using the private key and modulus.
- * @param message - The message to be signed.
- * @param privateKey - The private exponent used for signing.
- * @param modulus - The modulus used for signing.
- * @returns The RSA signature of the message.
+ * Generates a random prime number with the specified bit length.
+ * @param bitLength - The desired bit length of the prime number. Default is 1024.
+ * @returns A random prime number with the specified bit length.
  */
-function rsaSign(message: bigint, privateKey: bigint, modulus: bigint): bigint {
-  // Calculate the signature using modular exponentiation
-  return power(message, privateKey, modulus);
+function generateRandomPrime(bitLength: number): bigint {
+  let primeCandidate;
+  do {
+    // Generate a random number with the desired bit length
+    primeCandidate = bigInt.randBetween(
+      bigInt(2).pow(bitLength - 1), // Lower bound
+      bigInt(2).pow(bitLength).minus(1) // Upper bound
+    );
+
+    // Ensure the number is odd
+    if (!primeCandidate.isOdd()) {
+      primeCandidate = primeCandidate.add(1);
+    }
+  } while (!primeCandidate.isPrime());
+
+  return toBigInt(primeCandidate);
 }
+
+// finite field helpers (copied here from src/bindings/crypto/finite-field.ts)
 
 // modular exponentiation, a^n % p
 function power(a: bigint, n: bigint, p: bigint) {
@@ -100,6 +102,24 @@ function power(a: bigint, n: bigint, p: bigint) {
     a = mod(a * a, p);
   }
   return x;
+}
+
+// modular inverse, 1/a in Z_p
+function inverse(a_: bigint, p: bigint) {
+  let a = mod(a_, p);
+  if (a === 0n) throw Error('modular inverse: division by 0');
+  let b = p;
+  let [x, y, u, v] = [0n, 1n, 1n, 0n];
+  while (a !== 0n) {
+    let q = b / a;
+    [b, a] = [a, b - a * q];
+    [x, u] = [u, x - u * q];
+    [y, v] = [v, y - v * q];
+  }
+  if (b !== 1n) throw Error('modular inverse failed (b != 1)');
+  // TODO remove
+  if (mod(x * a_, p) !== 1n) throw Error('modular inverse failed');
+  return mod(x, p);
 }
 
 function mod(x: bigint, p: bigint) {
