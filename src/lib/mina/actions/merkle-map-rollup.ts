d9c382404d49e1a60869662a5f220e8369404574
Gregor <gregor.mitscha-baude@gmx.at>
2024-05-02 15:35:52 +0200

    finish v0 of merkle map rollup

--- src/lib/mina/actions/merkle-map-rollup.ts
+++ src/lib/mina/actions/merkle-map-rollup.ts
@@ -3,11 +3,14 @@ import { Field, Bool } from '../../provable/wrapped.js';
 import { Unconstrained } from '../../provable/types/unconstrained.js';
 import { MerkleList, MerkleListIterator } from '../../provable/merkle-list.js';
 import { Actions } from '../../../bindings/mina-transaction/transaction-leaves.js';
-import { MerkleTree } from '../../provable/merkle-tree.js';
+import { MerkleTree, MerkleWitness } from '../../provable/merkle-tree.js';
 import { Struct } from '../../provable/types/struct.js';
 import { SelfProof } from '../../proof-system/zkprogram.js';
 import { Provable } from '../../provable/provable.js';
 import { AnyTuple } from '../../util/types.js';
+import { assert } from '../../provable/gadgets/common.js';
+
+export { MerkleMapRollup };
 
 // our action type
 class MerkleLeaf extends Struct({ key: Field, value: Field }) {}
@@ -29,11 +32,16 @@ class ActionIterator extends MerkleListIterator.create(
 
 class MerkleMapState extends Struct({
   // this should just be a MerkleTree type that carries the full tree as aux data
-  merkleRoot: Field,
+  root: Field,
   // TODO: make zkprogram support auxiliary data in public inputs
   // actionState: ActionIterator.provable,
   actionState: Field,
 }) {}
+
+const TREE_HEIGHT = 256;
+class MerkleMapWitness extends MerkleWitness(TREE_HEIGHT) {}
+
+// TODO: it would be nice to abstract the logic for proving a chain of state transition proofs
 
 /**
  * This function represents a proof that we can go from MerkleMapState A -> B
@@ -84,21 +92,43 @@ const merkleUpdateBatch = (
     );
     // this would be unnecessary if the iterator could just be the public input
     actions.currentHash.assertEquals(stateB.actionState);
-    let root = stateB.merkleRoot;
+    let root = stateB.root;
 
     // update merkle root for each action
     for (let i = 0; i < maxUpdatesPerBatch; i++) {
-      let actionsList = actions.next();
+      actions.next().forEach(maxActionsPerUpdate, ({ key, value }, isDummy) => {
+        // merkle witness
+        let witness = Provable.witness(
+          MerkleMapWitness,
+          () => new MerkleMapWitness(tree.get().getWitness(key.toBigInt()))
+        );
 
-      actionsList.forEach(maxActionsPerUpdate, ({ key, value }, isDummy) => {
-        // TODO update root, using key, value and the tree
+        // previous value at the key
+        let previousValue = Provable.witness(Field, () =>
+          tree.get().getLeaf(key.toBigInt())
+        );
+
+        // prove that the witness is correct, by comparing the implied root and key
+        // note: this just works if the (key, value) is a (0,0) dummy, because the value at the 0 key will always be 0
+        witness.calculateIndex().assertEquals(key);
+        witness.calculateRoot(previousValue).assertEquals(root);
+
+        // store new value in at the key
+        let newRoot = witness.calculateRoot(value);
+
+        // update the tree, outside the circuit (this should all be part of a better merkle tree API)
+        Provable.asProver(() => {
+          // ignore dummy value
+          if (isDummy.toBoolean()) return;
+          tree.get().setLeaf(key.toBigInt(), value);
+        });
+
+        // update root
+        root = Provable.if(isDummy, root, newRoot);
       });
     }
 
-    return {
-      merkleRoot: root,
-      actionState: actions.currentHash,
-    };
+    return { root, actionState: actions.currentHash };
   },
 });
 
@@ -115,6 +145,75 @@ function MerkleMapRollup({
     },
   });
 
-  // TODO: convenient interface for doing the entire chain of proofs
-  return merkleMapRollup;
+  let MerkleRollupProof = ZkProgram.Proof(merkleMapRollup);
+
+  let isCompiled = true;
+
+  return {
+    async compile() {
+      if (isCompiled) return;
+      let result = await merkleMapRollup.compile();
+      isCompiled = true;
+      return result;
+    },
+
+    async prove(actions: MerkleList<MerkleList<MerkleLeaf>>, tree: MerkleTree) {
+      assert(tree.height === TREE_HEIGHT, 'Tree height must match');
+      await this.compile();
+
+      let n = actions.data.get().length;
+      let nBatches = Math.ceil(n / maxUpdatesPerBatch);
+
+      // if there are no actions, we still need to create a valid proof for the empty transition
+      if (n === 0) nBatches = 1;
+
+      // input state
+      let iterator = actions.startIterating();
+
+      let inputState = new MerkleMapState({
+        root: tree.getRoot(),
+        actionState: iterator.currentHash,
+      });
+
+      // dummy proof
+      console.time('dummy');
+      let dummyState = MerkleMapState.empty();
+      let dummy = await MerkleRollupProof.dummy(dummyState, dummyState, 1);
+      console.timeEnd('dummy');
+
+      // base proof
+      console.time('batch 0');
+      let proof = await merkleMapRollup.nextBatch(
+        inputState,
+        iterator,
+        Unconstrained.from(tree),
+        Bool(false),
+        dummy
+      );
+      console.timeEnd('batch 0');
+
+      // recursive proofs
+      for (let i = 1; i < nBatches; i++) {
+        // update iterator (would be nice if the method call would just return the updated one)
+        iterator.currentHash = proof.publicOutput.actionState;
+        for (let j = 0; j < maxUpdatesPerBatch; j++) {
+          iterator._updateIndex('next');
+        }
+
+        console.time(`batch ${i}`);
+        proof = await merkleMapRollup.nextBatch(
+          inputState,
+          iterator,
+          Unconstrained.from(tree),
+          Bool(true),
+          proof
+        );
+        console.timeEnd(`batch ${i}`);
+      }
+
+      return proof;
+    },
+
+    program: merkleMapRollup,
+  };
 }
