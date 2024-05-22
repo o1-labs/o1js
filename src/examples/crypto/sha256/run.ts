fb5bb734d4a9a3f2e75d298f0dae52025b04e93d
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-05-22 12:01:21 +0100

    Add dynamic SHA256 zkProgram

--- src/examples/crypto/sha256/run.ts
+++ src/examples/crypto/sha256/run.ts
@@ -1,5 +1,11 @@
-import { Bytes12, SHA256Program } from './sha256.js';
+import {
+  Bytes12,
+  SHA256Program,
+  SHA256UpdateProgram,
+  Bytes6,
+} from './sha256.js';
 
+console.log('SHA256Program Benchmarks');
 console.time('compile');
 await SHA256Program.compile();
 console.timeEnd('compile');
@@ -21,3 +27,30 @@ if (
 )
   throw new Error('Invalid sha256 digest!');
 if (!isValid) throw new Error('Invalid proof');
+
+console.log('\nSHA256UpdateProgram Benchmarks');
+console.time('compile');
+await SHA256UpdateProgram.compile();
+console.timeEnd('compile');
+
+console.log(
+  'dynamic sha256 rows:',
+  (await SHA256UpdateProgram.analyzeMethods()).update.rows
+);
+
+let preimage1 = Bytes6.fromString('hello ');
+let preimage2 = Bytes6.fromString('world!');
+
+console.time('prove');
+let proofHasher = await SHA256UpdateProgram.update(preimage1, preimage2);
+console.timeEnd('prove');
+let isValidProof = await SHA256UpdateProgram.verify(proofHasher);
+
+console.log('digest:', proofHasher.publicOutput.toHex());
+
+if (
+  proof.publicOutput.toHex() !==
+  '7509e5bda0c762d2bac7f90d758b5b2263fa01ccbc542ab5e3df163be08e6ca9'
+)
+  throw new Error('Invalid sha256 digest!');
+if (!isValidProof) throw new Error('Invalid proof');
--- src/examples/crypto/sha256/sha256.ts
+++ src/examples/crypto/sha256/sha256.ts
@@ -1,8 +1,9 @@
 import { Bytes, Gadgets, ZkProgram } from 'o1js';
 
-export { SHA256Program, Bytes12 };
+export { SHA256Program, Bytes12, SHA256UpdateProgram, Bytes6 };
 
 class Bytes12 extends Bytes(12) {}
+class Bytes6 extends Bytes(6) {}
 
 let SHA256Program = ZkProgram({
   name: 'sha256',
@@ -12,6 +13,19 @@ let SHA256Program = ZkProgram({
       privateInputs: [Bytes12.provable],
       async method(xs: Bytes12) {
         return Gadgets.SHA256.hash(xs);
+      },
+    },
+  },
+});
+
+let SHA256UpdateProgram = ZkProgram({
+  name: 'sha256-update',
+  publicOutput: Bytes(32).provable,
+  methods: {
+    update: {
+      privateInputs: [Bytes6.provable, Bytes6.provable],
+      async method(xs1: Bytes6, xs2: Bytes6) {
+        return Gadgets.SHA256.update(xs1).update(xs2).digest();
       },
     },
   },
