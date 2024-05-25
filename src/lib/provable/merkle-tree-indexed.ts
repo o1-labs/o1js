c71a7badbba6e5aa8e70591c851b58f8be3d5e27
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-05-25 17:15:11 +0200

    get / set internal nodes

--- src/lib/provable/merkle-tree-indexed.ts
+++ src/lib/provable/merkle-tree-indexed.ts
@@ -41,15 +41,20 @@ class IndexedMerkleMap implements IndexedMerkleMapBase {
   // the raw data stored in the tree
   readonly leaves: InferValue<typeof Leaf>[] = [];
 
-  // helpers structures
-  length: number; // length of the leaves array
-  readonly nodes: Field[][] = []; // for every level, an array of hashes
+  // helper structures
+  length: number = 0; // length of the leaves array
+  readonly nodes: (bigint | undefined)[][]; // for every level, an array of hashes
 
   /**
    * Creates a new, empty Indexed Merkle Map, given its height.
    */
   constructor(height: number) {
-    this.root = Field(zero(height - 1));
+    this.root = Field(empty(height - 1));
+
+    this.nodes = Array(height);
+    for (let level = 0; level < height; level++) {
+      this.nodes[level] = [];
+    }
   }
 
   insert(key: Field, value: Field) {
@@ -71,14 +76,46 @@ class IndexedMerkleMap implements IndexedMerkleMapBase {
   remove(key: Field) {
     assert(false, 'not implemented');
   }
+
+  // helper methods
+
+  // invariant: for every node that is not undefined, its descendants are either empty or not undefined
+  private setLeafNode(index: number, leaf: bigint) {
+    this.nodes[0][index] = leaf;
+
+    let isLeft = index % 2 === 0;
+
+    for (let level = 1; level < this.height; level++) {
+      index = Math.floor(index / 2);
+
+      let left = this.getNode(level - 1, index * 2, isLeft);
+      let right = this.getNode(level - 1, index * 2 + 1, !isLeft);
+      this.nodes[level][index] = PoseidonBigint.hash([left, right]);
+
+      isLeft = index % 2 === 0;
+    }
+  }
+
+  private getNode(level: number, index: number, nonEmpty: boolean) {
+    let node = this.nodes[level]?.[index];
+    if (node === undefined) {
+      if (nonEmpty)
+        throw Error(
+          `node at level=${level}, index=${index} was expected to be known, but isn't.`
+        );
+      node = empty(level);
+    }
+    return node;
+  }
 }
 
-// cache of zero hashes
-const zeroes = [0n];
-function zero(level: number) {
-  for (let i = zeroes.length; i <= level; i++) {
-    let zero = zeroes[i - 1];
-    zeroes[i] = PoseidonBigint.hash([zero, zero]);
+// cache of empty nodes (=: zero leaves and nodes with only empty nodes below them)
+const emptyNodes = [0n];
+
+function empty(level: number) {
+  for (let i = emptyNodes.length; i <= level; i++) {
+    let zero = emptyNodes[i - 1];
+    emptyNodes[i] = PoseidonBigint.hash([zero, zero]);
   }
-  return zeroes[level];
+  return emptyNodes[level];
 }
