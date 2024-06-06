10ed46328bfcae36ea61bef038e25648c206df13
Gregor <gregor.mitscha-baude@gmx.at>
2024-06-06 13:44:40 +0200

    make action batch a class

--- src/lib/mina/actions/batch-reducer.ts
+++ src/lib/mina/actions/batch-reducer.ts
@@ -4,8 +4,20 @@ import { Proof } from '../../proof-system/zkprogram.js';
 import { Field } from '../../provable/wrapped.js';
 import { SmartContract } from '../zkapp.js';
 import { assertDefined } from '../../util/errors.js';
-import { From } from '../../../bindings/lib/provable-generic.js';
-import { InferProvable } from '../../provable/types/struct.js';
+import { From, InferValue } from '../../../bindings/lib/provable-generic.js';
+import {
+  InferProvable,
+  ProvableExtended,
+} from '../../provable/types/struct.js';
+import { Provable } from '../../provable/provable.js';
+import { provableFromClass } from '../../provable/types/provable-derivers.js';
+import { Actionable } from './offchain-state-serialization.js';
+import {
+  ProvableHashable,
+  hashWithPrefix,
+} from '../../provable/crypto/poseidon.js';
+import { prefixes } from '../../../bindings/crypto/constants.js';
+import { Actions } from '../account-update.js';
 
 export { BatchReducer, ActionBatch, ActionBatchProof };
 
@@ -125,22 +137,66 @@ class BatchReducerBase<ActionType, BatchSize extends number = number> {
  * The `prove()` and `verify()` methods are intended to prove the missing part: namely, that the batch
  * is connected to a given final action state (typically stored onchain) by the `remainingActions`.
  */
-type ActionBatch<Action, BatchSize extends number = number> = {
+function ActionBatch<
+  ActionType extends Actionable<any>,
+  BatchSize extends number = number
+>(
+  actionType: ActionType,
+  batchSize: BatchSize
+): typeof ActionBatchBase<InferProvable<ActionType>, BatchSize> {
+  return class ActionBatch_ extends ActionBatchBase<
+    InferProvable<ActionType>,
+    BatchSize
+  > {
+    get batchSize() {
+      return batchSize;
+    }
+
+    provable = provableFromClass(
+      ActionBatch_,
+      provableBase(actionType, batchSize)
+    );
+  };
+}
+
+type ActionBatch<Action, BatchSize extends number = number> = ActionBatchBase<
+  Action,
+  BatchSize
+>;
+
+class ActionBatchBase<Action, BatchSize extends number = number> {
+  get batchSize(): BatchSize {
+    throw Error('Batch size must be defined in a subclass');
+  }
+
   batch: TupleN<Action, BatchSize>;
-  batchSize: BatchSize;
 
   initialActionState: Field;
-  get finalActionState(): Field;
+  get finalActionState(): Field {
+    return this.remainingActions.hash;
+  }
 
   batchActions: MerkleList<MerkleList<Field>>;
   remainingActions: MerkleList<MerkleList<Field>>;
+
+  constructor(input: {
+    batch: TupleN<Action, BatchSize>;
+    batchSize: BatchSize;
+    initialActionState: Field;
+    batchActions: MerkleList<MerkleList<Field>>;
+    remainingActions: MerkleList<MerkleList<Field>>;
+  }) {
+    Object.assign(this, input);
+  }
 
   /**
    * Iterate over the actions in this batch.
    *
    * Note: This is simply a for-loop over the fixed-size `batch`.
    */
-  forEach(callback: (action: Action) => void): void;
+  forEach(callback: (action: Action) => void): void {
+    notImplemented();
+  }
 
   /**
    * Verify the validity of this batch of actions against the given initial & final action states and the provided proof.
@@ -149,16 +205,49 @@ type ActionBatch<Action, BatchSize extends number = number> = {
     initialActionState: Field,
     finalActionState: Field,
     actionBatchProof: ActionBatchProof
-  ): void;
+  ): void {
+    notImplemented();
+  }
 
   /**
    * Prove the validity of this batch of actions, by showing that the `remainingActions`, when applied after the
    * `batchActions`, result in a final state that forms the proof's public output.
    */
-  prove(): Promise<ActionBatchProof>;
-};
+  prove(): Promise<ActionBatchProof> {
+    notImplemented();
+  }
+
+  provable: ProvableExtended<ActionBatch<Action, BatchSize>> = undefined as any;
+}
 
 type ActionBatchProof = Proof<Field, Field>;
+
+class FieldList extends MerkleList.create<Field>(
+  Field,
+  (actionsHash: Field, actionHash: Field): Field =>
+    hashWithPrefix(prefixes.sequenceEvents, [actionsHash, actionHash]),
+  Actions.empty().hash
+) {}
+
+class FieldListList extends MerkleList.create<MerkleList<Field>>(
+  FieldList.provable,
+  (hash: Field, actions: FieldList): Field =>
+    Actions.updateSequenceState(hash, actions.hash),
+  Actions.emptyActionState()
+) {}
+
+const provableBase = <ActionType extends Actionable<any>, N extends number>(
+  actionType: ActionType,
+  batchSize: N
+) => ({
+  batch: Provable.Array(actionType, batchSize) as unknown as ProvableHashable<
+    TupleN<InferProvable<ActionType>, N>,
+    TupleN<InferValue<ActionType>, N>
+  >,
+  initialActionState: Field,
+  batchActions: FieldListList.provable,
+  remainingActions: FieldListList.provable,
+});
 
 function notImplemented(): never {
   throw Error('Not implemented');
