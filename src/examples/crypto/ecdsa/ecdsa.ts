0e857b13280f94f61aaa2fcf5193f2c8728a90c0
Martin Minkov <minkovlmartin@gmail.com>
2024-06-25 11:06:47 -0700

    feat(foreign-curve.ts, foreign-ecdsa.ts): introduce ForeignCurveV2 and EcdsaSignatureV2 classes

--- src/examples/crypto/ecdsa/ecdsa.ts
+++ src/examples/crypto/ecdsa/ecdsa.ts
@@ -2,14 +2,14 @@ import {
   ZkProgram,
   Crypto,
   createEcdsa,
-  createForeignCurve,
+  createForeignCurveV2,
   Bool,
   Bytes,
 } from 'o1js';
 
 export { keccakAndEcdsa, ecdsa, Secp256k1, Ecdsa, Bytes32 };
 
-class Secp256k1 extends createForeignCurve(Crypto.CurveParams.Secp256k1) {}
+class Secp256k1 extends createForeignCurveV2(Crypto.CurveParams.Secp256k1) {}
 class Scalar extends Secp256k1.Scalar {}
 class Ecdsa extends createEcdsa(Secp256k1) {}
 class Bytes32 extends Bytes(32) {}
