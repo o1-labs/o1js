5bfdc0dd1e97b5c94c26f134e6ff50563161c01e
Florian <florian@technotro.com>
2024-06-26 12:33:46 +0200

    depracate

--- src/lib/provable/crypto/encryption.ts
+++ src/lib/provable/crypto/encryption.ts
@@ -3,7 +3,7 @@ import { Poseidon } from './poseidon.js';
 import { Provable } from '../provable.js';
 import { PrivateKey, PublicKey } from './signature.js';
 
-export { encrypt, decrypt };
+export { encrypt, decrypt, encryptV2, decryptV2 };
 
 type CipherText = {
   publicKey: Group;
@@ -11,9 +11,72 @@ type CipherText = {
 };
 
 /**
+ * @deprecated Use {@link encryptV2} instead. The private key used with this method should _never_ be reused again!
+ *
  * Public Key Encryption, using a given array of {@link Field} elements and encrypts it using a {@link PublicKey}.
  */
 function encrypt(message: Field[], otherPublicKey: PublicKey) {
+  // key exchange
+  let privateKey = Provable.witness(Scalar, () => Scalar.random());
+  let publicKey = Group.generator.scale(privateKey);
+  let sharedSecret = otherPublicKey.toGroup().scale(privateKey);
+
+  let sponge = new Poseidon.Sponge();
+  sponge.absorb(sharedSecret.x); // don't think we need y, that's enough entropy
+
+  // encryption
+  let cipherText = [];
+  for (let i = 0; i < message.length; i++) {
+    let keyStream = sponge.squeeze();
+    let encryptedChunk = message[i].add(keyStream);
+    cipherText.push(encryptedChunk);
+    // absorb for the auth tag (two at a time for saving permutations)
+    if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
+    if (i % 2 === 1 || i === message.length - 1) sponge.absorb(cipherText[i]);
+  }
+  // authentication tag
+  let authenticationTag = sponge.squeeze();
+  cipherText.push(authenticationTag);
+
+  return { publicKey, cipherText };
+}
+
+/**
+ * @deprecated Use {@link decryptV2} instead.
+ *
+ * Decrypts a {@link CipherText} using a {@link PrivateKey}.^
+ */
+function decrypt(
+  { publicKey, cipherText }: CipherText,
+  privateKey: PrivateKey
+) {
+  // key exchange
+  let sharedSecret = publicKey.scale(privateKey.s);
+
+  let sponge = new Poseidon.Sponge();
+  sponge.absorb(sharedSecret.x);
+  let authenticationTag = cipherText.pop();
+
+  // decryption
+  let message = [];
+  for (let i = 0; i < cipherText.length; i++) {
+    let keyStream = sponge.squeeze();
+    let messageChunk = cipherText[i].sub(keyStream);
+    message.push(messageChunk);
+    if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
+    if (i % 2 === 1 || i === cipherText.length - 1)
+      sponge.absorb(cipherText[i]);
+  }
+  // authentication tag
+  sponge.squeeze().assertEquals(authenticationTag!);
+
+  return message;
+}
+
+/**
+ * Public Key Encryption, using a given array of {@link Field} elements and encrypts it using a {@link PublicKey}.
+ */
+function encryptV2(message: Field[], otherPublicKey: PublicKey) {
   // key exchange
   let privateKey = Provable.witness(Scalar, () => Scalar.random());
   let publicKey = Group.generator.scale(privateKey);
@@ -43,7 +106,7 @@ function encrypt(message: Field[], otherPublicKey: PublicKey) {
 /**
  * Decrypts a {@link CipherText} using a {@link PrivateKey}.^
  */
-function decrypt(
+function decryptV2(
   { publicKey, cipherText }: CipherText,
   privateKey: PrivateKey
 ) {
