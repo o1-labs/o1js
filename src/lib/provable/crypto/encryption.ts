0070e22853cdf23ffb1a866097aab4caa95644e3
Florian <florian@technotro.com>
2024-06-26 12:46:21 +0200

    clean up

--- src/lib/provable/crypto/encryption.ts
+++ src/lib/provable/crypto/encryption.ts
@@ -11,104 +11,76 @@ type CipherText = {
 };
 
 /**
+ * Public Key Encryption, using a given array of {@link Field} elements and encrypts it using a {@link PublicKey}.
+ */
+function encryptV2(message: Field[], otherPublicKey: PublicKey) {
+  return encryptInternal(message, otherPublicKey);
+}
+
+/**
+ * Decrypts a {@link CipherText} using a {@link PrivateKey}.
+ */
+function decryptV2(cipherText: CipherText, privateKey: PrivateKey) {
+  return decryptInternal(cipherText, privateKey);
+}
+
+/**
  * @deprecated Use {@link encryptV2} instead. The private key used with this method should _never_ be reused again!
  *
  * Public Key Encryption, using a given array of {@link Field} elements and encrypts it using a {@link PublicKey}.
  */
 function encrypt(message: Field[], otherPublicKey: PublicKey) {
-  // key exchange
-  let privateKey = Provable.witness(Scalar, () => Scalar.random());
-  let publicKey = Group.generator.scale(privateKey);
-  let sharedSecret = otherPublicKey.toGroup().scale(privateKey);
-
-  let sponge = new Poseidon.Sponge();
-  sponge.absorb(sharedSecret.x); // don't think we need y, that's enough entropy
-
-  // encryption
-  let cipherText = [];
-  for (let i = 0; i < message.length; i++) {
-    let keyStream = sponge.squeeze();
-    let encryptedChunk = message[i].add(keyStream);
-    cipherText.push(encryptedChunk);
-    // absorb for the auth tag (two at a time for saving permutations)
-    if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
-    if (i % 2 === 1 || i === message.length - 1) sponge.absorb(cipherText[i]);
-  }
-  // authentication tag
-  let authenticationTag = sponge.squeeze();
-  cipherText.push(authenticationTag);
-
-  return { publicKey, cipherText };
-}
-
-/**
- * Public Key Encryption, using a given array of {@link Field} elements and encrypts it using a {@link PublicKey}.
- */
-function encryptV2(message: Field[], otherPublicKey: PublicKey) {
-  // key exchange
-  let privateKey = Provable.witness(Scalar, () => Scalar.random());
-  let publicKey = Group.generator.scale(privateKey);
-  let sharedSecret = otherPublicKey.toGroup().scale(privateKey);
-
-  let sponge = new Poseidon.Sponge();
-  sponge.absorb(sharedSecret.x); // don't think we need y, that's enough entropy
-
-  // encryption
-  let cipherText = [];
-  for (let i = 0; i < message.length; i++) {
-    let keyStream = sponge.squeeze();
-    let encryptedChunk = message[i].add(keyStream);
-    if (message.length - 1 === i) encryptedChunk = encryptedChunk.add(1);
-    cipherText.push(encryptedChunk);
-    // absorb for the auth tag (two at a time for saving permutations)
-    if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
-    if (i % 2 === 1 || i === message.length - 1) sponge.absorb(cipherText[i]);
-  }
-  // authentication tag
-  let authenticationTag = sponge.squeeze();
-  cipherText.push(authenticationTag);
-
-  return { publicKey, cipherText };
+  return encryptInternal(message, otherPublicKey, false);
 }
 
 /**
  * @deprecated Use {@link decryptV2} instead.
  *
- * Decrypts a {@link CipherText} using a {@link PrivateKey}.^
+ * Decrypts a {@link CipherText} using a {@link PrivateKey}.
  */
-function decrypt(
-  { publicKey, cipherText }: CipherText,
-  privateKey: PrivateKey
+function decrypt(cipherText: CipherText, privateKey: PrivateKey) {
+  return decryptInternal(cipherText, privateKey, false);
+}
+
+function encryptInternal(
+  message: Field[],
+  otherPublicKey: PublicKey,
+  frameBit = true
 ) {
   // key exchange
-  let sharedSecret = publicKey.scale(privateKey.s);
+  let privateKey = Provable.witness(Scalar, () => Scalar.random());
+  let publicKey = Group.generator.scale(privateKey);
+  let sharedSecret = otherPublicKey.toGroup().scale(privateKey);
 
   let sponge = new Poseidon.Sponge();
-  sponge.absorb(sharedSecret.x);
-  let authenticationTag = cipherText.pop();
+  sponge.absorb(sharedSecret.x); // don't think we need y, that's enough entropy
 
-  // decryption
-  let message = [];
-  for (let i = 0; i < cipherText.length; i++) {
+  // encryption
+  let cipherText = [];
+  for (let i = 0; i < message.length; i++) {
     let keyStream = sponge.squeeze();
-    let messageChunk = cipherText[i].sub(keyStream);
-    message.push(messageChunk);
+    let encryptedChunk = message[i].add(keyStream);
+    if (message.length - 1 === i && frameBit)
+      encryptedChunk = encryptedChunk.add(1);
+    cipherText.push(encryptedChunk);
+    // absorb for the auth tag (two at a time for saving permutations)
     if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
-    if (i % 2 === 1 || i === cipherText.length - 1)
-      sponge.absorb(cipherText[i]);
+    if (i % 2 === 1 || i === message.length - 1) sponge.absorb(cipherText[i]);
   }
   // authentication tag
-  sponge.squeeze().assertEquals(authenticationTag!);
+  let authenticationTag = sponge.squeeze();
+  cipherText.push(authenticationTag);
 
-  return message;
+  return { publicKey, cipherText };
 }
 
 /**
- * Decrypts a {@link CipherText} using a {@link PrivateKey}.^
+ * Decrypts a {@link CipherText} using a {@link PrivateKey}.
  */
-function decryptV2(
+function decryptInternal(
   { publicKey, cipherText }: CipherText,
-  privateKey: PrivateKey
+  privateKey: PrivateKey,
+  frameBit = true
 ) {
   // key exchange
   let sharedSecret = publicKey.scale(privateKey.s);
@@ -122,7 +94,8 @@ function decryptV2(
   for (let i = 0; i < cipherText.length; i++) {
     let keyStream = sponge.squeeze();
     let messageChunk = cipherText[i].sub(keyStream);
-    if (cipherText.length - 1 === i) messageChunk = messageChunk.sub(1);
+    if (cipherText.length - 1 === i && frameBit)
+      messageChunk = messageChunk.sub(1);
     message.push(messageChunk);
     if (i % 2 === 1) sponge.absorb(cipherText[i - 1]);
     if (i % 2 === 1 || i === cipherText.length - 1)
