f0156e2a993f7c77ef547921e89a3110dc3f38a9
ymekuria <ymekuria@gmail.com>
2024-07-26 10:41:53 -0600

    Merge branch main into feature/infinity-flag

--- src/lib/mina/actions/batch-reducer.unit-test.ts
+++ src/lib/mina/actions/batch-reducer.unit-test.ts
@@ -86,12 +86,10 @@ class UnsafeAirdrop extends SmartContract {
    *
    * Note: This two-step process is necessary so that multiple users can claim concurrently.
    */
-  @method.returns(MerkleMap.provable)
+  @method.returns(MerkleMap)
   async settleClaims(batch: Batch, proof: BatchProof) {
     // witness merkle map and require that it matches the onchain root
-    let eligibleMap = Provable.witness(MerkleMap.provable, () =>
-      eligible.clone()
-    );
+    let eligibleMap = Provable.witness(MerkleMap, () => eligible.clone());
     this.eligibleRoot.requireEquals(eligibleMap.root);
     this.eligibleLength.requireEquals(eligibleMap.length);
 
--- src/lib/provable/types/provable-derivers.ts
+++ src/lib/provable/types/provable-derivers.ts
@@ -1,4 +1,10 @@
-import { Provable, ProvablePure } from './provable-intf.js';
+import {
+  Provable,
+  ProvableHashable,
+  ProvablePure,
+  ProvableType,
+  ToProvable,
+} from './provable-intf.js';
 import type { Field } from '../wrapped.js';
 import {
   createDerivers,
@@ -7,9 +13,13 @@ import {
   InferJson,
   InferredProvable as GenericInferredProvable,
   IsPure as GenericIsPure,
+  NestedProvable as GenericNestedProvable,
   createHashInput,
   Constructor,
   InferValue,
+  InferJsonNested,
+  InferValueNested,
+  InferProvableNested,
 } from '../../../bindings/lib/provable-generic.js';
 import { Tuple } from '../../util/types.js';
 import { GenericHashInput } from '../../../bindings/lib/generic.js';
@@ -22,6 +32,7 @@ export {
   provablePure,
   provableTuple,
   provableFromClass,
+  provableExtends,
 };
 
 // internal API
@@ -29,9 +40,11 @@ export {
   NonMethods,
   HashInput,
   InferProvable,
+  InferProvableType,
   InferJson,
   InferredProvable,
   IsPure,
+  NestedProvable,
 };
 
 type ProvableExtension<T, TJson = any> = {
@@ -49,6 +62,7 @@ type ProvablePureExtended<T, TValue = any, TJson = any> = ProvablePure<
   ProvableExtension<T, TJson>;
 
 type InferProvable<T> = GenericInferProvable<T, Field>;
+type InferProvableType<T extends ProvableType> = InferProvable<ToProvable<T>>;
 type InferredProvable<T> = GenericInferredProvable<T, Field>;
 type IsPure<T> = GenericIsPure<T, Field>;
 type ProvableInferPureFrom<A, T, V> = IsPure<A> extends true
@@ -57,6 +71,8 @@ type ProvableInferPureFrom<A, T, V> = IsPure<A> extends true
 
 type HashInput = GenericHashInput<Field>;
 const HashInput = createHashInput<Field>();
+
+type NestedProvable = GenericNestedProvable<Field>;
 
 const { provable } = createDerivers<Field>();
 
@@ -70,13 +86,18 @@ function provableTuple<T extends Tuple<any>>(types: T): InferredProvable<T> {
   return provable(types) as any;
 }
 
-function provableFromClass<A, T extends InferProvable<A>>(
+function provableFromClass<
+  A extends NestedProvable,
+  T extends InferProvableNested<Field, A>,
+  V extends InferValueNested<Field, A>,
+  J extends InferJsonNested<Field, A>
+>(
   Class: Constructor<T> & { check?: (x: T) => void; empty?: () => T },
   typeObj: A
 ): IsPure<A> extends true
-  ? ProvablePureExtended<T, InferValue<A>, InferJson<A>>
-  : ProvableExtended<T, InferValue<A>, InferJson<A>> {
-  let raw = provable(typeObj);
+  ? ProvablePureExtended<T, V, J>
+  : ProvableExtended<T, V, J> {
+  let raw: ProvableExtended<T, V, J> = provable(typeObj) as any;
   return {
     sizeInFields: raw.sizeInFields,
     toFields: raw.toFields,
@@ -105,10 +126,46 @@ function provableFromClass<A, T extends InferProvable<A>>(
         ? Class.empty()
         : construct(Class, raw.empty());
     },
-  } satisfies ProvableExtended<T, InferValue<A>, InferJson<A>> as any;
+  } satisfies ProvableExtended<T, V, J> as any;
 }
 
 function construct<Raw, T extends Raw>(Class: Constructor<T>, value: Raw): T {
   let instance = Object.create(Class.prototype);
   return Object.assign(instance, value);
+}
+
+function provableExtends<
+  A extends ProvableHashable<any>,
+  T extends InferProvable<A>,
+  S extends T
+>(S: new (t: T) => S, base: A) {
+  return {
+    sizeInFields() {
+      return base.sizeInFields();
+    },
+    toFields(value: S | T) {
+      return base.toFields(value);
+    },
+    toAuxiliary(value?: S | T) {
+      return base.toAuxiliary(value);
+    },
+    fromFields(fields, aux) {
+      return new S(base.fromFields(fields, aux));
+    },
+    check(value: S | T) {
+      base.check(value);
+    },
+    toValue(value: S | T) {
+      return base.toValue(value);
+    },
+    fromValue(value) {
+      return new S(base.fromValue(value));
+    },
+    empty() {
+      return new S(base.empty());
+    },
+    toInput(value: S | T) {
+      return base.toInput(value);
+    },
+  } satisfies ProvableHashable<S, InferValue<A>>;
 }
