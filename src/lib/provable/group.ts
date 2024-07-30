8dd7d9dc87bb236c8d6d6ebbd1eb4284dede771e
Florian <florian@technotro.com>
2024-07-30 11:46:04 +0200

    scale zero

--- src/lib/provable/group.ts
+++ src/lib/provable/group.ts
@@ -181,11 +181,16 @@ class Group {
     let scalar = Scalar.from(s);
 
     if (isConstant(this) && scalar.isConstant()) {
-      let g_proj = Pallas.scale(toProjective(this), scalar.toBigInt());
-      return fromProjective(g_proj);
+      // edge case there this is zero/at infinity
+      if (this.isZero().toBoolean()) return this;
+
+      let gProj = Pallas.scale(toProjective(this), scalar.toBigInt());
+      return fromProjective(gProj);
     } else {
-      let result = scaleShifted(this, scalar);
-      return new Group(result);
+      // edge case where this is zero/point at infinity
+      let point = Provable.if(this.isZero(), Group, Group.generator, this);
+      let result = scaleShifted(point, scalar);
+      return Provable.if(this.isZero(), Group.zero, new Group(result));
     }
   }
 
