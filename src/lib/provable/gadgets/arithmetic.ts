2a2f2babd8ecbf03960cb3415a86d5fe6e4adbb6
ymekuria <ymekuria@gmail.com>
2024-07-29 18:25:33 -0600

    fix(arithmetic.ts): correct the range check for nBits parameter to ensure it is less than 255 instead of less than or equal to 255

--- src/lib/provable/gadgets/arithmetic.ts
+++ src/lib/provable/gadgets/arithmetic.ts
@@ -8,7 +8,7 @@ export { divMod32, addMod32 };
 
 function divMod32(n: Field, nBits = 64) {
   assert(
-    nBits >= 0 && nBits <= 255,
+    nBits >= 0 && nBits < 255,
     `nBits must be in the range [0, 255], got ${nBits}`
   );
   const quotientBits = Math.max(0, nBits - 32);
