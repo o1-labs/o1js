8aaf007cb4fe9358ad4892ba790ba5d76d4cc19c
Florian <florian@technotro.com>
2024-08-23 10:19:34 +0200

    move example

--- src/examples/zkprogram/program-with-auxiliary-output.ts
+++ src/examples/zkprogram/program-with-auxiliary-output.ts
@@ -12,33 +12,30 @@ import {
 } from 'o1js';
 
 let MyProgram = ZkProgram({
-  name: 'example-with-aux-output',
-  publicInput: Field,
+  name: 'example-with-auxiliary-output',
   publicOutput: Field,
   auxiliaryOutput: Field,
   methods: {
     baseCase: {
       privateInputs: [],
-      async method(publicInput: Field) {
+      async method() {
         return {
-          publicOutput: Provable.witness(Field, () => Field(9)),
-          aux: Provable.witness(Field, () => Field(5)),
+          publicOutput: Field(9),
+          auxiliaryOutput: Field(5),
         };
       },
     },
   },
 });
 // type sanity checks
-MyProgram.publicInputType satisfies typeof Field;
 MyProgram.publicOutputType satisfies typeof Field;
 MyProgram.auxiliaryOutputType satisfies typeof Field;
 
 await MyProgram.compile({ cache: Cache.None, forceRecompile: true });
 
-let { proof, auxiliaryOutput } = await MyProgram.baseCase(Field(1));
-Provable.log(auxiliaryOutput);
-Provable.log(proof.publicInput);
-Provable.log(proof.publicOutput);
+let { proof, auxiliaryOutput } = await MyProgram.baseCase();
+assert(auxiliaryOutput.equals(5).toBoolean());
+assert(proof.publicOutput.equals(9).toBoolean());
 
 let result = await MyProgram.verify(proof);
 assert(result, 'cannot verify proof');
