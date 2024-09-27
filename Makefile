00f78cdd019ce83bda3ac275f5ddf96d0038ff8c
mike dupont <mike.dupont@introspector.local>
2024-09-27 11:56:16 -0400

    now starting to run locally, try github

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 unit-tests:
-	docker compose build unit-tests
-	TESTS="/app/dist/node/lib/util/base58.unit-test.js" docker compose up unit-tests
+	docker compose build unit-tests-local
+	TESTS="/app/dist/node/lib/util/base58.unit-test.js" docker compose up unit-tests-local
 
 test:
 	docker compose build mina-local-network
--- run-all-unit-tests.sh
+++ run-all-unit-tests.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-set +e
+set -e
 set -x
 export OUTPUT_DIR="/tmp/perf"
+rm -rf /tmp/perf/ 
 export NODE_OUTPUT_DIR="${OUTPUT_DIR}/node"
 export CLINIC_OUTPUT_DIR="${OUTPUT_DIR}/clinic"
 export NO_INSIGHT=true
 export NODE_OPTIONS="--experimental-vm-modules --perf-basic-prof --enable-source-maps --stack-trace-limit=1000 --report-dir $NODE_OUTPUT_DIR    --perf-prof-unwinding-info --perf-prof"
 export NO_INSIGHT=true
-export JESTOPTS=--collectCoverage
-export NODEOPT1="--prof  --expose-gc"
-
-
+export NODEOPT1="--test"
 # setup perf permissions
-echo 2 > /proc/sys/kernel/perf_event_paranoid
+echo 2 > /proc/sys/kernel/perf_event_paranoid || echo run as root!
+
+
+## hacks to update the docker image
+ls -latr /opt/introspector/test/package.json /app/package.json
+md5sum /opt/introspector/test/package.json /app/package.json
+cp /opt/introspector/test/package.json /app/
+cp /opt/introspector/test/*.json /app/ || echo ok
+cp /opt/introspector/test/*.yaml /app/ || echo ok
+cp /opt/introspector/test/*.yml /app/ || echo ok
+rm /app/jest.config.js || echo ok
+ls -latr /opt/introspector/test/package.json /app/package.json
+md5sum /opt/introspector/test/package.json /app/package.json
+######
 
 mkdir "${OUTPUT_DIR}"
 mkdir "${NODE_OUTPUT_DIR}"
 mkdir "${CLINIC_OUTPUT_DIR}"
 pnpm install -g clinic
-
 cd /app/
 export SOURCE_DIR=/app/src
-# limit to only working tests
-#TESTS="${SOURCE_DIR}/lib/provable/test/merkle-list.test.ts ${SOURCE_DIR}/lib/provable/test/merkle-tree.test.ts ${SOURCE_DIR}/lib/provable/test/scalar.test.ts  ${SOURCE_DIR}/lib/provable/test/merkle-map.test.ts  ${SOURCE_DIR}/lib/provable/test/provable.test.ts  ${SOURCE_DIR}/lib/provable/test/primitives.test.ts  ${SOURCE_DIR}/lib/provable/test/group.test.ts  ${SOURCE_DIR}/lib/provable/test/int.test.ts  ${SOURCE_DIR}/lib/mina/precondition.test.ts"
-#${SOURCE_DIR}/lib/mina/token.test.ts"
-# FIXME this all tests is not used because many of them fail.
-ALL_TESTS=`ls -b ${SOURCE_DIR}/lib/provable/test/*.test.ts ${SOURCE_DIR}/lib/mina/*.test.ts `
-
-
 run_test() {
     testname=$1
     export MULTIPLE="${testname}"
@@ -37,21 +40,30 @@ run_test() {
     mkdir -p "${OUTPUT_DIR2}log/"
     mkdir -p "${OUTPUT_DIR2}coverage/"
 
-    clinic flame -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-flame.txt" 2>&1
-    clinic doctor -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-doctor.txt" 2>&1
-    clinic bubbleprof -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-bubble.txt" 2>&1
-    clinic heapprofiler -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}"  | tee "${OUTPUT_DIR2}clinic-heap.txt" 2>&1    
-    perf record -g -o "${testname}.perf.data" -F 999 --call-graph dwarf node  $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" > "${testname}.reportout.txt" 2>&1
-    perf archive ${testname}.perf.data
+    echo plain test
+    node  "${NODEOPT1}"   "${MULTIPLE}"
+
+    echo run again for capture
+    node  "${NODEOPT1}"   "${MULTIPLE}" | tee "${testname}.report_plain.txt" 2>&1
+
+    echo run clinic
+    
+    clinic flame -- node "${NODEOPT1}"   "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-flame.txt" 2>&1
+    clinic doctor -- node "${NODEOPT1}"    "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-doctor.txt" 2>&1
+    clinic bubbleprof -- node "${NODEOPT1}"  "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-bubble.txt" 2>&1
+    clinic heapprofiler --open=false -- node "${NODEOPT1}"   "${MULTIPLE}"  | tee "${OUTPUT_DIR2}clinic-heap.txt" 2>&1
+
+    echo run perf
+    perf record -g -o "${testname}.perf.data" -F 999 --call-graph dwarf node  "${NODEOPT1}"   "${MULTIPLE}" > "${testname}.reportout.txt" 2>&1
+    perf archive "${testname}.perf.data"
     perf report -i "${perfdata}" --verbose --stdio --header > "${perfdata}.header.txt" 2>&1
     perf report -i "${perfdata}" --verbose --stdio --stats > "${perfdata}.stats.txt" 2>&1
     perf script -F +pid -i "${perfdata}"  > "${perfdata}.script.txt" 2>&1
     perf report --stdio -i "${perfdata}"  > "${perfdata}.report.txt" 2>&1
     ls -latr jit*.dump
-    rm -r jit*.dump
-    
-    mv ${testname}.perf.data "${OUTPUT_DIR2}/perf_data"
-    mv "${testname}.*.txt" "${OUTPUT_DIR2}/perf_data"
+    rm -r jit*.dump    
+    mv ${testname}.perf.data "${OUTPUT_DIR2}perf_data"
+    mv "${testname}.*.txt" "${OUTPUT_DIR2}perf_data"
     mv .clinic/* "${OUTPUT_DIR2}clinic/"
     mv coverage/* "${OUTPUT_DIR2}coverage/"
     mv  *.log "${OUTPUT_DIR2}log/"    
