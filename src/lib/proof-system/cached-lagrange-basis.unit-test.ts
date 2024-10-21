7958bda0393c66a7910582763e0eee21fe4ade52
Test <test@example.com>
2024-10-21 09:49:52 -0400

    updating changelog and adding comment to unit test

--- src/lib/proof-system/cached-lagrange-basis.unit-test.ts
+++ src/lib/proof-system/cached-lagrange-basis.unit-test.ts
@@ -47,6 +47,15 @@ describe('Compiling a program with a cache', () => {
     await fs.rm(__cacheDirname, { recursive: true });
   });
 
+  /**
+   * This test is a regression test for https://github.com/o1-labs/o1js/issues/1869
+   * It ensures that the lagrange basis cache is accessed properly.  If the file system cache is not
+   * read during compile, that means that the lagrange basis was returned from WASM on the first attempt.
+   *
+   * This is not necessarily a problem.  If the WASM code is updated such that we expect the LB to be
+   * returned on the first try, and we explicitly skip the file system cache, then this test can be
+   * safely removed.  Otherwise, a failure here probably indicates a performance regression.
+   */
   it('should attempt to read lagrange basis from the cache during compile', async () => {
     cache.lagrangeBasisReadCount = 0;
     await exampleProgram.compile({ cache });
