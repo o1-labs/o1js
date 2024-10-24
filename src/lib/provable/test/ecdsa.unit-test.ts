a0812d7b117c6c797dc36f961324d51cca83081a
Gregor <gregor.mitscha-baude@gmx.at>
2024-10-24 22:25:47 +0200

    test ecdsa against web crypto

--- src/lib/provable/test/ecdsa.unit-test.ts
+++ src/lib/provable/test/ecdsa.unit-test.ts
@@ -24,12 +24,56 @@ import {
 } from '../../testing/equivalent.js';
 import { Bool } from '../bool.js';
 import { Random } from '../../testing/random.js';
+import { bytesToBigInt } from '../../../bindings/crypto/bigint-helpers.js';
+import { expect } from 'expect';
 
 // quick tests
 const Secp256k1 = createCurveAffine(CurveParams.Secp256k1);
 const Secp256r1 = createCurveAffine(CurveParams.Secp256r1);
 const Pallas = createCurveAffine(CurveParams.Pallas);
 const Vesta = createCurveAffine(CurveParams.Vesta);
+
+// secp256r1 test against web crypto API
+{
+  // generate a key pair
+  let { privateKey, publicKey } = await crypto.subtle.generateKey(
+    { name: 'ECDSA', namedCurve: 'P-256' },
+    true,
+    ['sign']
+  );
+
+  // extract public and private key to bigints
+  let pkBuffer = await crypto.subtle.exportKey('raw', publicKey);
+  let x = bytesToBigIntBE(pkBuffer.slice(1, 33));
+  let y = bytesToBigIntBE(pkBuffer.slice(33));
+  let pk = { x, y };
+
+  var skJwk = await crypto.subtle.exportKey('jwk', privateKey);
+  let sk = bytesToBigIntBE(fromBase64Url(skJwk.d!));
+
+  // sanity check: we extracted keys correctly
+  expect(Secp256r1.from(pk)).toEqual(Secp256r1.scale(Secp256r1.one, sk));
+
+  // sign a message using web crypto
+  let message = new TextEncoder().encode('hello world');
+
+  let sigBuffer = await crypto.subtle.sign(
+    { name: 'ECDSA', hash: 'SHA-256' },
+    privateKey,
+    message
+  );
+  let r = bytesToBigIntBE(sigBuffer.slice(0, 32));
+  let s = bytesToBigIntBE(sigBuffer.slice(32));
+  let signature = Ecdsa.Signature.from({ r, s });
+
+  // check that we can verify the signature on the message hash
+  let msgHash = await crypto.subtle.digest('SHA-256', message);
+  let m = Field3.from(Secp256r1.Scalar.mod(bytesToBigIntBE(msgHash)));
+
+  let ok = Ecdsa.verify(Secp256r1, signature, m, Point.from(pk));
+  assert(ok, 'web crypto signature verifies');
+}
+
 let curves = [Secp256k1, Secp256r1, Pallas, Vesta];
 
 for (let Curve of curves) {
@@ -223,3 +267,16 @@ let csNoEndo = (await programNoEndo.analyzeMethods()).ecdsa;
 console.timeEnd('ecdsa verify, no endomorphism (build constraint system)');
 
 console.log(csNoEndo.summary());
+
+// helpers
+
+function bytesToBigIntBE(bytes: ArrayBuffer | Uint8Array | number[]) {
+  return bytesToBigInt([...new Uint8Array(bytes)].reverse());
+}
+
+function fromBase64Url(b64url: string): Uint8Array {
+  let b64 =
+    b64url.replace(/-/g, '+').replace(/_/g, '/') +
+    '===='.slice(0, (4 - (b64url.length % 4)) % 4);
+  return Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
+}
