c29a4ec6f3b20d21b33d716f9598a5e5a2c3732e
querolita <anais.querol@gmail.com>
2024-11-18 15:29:42 +0100

    test with 2 chunks

--- src/examples/zkprogram/program-with-chunking.ts
+++ src/examples/zkprogram/program-with-chunking.ts
@@ -1,27 +1,35 @@
-import { Field, ZkProgram, verify } from 'o1js';
+import { Field, Cache, Gadgets, ZkProgram } from 'o1js';
 
 let MyProgram = ZkProgram({
-  chunks: 1,
-  overrideWrapDomain: 0,
+  chunks: 2,
+  overrideWrapDomain: 1,
   name: 'example-with-chunking',
   publicOutput: Field,
 
   methods: {
     baseCase: {
-      privateInputs: [],
-      async method() {
-        return Field(0);
+      privateInputs: [Field],
+      async method(input: Field) {
+        for (let i = 0; i < 1 << 16; i++) {
+          Gadgets.rangeCheck64(Field(input).add(Field(i)));
+        }
+        // The above generates 2^16+2^15 rows which needs to be split into 2 chunks
+        return {
+          publicOutput: Field(0),
+        };
       },
     },
   },
 });
 
+console.log(await MyProgram.analyzeMethods());
+
 console.log('compiling MyProgram...');
-let { verificationKey } = await MyProgram.compile();
+await MyProgram.compile({ cache: Cache.None });
 
 console.log('proving base case...');
-let proof = await MyProgram.baseCase();
+let { proof } = await MyProgram.baseCase(Field(0));
 
 console.log('verify...');
-let ok = await verify(proof.toJSON(), verificationKey);
+let ok = await MyProgram.verify(proof);
 console.log('ok?', ok);
