d9588a902f663c100ed6430e916e7d7055c8912a
ymekuria <ymekuria@gmail.com>
2024-11-21 01:58:28 +0800

    refactor(program-with-non-pure-input.ts): refactor code to destructure proof object for better readability and consistency in accessing proof properties

--- src/examples/zkprogram/program-with-non-pure-input.ts
+++ src/examples/zkprogram/program-with-non-pure-input.ts
@@ -55,29 +55,29 @@ console.log('compiling NonPureIOprogram...');
 await NonPureIOprogram.compile();
 console.log('compile done');
 let input = new MyStruct({ label: 'input', value: Field(5) });
-
-let result1 = await NonPureIOprogram.baseCase(input);
-let isProof1Valid = await NonPureIOprogram.verify(result1.proof);
+let proof;
+({ proof } = await NonPureIOprogram.baseCase(input));
+let isProof1Valid = await NonPureIOprogram.verify(proof);
 assert(isProof1Valid, 'proof not valid!');
-assert(result1.proof.publicOutput.label === 'in-circuit');
-console.log('proof IO', result1.proof);
+assert(proof.publicOutput.label === 'in-circuit');
+console.log('i/o proof', proof);
 
 console.log('compiling NonPureOutputProgram...');
 await NonPureOutputProgram.compile();
 console.log('compile done');
 
-let result2 = await NonPureOutputProgram.baseCase();
-let isProof2Valid = await NonPureOutputProgram.verify(result2.proof);
+({ proof } = await NonPureOutputProgram.baseCase());
+let isProof2Valid = await NonPureOutputProgram.verify(proof);
 assert(isProof2Valid, 'proof not valid!');
-assert(result2.proof.publicOutput.label === 'output');
-console.log('proof O', result2.proof);
+assert(proof.publicOutput.label === 'output');
+console.log('output proof', proof);
 
 console.log('compiling NonPureInputProgram...');
 await NonPureInputProgram.compile();
 console.log('compile done');
 
-let result3 = await NonPureInputProgram.baseCase(input);
-let isProof3Valid = await NonPureInputProgram.verify(result3.proof);
+({ proof } = await NonPureInputProgram.baseCase(input));
+let isProof3Valid = await NonPureInputProgram.verify(proof);
 assert(isProof3Valid, 'proof not valid!');
-assert(result3.proof.publicInput.label === 'input');
-console.log('proof O', result3.proof);
+assert(proof.publicInput.label === 'input');
+console.log('input proof', proof);
