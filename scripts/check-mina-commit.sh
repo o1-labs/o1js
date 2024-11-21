99a9178374c4d4a7326ee6638d5ace48fc5deb91
Florian <florian@technotro.com>
2024-11-21 14:39:12 +0700

    temp debug logs

--- scripts/check-mina-commit.sh
+++ scripts/check-mina-commit.sh
@@ -6,17 +6,24 @@ if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <target-branch>"
   exit 1
 fi
-
+echo "1"
 cd src/mina
 
 git fetch --depth=1 origin $1
+echo "2"
 
 CURR=$(git rev-parse HEAD)
+echo "3"
 
 BRANCH=$1
+echo "4"
 
 function in_branch {
+  echo "5"
+
   if git rev-list origin/$1 | grep -q $CURR; then
+    echo "6"
+
     echo "Mina submodule commit is an ancestor of $1"
     true
   else
