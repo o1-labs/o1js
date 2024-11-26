03ce39809d4bb27ab71a0c2e435abedf3f890645
Geometer1729 <16kuhnb@gmail.com>
2024-11-26 09:26:57 -0500

    non-change to test ci

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     dune-nix.inputs.flake-utils.follows = "flake-utils";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
