dbebf51a93a8a7c8a391812ceaa94107a0cf5b55
Gregor <gregor.mitscha-baude@gmx.at>
2024-11-28 13:22:19 +0100

    allow test to run in web

--- src/tests/inductive-proofs-internal.ts
+++ src/tests/inductive-proofs-internal.ts
@@ -1,6 +1,5 @@
 import { Field, ZkProgram, assert, Provable } from 'o1js';
 import { tic, toc } from '../examples/utils/tic-toc.js';
-import { expect } from 'expect';
 
 let log: string[] = [];
 
@@ -59,4 +58,8 @@ assert(await MaxProofsVerifiedTwo.verify(proof), 'Proof is not valid');
 
 proof.publicOutput.assertEquals(15);
 
-expect(log).toEqual(['mergeTwo', 'baseCase', 'mergeOne', 'baseCase']);
+assert(log.length === 4, 'log.length === 4');
+assert(log[0] === 'mergeTwo', 'log[0] === "mergeTwo"');
+assert(log[1] === 'baseCase', 'log[1] === "baseCase"');
+assert(log[2] === 'mergeOne', 'log[2] === "mergeOne"');
+assert(log[3] === 'baseCase', 'log[3] === "baseCase"');
