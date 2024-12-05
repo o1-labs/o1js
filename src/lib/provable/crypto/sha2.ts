62c4ae54c9ec1e340ac422d32f659617ad17d837
querolita <anais.querol@gmail.com>
2024-12-05 17:43:49 +0100

    prettify and sigma for 256 and 512

--- src/lib/provable/crypto/sha2.ts
+++ src/lib/provable/crypto/sha2.ts
@@ -80,87 +80,28 @@ const SHA2Constants = {
     0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
   ],
   // constants for SHA2-384 and SHA2-512 §4.2.3
+  // prettier-ignore
   K384_512: [
-    0x428a2f98d728ae22n,
-    0x7137449123ef65cdn,
-    0xb5c0fbcfec4d3b2fn,
-    0xe9b5dba58189dbbcn,
-    0x3956c25bf348b538n,
-    0x59f111f1b605d019n,
-    0x923f82a4af194f9bn,
-    0xab1c5ed5da6d8118n,
-    0xd807aa98a3030242n,
-    0x12835b0145706fben,
-    0x243185be4ee4b28cn,
-    0x550c7dc3d5ffb4e2n,
-    0x72be5d74f27b896fn,
-    0x80deb1fe3b1696b1n,
-    0x9bdc06a725c71235n,
-    0xc19bf174cf692694n,
-    0xe49b69c19ef14ad2n,
-    0xefbe4786384f25e3n,
-    0x0fc19dc68b8cd5b5n,
-    0x240ca1cc77ac9c65n,
-    0x2de92c6f592b0275n,
-    0x4a7484aa6ea6e483n,
-    0x5cb0a9dcbd41fbd4n,
-    0x76f988da831153b5n,
-    0x983e5152ee66dfabn,
-    0xa831c66d2db43210n,
-    0xb00327c898fb213fn,
-    0xbf597fc7beef0ee4n,
-    0xc6e00bf33da88fc2n,
-    0xd5a79147930aa725n,
-    0x06ca6351e003826fn,
-    0x142929670a0e6e70n,
-    0x27b70a8546d22ffcn,
-    0x2e1b21385c26c926n,
-    0x4d2c6dfc5ac42aedn,
-    0x53380d139d95b3dfn,
-    0x650a73548baf63den,
-    0x766a0abb3c77b2a8n,
-    0x81c2c92e47edaee6n,
-    0x92722c851482353bn,
-    0xa2bfe8a14cf10364n,
-    0xa81a664bbc423001n,
-    0xc24b8b70d0f89791n,
-    0xc76c51a30654be30n,
-    0xd192e819d6ef5218n,
-    0xd69906245565a910n,
-    0xf40e35855771202an,
-    0x106aa07032bbd1b8n,
-    0x19a4c116b8d2d0c8n,
-    0x1e376c085141ab53n,
-    0x2748774cdf8eeb99n,
-    0x34b0bcb5e19b48a8n,
-    0x391c0cb3c5c95a63n,
-    0x4ed8aa4ae3418acbn,
-    0x5b9cca4f7763e373n,
-    0x682e6ff3d6b2b8a3n,
-    0x748f82ee5defb2fcn,
-    0x78a5636f43172f60n,
-    0x84c87814a1f0ab72n,
-    0x8cc702081a6439ecn,
-    0x90befffa23631e28n,
-    0xa4506cebde82bde9n,
-    0xbef9a3f7b2c67915n,
-    0xc67178f2e372532bn,
-    0xca273eceea26619cn,
-    0xd186b8c721c0c207n,
-    0xeada7dd6cde0eb1en,
-    0xf57d4f7fee6ed178n,
-    0x06f067aa72176fban,
-    0x0a637dc5a2c898a6n,
-    0x113f9804bef90daen,
-    0x1b710b35131c471bn,
-    0x28db77f523047d84n,
-    0x32caab7b40c72493n,
-    0x3c9ebe0a15c9bebcn,
-    0x431d67c49c100d4cn,
-    0x4cc5d4becb3e42b6n,
-    0x597f299cfc657e2an,
-    0x5fcb6fab3ad6faecn,
-    0x6c44198c4a475817n,
+    0x428a2f98d728ae22n, 0x7137449123ef65cdn, 0xb5c0fbcfec4d3b2fn, 0xe9b5dba58189dbbcn,
+    0x3956c25bf348b538n, 0x59f111f1b605d019n, 0x923f82a4af194f9bn, 0xab1c5ed5da6d8118n,
+    0xd807aa98a3030242n, 0x12835b0145706fben, 0x243185be4ee4b28cn, 0x550c7dc3d5ffb4e2n,
+    0x72be5d74f27b896fn, 0x80deb1fe3b1696b1n, 0x9bdc06a725c71235n, 0xc19bf174cf692694n,
+    0xe49b69c19ef14ad2n, 0xefbe4786384f25e3n, 0x0fc19dc68b8cd5b5n, 0x240ca1cc77ac9c65n,
+    0x2de92c6f592b0275n, 0x4a7484aa6ea6e483n, 0x5cb0a9dcbd41fbd4n, 0x76f988da831153b5n,
+    0x983e5152ee66dfabn, 0xa831c66d2db43210n, 0xb00327c898fb213fn, 0xbf597fc7beef0ee4n,
+    0xc6e00bf33da88fc2n, 0xd5a79147930aa725n, 0x06ca6351e003826fn, 0x142929670a0e6e70n,
+    0x27b70a8546d22ffcn, 0x2e1b21385c26c926n, 0x4d2c6dfc5ac42aedn, 0x53380d139d95b3dfn,
+    0x650a73548baf63den, 0x766a0abb3c77b2a8n, 0x81c2c92e47edaee6n, 0x92722c851482353bn,
+    0xa2bfe8a14cf10364n, 0xa81a664bbc423001n, 0xc24b8b70d0f89791n, 0xc76c51a30654be30n,
+    0xd192e819d6ef5218n, 0xd69906245565a910n, 0xf40e35855771202an, 0x106aa07032bbd1b8n,
+    0x19a4c116b8d2d0c8n, 0x1e376c085141ab53n, 0x2748774cdf8eeb99n, 0x34b0bcb5e19b48a8n,
+    0x391c0cb3c5c95a63n, 0x4ed8aa4ae3418acbn, 0x5b9cca4f7763e373n, 0x682e6ff3d6b2b8a3n,
+    0x748f82ee5defb2fcn, 0x78a5636f43172f60n, 0x84c87814a1f0ab72n, 0x8cc702081a6439ecn,
+    0x90befffa23631e28n, 0xa4506cebde82bde9n, 0xbef9a3f7b2c67915n, 0xc67178f2e372532bn,
+    0xca273eceea26619cn, 0xd186b8c721c0c207n, 0xeada7dd6cde0eb1en, 0xf57d4f7fee6ed178n,
+    0x06f067aa72176fban, 0x0a637dc5a2c898a6n, 0x113f9804bef90daen, 0x1b710b35131c471bn,
+    0x28db77f523047d84n, 0x32caab7b40c72493n, 0x3c9ebe0a15c9bebcn, 0x431d67c49c100d4cn,
+    0x4cc5d4becb3e42b6n, 0x597f299cfc657e2an, 0x5fcb6fab3ad6faecn, 0x6c44198c4a475817n,
   ],
   // initial hash values for SHA2-224 §5.3.2
   H224: [
@@ -173,26 +114,16 @@ const SHA2Constants = {
     0x1f83d9ab, 0x5be0cd19,
   ],
   // initial hash values for SHA2-384 §5.3.4
+  // prettier-ignore
   H384: [
-    0xcbbb9d5dc1059ed8n,
-    0x629a292a367cd507n,
-    0x9159015a3070dd17n,
-    0x152fecd8f70e5939n,
-    0x67332667ffc00b31n,
-    0x8eb44a8768581511n,
-    0xdb0c2e0d64f98fa7n,
-    0x47b5481dbefa4fa4n,
+    0xcbbb9d5dc1059ed8n, 0x629a292a367cd507n, 0x9159015a3070dd17n, 0x152fecd8f70e5939n,
+    0x67332667ffc00b31n, 0x8eb44a8768581511n, 0xdb0c2e0d64f98fa7n, 0x47b5481dbefa4fa4n,
   ],
   // initial hash values for SHA2-512 §5.3.5
+  // prettier-ignore
   H512: [
-    0x6a09e667f3bcc908n,
-    0xbb67ae8584caa73bn,
-    0x3c6ef372fe94f82bn,
-    0xa54ff53a5f1d36f1n,
-    0x510e527fade682d1n,
-    0x9b05688c2b3e6c1fn,
-    0x1f83d9abfb41bd6bn,
-    0x5be0cd19137e2179n,
+    0x6a09e667f3bcc908n, 0xbb67ae8584caa73bn, 0x3c6ef372fe94f82bn, 0xa54ff53a5f1d36f1n,
+    0x510e527fade682d1n, 0x9b05688c2b3e6c1fn, 0x1f83d9abfb41bd6bn, 0x5be0cd19137e2179n,
   ],
 };
 
@@ -566,7 +497,10 @@ function sigma<T extends UInt32 | UInt64>(
   bits: TupleN<number, 3>,
   firstShifted = false
 ): T {
-  if (u.isConstant()) return sigmaSimple(u, bits, firstShifted);
+  if (u.isConstant() || !isShort()) return sigmaSimple(u, bits, firstShifted);
+
+  // When T is UInt64, 64-bit rotation is natively supported in the gadgets.
+  // However, 32-bit rotation is not natively supported, thus the following:
 
   let [r0, r1, r2] = bits; // TODO assert bits are sorted
   let x = u.value;
@@ -633,5 +567,5 @@ function sigma<T extends UInt32 | UInt64>(
   // proof that xRotR0, xRotR1, xRotR2 < 2^32, which implies x0 < 2^d0, x1 < 2^d1, x2 < 2^d2
   return UInt32.Unsafe.fromField(xRotR0)
     .xor(UInt32.Unsafe.fromField(xRotR1))
-    .xor(UInt32.Unsafe.fromField(xRotR2));
+    .xor(UInt32.Unsafe.fromField(xRotR2)) as T;
 }
