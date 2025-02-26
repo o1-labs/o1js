2d2c79234878d9a64fd34f7abf95d463ca6c7dd3
boray <bsaygilier33@gmail.com>
2025-02-26 12:52:12 +0300

    feat: export all test utils

--- src/lib/testing/testing.ts
+++ src/lib/testing/testing.ts
@@ -1,15 +1,51 @@
-import {
-  test as test_,
-  Random as Random_,
-  sample as sample_,
-  withHardCoded as withHardCoded_,
-} from './property.js';
+import * as Property from './property.js';
+import * as Equivalent from './equivalent.js';
+import * as ConstraintSystem from './constraint-system.js';
 
 export { Testing };
 
 namespace Testing {
-  export let test = test_;
-  export let Random = Random_;
-  export let sample = sample_;
-  export let withHardCoded = withHardCoded_;
+  export let test = Property.test;
+  export let Random = Property.Random;
+  export let sample = Property.sample;
+  export let withHardCoded = Property.withHardCoded;
+
+  export let equivalent = Equivalent.equivalent;
+  export let equivalentProvable = Equivalent.equivalentProvable;
+  export let equivalentAsync = Equivalent.equivalentAsync;
+  export let oneOf = Equivalent.oneOf;
+  export let throwError = Equivalent.throwError;
+  export let handleErrors = Equivalent.handleErrors;
+  export let defaultAssertEqual = Equivalent.defaultAssertEqual;
+  export let id = Equivalent.id;
+  export let spec = Equivalent.spec;
+  export let field = Equivalent.field;
+  export let fieldWithRng = Equivalent.fieldWithRng;
+  export let bigintField = Equivalent.bigintField;
+  export let bool = Equivalent.bool;
+  export let boolean = Equivalent.boolean;
+  export let unit = Equivalent.unit;
+  export let array = Equivalent.array;
+  export let record = Equivalent.record;
+  export let map = Equivalent.map;
+  export let onlyIf = Equivalent.onlyIf;
+  export let fromRandom = Equivalent.fromRandom;
+  export let first = Equivalent.first;
+  export let second = Equivalent.second;
+  export let constant = Equivalent.constant;
+
+  export let constraintSystem = ConstraintSystem.constraintSystem;
+  export let not = ConstraintSystem.not;
+  export let and = ConstraintSystem.and;
+  export let or = ConstraintSystem.or;
+  export let fulfills = ConstraintSystem.fulfills;
+  export let equals = ConstraintSystem.equals;
+  export let contains = ConstraintSystem.contains;
+  export let allConstant = ConstraintSystem.allConstant;
+  export let ifNotAllConstant = ConstraintSystem.ifNotAllConstant;
+  export let isEmpty = ConstraintSystem.isEmpty;
+  export let withoutGenerics = ConstraintSystem.withoutGenerics;
+  export let print = ConstraintSystem.print;
+  export let repeat = ConstraintSystem.repeat;
+  export type ConstraintSystemTest = ConstraintSystem.ConstraintSystemTest;
 }
