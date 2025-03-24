dd318eb5d946a6f96cac605e23555419170a7807
boray <bsaygilier33@gmail.com>
2025-03-24 11:54:42 +0300

    format: fix

--- src/testing/copyBindings.js
+++ src/testing/copyBindings.js
@@ -1,10 +1,7 @@
 import { copyFromTo } from '../build/utils.js';
 
 await copyFromTo(
-    [
-      '../../dist/node/bindings/compiled'
-    ],
-    '../../dist/node/bindings',
-    'dist/node/bindings'
-  );
-  
+  ['../../dist/node/bindings/compiled'],
+  '../../dist/node/bindings',
+  'dist/node/bindings'
+);
