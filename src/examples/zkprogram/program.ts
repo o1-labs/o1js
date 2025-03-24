fdad55d9fb3b4baff376c0690350540533996794
Florian <florian@technotro.com>
2025-03-25 02:15:47 +0700

    fix example

--- src/examples/zkprogram/program.ts
+++ src/examples/zkprogram/program.ts
@@ -1,15 +1,28 @@
-import {
-  SelfProof,
-  Field,
-  ZkProgram,
-  verify,
-  Proof,
-  JsonProof,
-  Provable,
-  Empty,
-  Cache,
-  VerificationKey,
-} from 'o1js';
-let vk = VerificationKey.dummySync();
-console.log(vk.data);
-console.log(vk.hash.toString());
+import { Field, ZkProgram, verify } from 'o1js';
+
+let MyProgram = ZkProgram({
+  name: 'example-with-output',
+  publicOutput: Field,
+  methods: {
+    baseCase: {
+      privateInputs: [],
+      async method() {
+        return {
+          publicOutput: Field(1),
+        };
+      },
+    },
+  },
+});
+
+console.time('compile (with cache)');
+let { verificationKey } = await MyProgram.compile();
+console.timeEnd('compile (with cache)');
+
+console.time('proving');
+let result = await MyProgram.baseCase();
+console.timeEnd('proving');
+
+console.log('verifying');
+let ok = await verify(result.proof, verificationKey);
+console.log('ok', ok);
