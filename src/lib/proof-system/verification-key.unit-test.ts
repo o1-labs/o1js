c9f97953c57726754d8ab6669bb374e5b95d9c71
Florian <florian@technotro.com>
2025-03-25 12:20:04 +0700

    unused import

--- src/lib/proof-system/verification-key.unit-test.ts
+++ src/lib/proof-system/verification-key.unit-test.ts
@@ -1,6 +1,5 @@
 import { assert } from 'console';
 import { VerificationKey } from './verification-key.js';
-import { Provable } from '../provable/provable.js';
 
 console.log('verification key consistency check (generated and cached)');
 let generated = await VerificationKey.dummy();
