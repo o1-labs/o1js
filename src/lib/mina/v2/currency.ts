8fd34ef52b8b2a1b667e8513ce999aef622d5eb6
Florian <florian@technotro.com>
2025-03-30 16:36:46 +0700

    tbd

--- src/lib/mina/v2/currency.ts
+++ src/lib/mina/v2/currency.ts
@@ -1,11 +1,13 @@
-import { UInt64 } from '../../provable/int.js';
+import { Int64, UInt64 } from '../../provable/int.js';
 
-export { MinaAmount };
+export { MinaAmount, BalanceChange };
 
 type MinaAmount = UInt64;
 const MinaAmount = {
-  fromMINA: (amount: bigint): MinaAmount => {
-    return UInt64.from(amount * BigInt(1e9));
+  fromMINA: (amount: bigint | number): MinaAmount => {
+    if (typeof amount === 'number') {
+      return UInt64.from(amount * 1e9);
+    } else return UInt64.from(amount * BigInt(1e9));
   },
   toMINA: (amount: MinaAmount): bigint => {
     return amount.toBigInt() / BigInt(1e9);
@@ -15,5 +17,19 @@ const MinaAmount = {
   },
   toNanoMINA: (amount: MinaAmount): bigint => {
     return amount.toBigInt();
+  },
+};
+
+type BalanceChange = Int64;
+const BalanceChange = {
+  positive: {
+    fromMINA: (amount: bigint): BalanceChange => {
+      return Int64.from(amount * BigInt(1e9));
+    },
+  },
+  negative: {
+    fromMINA: (amount: bigint): BalanceChange => {
+      return Int64.from(-amount * BigInt(1e9));
+    },
   },
 };
