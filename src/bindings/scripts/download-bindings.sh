040ac764354996a01725cf781e2216bad12277fb
Geometer1729 <16kuhnb@gmail.com>
2025-04-09 16:10:05 -0400

    fix scripts add to package.json

--- src/bindings/scripts/download-bindings.sh
+++ src/bindings/scripts/download-bindings.sh
@@ -3,15 +3,21 @@ set -e
 # TODO good error handleing
 
 REV=$(git rev-parse HEAD)
-RUN_ID=$(gh run list --commit "$REV" --workflow 'Build o1js' --json databaseId --jq '.[0].databaseId')
+RUN_ID=$(gh run list --commit "$REV" --workflow 'Build bindings' --json databaseId --jq '.[0].databaseId')
 
-if [ -n "$RUN_ID" ]
+if [ -z "$RUN_ID" ]
 then
-  gh run watch "$RUN_ID" --exit-status || echo "Warning: ci failed on this job, trying to download bindings anyway"
-  #remove if it exists but don't fail
-  [ -z ".bindings_download/bindings.tar.gz" ] || rm .bindings_download/bindings.tar.gz
-  gh run download "$RUN_ID" --dir .bindings_download --name=bindings.tar.gz
-  tar xzf .bindings_download/bindings.tar.gz -C src/bindings --overwrite
-else
-
+  echo bindings have not been built for this commit
+  echo you may want to:
+  echo - switch to a commit where they have been built
+  echo - trigger a remote build with npm run build:bindings-remote
+  echo - open a pr and run ci
+  echo - if you are running remote-build you may not have pushed your latest commit
+  exit 1
 fi
+
+gh run watch "$RUN_ID" --exit-status || echo "Warning: ci failed on this job, trying to download bindings anyway"
+#remove if it exists but don't fail
+[ -z ".bindings_download/bindings.tar.gz" ] || rm .bindings_download/bindings.tar.gz
+gh run download "$RUN_ID" --dir .bindings_download --name=bindings.tar.gz
+tar xzf .bindings_download/bindings.tar.gz -C src/bindings --overwrite
--- src/bindings/scripts/remote-build-bindings.sh
+++ src/bindings/scripts/remote-build-bindings.sh
@@ -1,8 +1,5 @@
 # /usr/bin/env bash
 set -e
-# TODO this runs all of ci which isn't really needed
-gh workflow run build-action.yml --ref $(git branch --show-current)
-REV=$(git rev-parse HEAD)
-RUN_ID=$(gh run list --commit "$REV" --workflow 'Build o1js' --json databaseId --jq '.[0].databaseId')
-gh run watch "$RUN_ID" --exit-status
+gh workflow run build-bindings.yml --ref $(git branch --show-current)
+sleep 2s # wait a bit to make sure the job exists
 "$(dirname "$0")"/download-bindings.sh
