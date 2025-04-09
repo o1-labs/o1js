50ecb6bbf24af2bb2f82778518e587fef5a7cb50
Geometer1729 <16kuhnb@gmail.com>
2025-04-09 14:46:03 -0400

    start new branch to test manual trigger

--- src/bindings/scripts/download-bindings.sh
+++ src/bindings/scripts/download-bindings.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+# /usr/bin/env bash
+set -e
+# TODO good error handleing
+
 REV=$(git rev-parse HEAD)
 RUN_ID=$(gh run list --commit "$REV" --workflow 'Build o1js' --json databaseId --jq '.[0].databaseId')
-
+#remove if it exists but don't fail
 [ -z ".bindings_download/bindings.tar.gz" ] || rm .bindings_download/bindings.tar.gz
 gh run download "$RUN_ID" --dir .bindings_download --name=bindings.tar.gz
 tar xzf .bindings_download/bindings.tar.gz -C src/bindings --overwrite
