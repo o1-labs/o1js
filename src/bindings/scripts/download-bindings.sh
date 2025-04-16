1a4df8a888e5190b47fbb782faaf959f09714e29
Geometer1729 <16kuhnb@gmail.com>
2025-04-16 15:05:09 -0400

    improve error when gh is not installed

--- src/bindings/scripts/download-bindings.sh
+++ src/bindings/scripts/download-bindings.sh
@@ -1,6 +1,8 @@
 # /usr/bin/env bash
 set -e
 
+(which gh > /dev/null 2>&1) || (echo "Please install gh the github cli tool: https://github.com/cli/cli#installation" && exit 1)
+
 REV=${REV:=$(git rev-parse HEAD)}
 RUN_ID=$(gh run list --commit "$REV" --workflow 'build-bindings.yml' --json databaseId --jq '.[0].databaseId')
 
