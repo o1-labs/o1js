e889c30feb68ae96a7a0e4936040f6721016bcc5
Boray Saygılıer <bsaygilier33@gmail.com>
2025-04-28 17:29:51 +0300

    test: extend vk unit test

--- src/lib/proof-system/verification-key.unit-test.ts
+++ src/lib/proof-system/verification-key.unit-test.ts
@@ -1,5 +1,6 @@
 import { assert } from 'console';
 import { VerificationKey } from './verification-key.js';
+import { Field } from '../provable/wrapped.js';
 
 console.log('verification key consistency check (generated and cached)');
 let generated = await VerificationKey.dummy();
@@ -9,4 +10,13 @@ assert(generated.data === cached.data, 'data equals');
 assert(generated.hash.equals(cached.hash).toBoolean(), 'hash equals');
 
 let vkIsValid = await VerificationKey.checkValidity(generated);
-assert(vkIsValid, 'verification key is valid');
+assert(vkIsValid, 'valid verification key is being rejected as invalid');
+
+const invalidVerificationKey = new VerificationKey({
+    data: generated.data,
+    hash: Field.random(),
+  });
+
+
+let vkIsNotValid = await VerificationKey.checkValidity(invalidVerificationKey);
+assert(vkIsNotValid === false, 'invalid verification key is being accepted as valid');
