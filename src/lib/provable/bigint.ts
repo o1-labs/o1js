7f6e7a8b4f15bdc4ecdb3d7388b58556f519552e
boray <bsaygilier33@gmail.com>
2025-05-06 17:09:15 +0300

    fix: clone method()

--- src/lib/provable/bigint.ts
+++ src/lib/provable/bigint.ts
@@ -228,7 +228,9 @@ function createProvableBigInt(modulus: bigint, config?: BigIntParameter) {
      * @returns A new ProvableBigInt instance with the same value
      */
     clone(): ProvableBigInt_ {
-      return new ProvableBigInt_(this.fields, this.value);
+      const clonedFields = this.fields.map(f => new Field(f.toBigInt()));
+      const clonedValue = this.value.get();
+      return new ProvableBigInt_(clonedFields,Unconstrained.from(clonedValue));
     }
 
     /**
