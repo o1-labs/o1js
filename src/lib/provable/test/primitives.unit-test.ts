d30855e9abd8df98edf2c1f34a2ad6dd323b7e17
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2025-06-03 10:27:29 +0100

    Update primitives unit tests

--- src/lib/provable/test/primitives.unit-test.ts
+++ src/lib/provable/test/primitives.unit-test.ts
@@ -1,22 +1,25 @@
-import { Circuit, circuitMain } from '../../proof-system/circuit.js';
+import { ZkFunction } from '../../proof-system/zkFunction.js';
 import { UInt64, UInt32 } from '../int.js';
 import { expect } from 'expect';
 import { Provable } from '../provable.js';
+import { Undefined } from '../../proof-system/zkprogram.js';
 
-class Primitives extends Circuit {
-  @circuitMain
-  static main() {
+const primitives = ZkFunction({
+  name: 'Primitives',
+  publicInputType: Undefined,
+  privateInputTypes: [],
+  main: () => {
     // division
     let x64 = Provable.witness(UInt64, () => 10n);
     x64.div(2).assertEquals(UInt64.from(5));
     let x32 = Provable.witness(UInt32, () => 15n);
     x32.div(4).assertEquals(UInt32.from(3));
-  }
-}
+  },
+});
 
-let keypair = await Primitives.generateKeypair();
-let proof = await Primitives.prove([], [], keypair);
-let ok = await Primitives.verify([], keypair.verificationKey(), proof);
+let keypair = await primitives.generateKeypair();
+let proof = await primitives.prove([], Undefined.empty(), keypair);
+let ok = await primitives.verify(Undefined.empty(), keypair.verificationKey(), proof);
 
 expect(ok).toEqual(true);
 
