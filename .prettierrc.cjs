520b2a5fa252ae7285f872e30d3d9a96178467b1
Danny Willems <be.danny.willems@gmail.com>
2025-07-08 15:47:11 +0200

    Deps: add plugin to sort imports

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -4,4 +4,14 @@ module.exports = {
   semi: true,
   singleQuote: true,
   printWidth: 100,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>', // External libraries
+    '^@/.*', // Absolute aliases (e.g., `@/components`)
+    '^\\.\\./.*', // Relative parent imports
+    '^\\./.*', // Relative sibling imports
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
 };
