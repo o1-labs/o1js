028089eaa0ee24d70cc5df32b0d716c88e8019ad
Anaïs Querol <anais.querol@gmail.com>
2025-07-08 12:59:48 +0200

    only half a generic gate with Field type

--- src/lib/provable/test/primitives.unit-test.ts
+++ src/lib/provable/test/primitives.unit-test.ts
@@ -1,6 +1,7 @@
 import { ZkFunction } from '../../proof-system/zkfunction.js';
 import { UInt64, UInt32 } from '../int.js';
 import { expect } from 'expect';
+import { Field } from '../field.js';
 import { Provable } from '../provable.js';
 
 const primitives = ZkFunction({
@@ -8,12 +9,18 @@ const primitives = ZkFunction({
   privateInputTypes: [],
   main: () => {
     // division
-    let x64 = Provable.witness(UInt64, () => 10n);
-    x64.div(2).assertEquals(UInt64.from(5));
+    let x64 = Provable.witness(Field, () => 10n);
+    x64.div(2).assertEquals(Field.from(5));
+    /*let x32 = Provable.witness(Field, () => 12n);
+    x32.div(4).assertEquals(Field.from(3));
+    let x16 = Provable.witness(Field, () => 14n);
+    x16.div(2).assertEquals(Field.from(7));*/
   },
 });
 
 const { verificationKey } = await primitives.compile();
+let analysis = await primitives.constraintSystem();
+console.log(analysis);
 let proof = await primitives.prove();
 let ok = await primitives.verify(proof, verificationKey);
 
