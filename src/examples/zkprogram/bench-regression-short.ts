d6310ed129dd32e006356402e5b2d5ad1007cb02
Anaïs Querol <anais.querol@gmail.com>
2025-07-11 13:50:33 +0200

    add programs

---
+++ src/examples/zkprogram/bench-regression-short.ts
@@ -0,0 +1,28 @@
+import { Field, ZkProgram, verify } from 'o1js';
+
+let MyProgram = ZkProgram({
+  name: 'bench-regression-short',
+  publicOutput: Field,
+  methods: {
+    baseCase: {
+      privateInputs: [],
+      async method() {
+        return {
+          publicOutput: Field(1),
+        };
+      },
+    },
+  },
+});
+
+console.time('compile (with cache)');
+let { verificationKey } = await MyProgram.compile();
+console.timeEnd('compile (with cache)');
+
+console.time('proving');
+let result = await MyProgram.baseCase();
+console.timeEnd('proving');
+
+console.log('verifying');
+let ok = await verify(result.proof, verificationKey);
+console.log('ok', ok);
