def795ecfe6fb6205057792418f0b033d1a0b12f
leon chou <leon@o1labs.org>
2025-09-30 12:49:43 -0400

    Added cache regression testing framework with uploads, downloads, and verifications of existing logic

---
+++ scripts/tests/dump-cache-regressions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail
+
+source ./scripts/lib/ux.sh
+
+ARTIFACT_PIN=$(date -Iseconds)
+
+WORKDIR=tests/test-artifacts/cache/$ARTIFACT_PIN/
+mkdir -p $WORKDIR
+
+./run ./src/tests/cache/simple-regression.ts --bundle --mode dump --tarball $WORKDIR/simple-regression.tar.gz
+./run ./src/tests/cache/complex-regression.ts --bundle --keep --mode dump --tarball $WORKDIR/complex-regression.tar.gz
+
+gcloud storage cp --recursive $WORKDIR gs://o1js-ci/tests/cache/fixtures/
+
+echo "Uploaded artifacts to: ($ARTIFACT_PIN)"
