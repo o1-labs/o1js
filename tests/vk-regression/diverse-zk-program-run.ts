ff65734d2cf7cf6d527ef3c4510a9cddc1169d7b
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2025-09-30 21:16:29 +0100

    Update "diverse-zk-program-run.ts" to use the performance regression framework

--- tests/vk-regression/diverse-zk-program-run.ts
+++ tests/vk-regression/diverse-zk-program-run.ts
@@ -1,11 +1,21 @@
 import assert from 'node:assert';
 import { diverse, Bytes128 } from './diverse-zk-program.js';
+import { perfStart, perfEnd } from '../../src/lib/testing/perf-regression.js';
 
-console.log('testing proof generation for diverse program');
+const cs = await diverse.analyzeMethods();
+
+perfStart('compile', diverse.name, cs);
 await diverse.compile();
+perfEnd();
 
+perfStart('prove', diverse.name, cs, 'sha3');
 let { proof: proof1 } = await diverse.sha3(Bytes128.fromString('hello'));
+perfEnd();
+
 assert(await diverse.verify(proof1), 'verifies');
 
+perfStart('prove', diverse.name, cs, 'recursive');
 let { proof: proof2 } = await diverse.recursive(proof1);
+perfEnd();
+
 assert(await diverse.verify(proof2), 'verifies');
