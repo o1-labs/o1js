c99cec7acadc10ae7b3242c5031cd07474f119b8
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2025-10-09 18:18:11 +0300

    Update program-small-big.ts to use the performance regression framework

--- tests/perf-regression/perf-regression.sh
+++ tests/perf-regression/perf-regression.sh
@@ -20,6 +20,7 @@ STOP_AFTER=2 ./run src/examples/zkprogram/hash-chain.ts --bundle "$MODE"
 STOP_AFTER=4 ./run src/examples/zkprogram/gadgets.ts --bundle "$MODE"
 STOP_AFTER=6 ./run src/examples/zkprogram/side-loading/run.ts --bundle "$MODE"
 STOP_AFTER=4 ./run src/examples/zkprogram/runtime-table/run.ts --bundle "$MODE"
+STOP_AFTER=4 ./run src/examples/zkprogram/program-small-big.ts --bundle "$MODE"
 
 # Run CS + zkApps performance regression tests
 ./run tests/perf-regression/perf-regression.ts --bundle "$MODE"
