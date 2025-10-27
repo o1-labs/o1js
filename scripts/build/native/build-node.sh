2fba2d3152317ee7ac57a1c1b12d4d6ac8226850
leon chou <leon@o1labs.org>
2025-10-27 10:43:18 -0400

    native bindings typegen

--- scripts/build/native/build-node.sh
+++ scripts/build/native/build-node.sh
@@ -6,7 +6,7 @@ source ./scripts/lib/ux.sh
 setup_script "native-node-build" "native node build"
 
 KIMCHI_PATH=./src/mina/src/lib/crypto/kimchi_bindings/js/native
-BUILT_PATH=./_build/default/$KIMCHI_PATH
+BUILT_PATH=./src/mina/src/lib/crypto/kimchi_bindings/js/native/artifacts
 BINDINGS_PATH=./src/bindings/compiled/native
 
 mkdir -p $BINDINGS_PATH
@@ -19,5 +19,7 @@ info "copying artifacts into the right place..."
 
 cp $BUILT_PATH/plonk_napi.node $BINDINGS_PATH/plonk_napi.node
 chmod 660 $BINDINGS_PATH/plonk_napi.node
+cp $BUILT_PATH/index.d.ts $BINDINGS_PATH/index.d.ts
+chmod 660 $BINDINGS_PATH/index.d.ts
 
 success "Native build success!"
