646a7387ae3d5b632a5d117653d076498fafa75f
Florian <florian@technotro.com>
2025-10-30 18:54:11 +0700

    add shiftsFromRust

--- src/bindings/crypto/bindings-napi.ts
+++ src/bindings/crypto/bindings-napi.ts
@@ -1,4 +1,4 @@
-import { fieldsFromRustFlat, fieldsToRustFlat } from './bindings/conversion-base.js';
+import { fieldFromRust, fieldsFromRustFlat, fieldsToRustFlat } from './bindings/conversion-base.js';
 
 export { bindingsNapi };
 
@@ -6,25 +6,26 @@ function bindingsNapi(napi: any) {
   return {
     fp: {
       vectorToRust: (fields: any) => {
-        console.log('values going in ', fields);
-        let res = fieldsToRustFlat(fields);
-        console.log('values going out ', res);
-        return res;
+        return fieldsToRustFlat(fields);
       },
       vectorFromRust: fieldsFromRustFlat,
+      shiftsFromRust(s: any) {
+        let shifts = [s.s0, s.s1, s.s2, s.s3, s.s4, s.s5, s.s6].map((x) => Uint8Array.from(x));
+        let shifted = [0, ...shifts.map(fieldFromRust)];
+        return shifted;
+      },
     },
     fq: {
+      shiftsFromRust(s: any) {
+        let shifts = [s.s0, s.s1, s.s2, s.s3, s.s4, s.s5, s.s6].map((x) => Uint8Array.from(x));
+        let shifted = [0, ...shifts.map(fieldFromRust)];
+        return shifted;
+      },
       vectorToRust: (fields: any) => {
-        console.log('values going in ', fields);
-        let res = fieldsToRustFlat(fields);
-        console.log('values going out ', res);
-        return res;
+        return fieldsToRustFlat(fields);
       },
       vectorFromRust: (fieldBytes: any) => {
-        console.log('values going in ', fieldBytes);
-        let res = fieldsFromRustFlat(fieldBytes);
-        console.log('values going out ', res);
-        return res;
+        return fieldsFromRustFlat(fieldBytes);
       },
     },
   };
