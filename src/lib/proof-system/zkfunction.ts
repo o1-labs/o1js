59fc629d0d57184732bd61eecd4b79ee071a2692
Florian <florian@technotro.com>
2025-11-12 23:21:07 +0700

    unused import

--- src/lib/proof-system/zkfunction.ts
+++ src/lib/proof-system/zkfunction.ts
@@ -1,5 +1,4 @@
 import { Pickles, Snarky, initializeBindings, withThreadPool } from '../../bindings.js';
-import { getRustConversion } from '../../bindings/crypto/bindings.js';
 import { MlFieldArray, MlFieldConstArray } from '../ml/fields.js';
 import {
   ConstraintSystemSummary,
