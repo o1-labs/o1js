78f299d1cce11e86fe8966b20770d80b3be70b52
Danny Willems <be.danny.willems@gmail.com>
2026-01-13 10:38:09 -0700

    Bump up nightly to 2025-12-11

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
           ((pkgs.rustChannelOf
             {
               channel = "nightly";
-              date = "2024-06-13";
+              date = "2025-12-11";
               sha256 = "sha256-s5nlYcYG9EuO2HK2BU3PkI928DZBKCTJ4U9bz3RX1t4=";
             }).rust.override
             {
@@ -205,7 +205,7 @@
             checkPhase = if pkgs.stdenv.isDarwin then "" else null;
             text =
               ''
-                if [ "$1" = run ] && { [ "$2" = nightly-2024-06-13 ] || [[ "$2" =~ 1.79-x86_64* ]]; }
+                if [ "$1" = run ] && { [ "$2" = nightly-2025-12-11 ] || [[ "$2" =~ 1.79-x86_64* ]]; }
                 then
                   echo using nix toolchain
                   ${rustup}/bin/rustup run nix "''${@:3}"
