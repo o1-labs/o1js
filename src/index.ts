793e9302b4c2a2633b2626b27145436c5f73aebb
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2026-01-29 11:39:27 +0300

    Export "fetchTransactionDepth"

--- src/index.ts
+++ src/index.ts
@@ -99,6 +99,7 @@ export {
   fetchEvents,
   fetchLastBlock,
   fetchTimedAccountInfo,
+  fetchTransactionDepth,
   fetchTransactionStatus,
   Lightnet,
   sendZkapp,
@@ -106,7 +107,11 @@ export {
   setGraphqlEndpoint,
   setGraphqlEndpoints,
 } from './lib/mina/v1/fetch.js';
-export type { TransactionStatus } from './lib/mina/v1/graphql.js';
+export type {
+  DepthOptions,
+  TransactionDepthInfo,
+  TransactionStatus,
+} from './lib/mina/v1/graphql.js';
 export * as Encryption from './lib/provable/crypto/encryption.js';
 export { MerkleMap, MerkleMapWitness } from './lib/provable/merkle-map.js';
 export { MerkleTree, MerkleWitness } from './lib/provable/merkle-tree.js';
