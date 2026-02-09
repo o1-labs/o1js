a500ba339e1e517edc77df64b21eb9e64fe194df
Florian <florian@technotro.com>
2026-02-09 14:58:16 +0700

    refactor to use shared functionaity

--- src/bindings/crypto/bindings/conversion-proof.ts
+++ src/bindings/crypto/bindings/conversion-proof.ts
@@ -25,8 +25,13 @@ import {
   fieldsToRustFlat,
 } from './conversion-base.js';
 import { ConversionCore, ConversionCores, mapToUint32Array, unwrap } from './conversion-core.js';
+import {
+  proofEvaluationsToRust,
+  proofEvaluationsFromRust,
+  pointEvalsOptionToRust,
+  pointEvalsOptionFromRust,
+} from './conversion-proof-shared.js';
 import type {
-  Field,
   LookupCommitments,
   LookupTable,
   OpeningProof,
@@ -43,12 +48,6 @@ import type {
 } from './kimchi-types.js';
 
 export { proofConversion };
-
-const fieldToRust_ = (x: Field) => fieldToRust(x);
-const proofEvaluationsToRust = mapProofEvaluations(fieldToRust_);
-const proofEvaluationsFromRust = mapProofEvaluations(fieldFromRust);
-const pointEvalsOptionToRust = mapPointEvalsOption(fieldToRust_);
-const pointEvalsOptionFromRust = mapPointEvalsOption(fieldFromRust);
 
 type WasmProofEvaluations = [
   0,
@@ -265,85 +264,6 @@ function proofConversionPerField(
     lookupTablesToRust([, ...tables]: MlArray<LookupTable>) {
       return mapToUint32Array(tables, (table) => unwrap(lookupTableToRust(table)));
     },
-  };
-}
-
-function createMapPointEvals<Field1, Field2>(map: (x: Field1) => Field2) {
-  return (evals: PointEvaluations<Field1>): PointEvaluations<Field2> => {
-    let [, zeta, zeta_omega] = evals;
-    return [0, MlArray.map(zeta, map), MlArray.map(zeta_omega, map)];
-  };
-}
-
-function mapPointEvalsOption<Field1, Field2>(map: (x: Field1) => Field2) {
-  return (evals: MlOption<PointEvaluations<Field1>>) =>
-    MlOption.map(evals, createMapPointEvals(map));
-}
-
-function mapProofEvaluations<Field1, Field2>(map: (x: Field1) => Field2) {
-  const mapPointEvals = createMapPointEvals(map);
-
-  const mapPointEvalsOption = (
-    evals: MlOption<PointEvaluations<Field1>>
-  ): MlOption<PointEvaluations<Field2>> => MlOption.map(evals, mapPointEvals);
-
-  return function mapProofEvaluations(evals: ProofEvaluations<Field1>): ProofEvaluations<Field2> {
-    let [
-      ,
-      w,
-      z,
-      s,
-      coeffs,
-      genericSelector,
-      poseidonSelector,
-      completeAddSelector,
-      mulSelector,
-      emulSelector,
-      endomulScalarSelector,
-      rangeCheck0Selector,
-      rangeCheck1Selector,
-      foreignFieldAddSelector,
-      foreignFieldMulSelector,
-      xorSelector,
-      rotSelector,
-      lookupAggregation,
-      lookupTable,
-      lookupSorted,
-      runtimeLookupTable,
-      runtimeLookupTableSelector,
-      xorLookupSelector,
-      lookupGateLookupSelector,
-      rangeCheckLookupSelector,
-      foreignFieldMulLookupSelector,
-    ] = evals;
-    return [
-      0,
-      MlTuple.map(w, mapPointEvals),
-      mapPointEvals(z),
-      MlTuple.map(s, mapPointEvals),
-      MlTuple.map(coeffs, mapPointEvals),
-      mapPointEvals(genericSelector),
-      mapPointEvals(poseidonSelector),
-      mapPointEvals(completeAddSelector),
-      mapPointEvals(mulSelector),
-      mapPointEvals(emulSelector),
-      mapPointEvals(endomulScalarSelector),
-      mapPointEvalsOption(rangeCheck0Selector),
-      mapPointEvalsOption(rangeCheck1Selector),
-      mapPointEvalsOption(foreignFieldAddSelector),
-      mapPointEvalsOption(foreignFieldMulSelector),
-      mapPointEvalsOption(xorSelector),
-      mapPointEvalsOption(rotSelector),
-      mapPointEvalsOption(lookupAggregation),
-      mapPointEvalsOption(lookupTable),
-      MlArray.map(lookupSorted, mapPointEvalsOption),
-      mapPointEvalsOption(runtimeLookupTable),
-      mapPointEvalsOption(runtimeLookupTableSelector),
-      mapPointEvalsOption(xorLookupSelector),
-      mapPointEvalsOption(lookupGateLookupSelector),
-      mapPointEvalsOption(rangeCheckLookupSelector),
-      mapPointEvalsOption(foreignFieldMulLookupSelector),
-    ];
   };
 }
 
