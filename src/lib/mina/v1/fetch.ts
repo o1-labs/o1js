fe361b3065e9772a08389f578d60df6e9fc6d2fd
Florian <florian@technotro.com>
2026-03-24 15:54:25 +0700

    add some debug logging

--- src/lib/mina/v1/fetch.ts
+++ src/lib/mina/v1/fetch.ts
@@ -1299,8 +1299,11 @@ async function checkResponseStatus<TDataResponse>(
 }
 
 function inferError(error: unknown): FetchError {
-  let errorMessage = JSON.stringify(error);
-  if (error instanceof AbortSignal) {
+  let errorMessage =
+    error instanceof Error
+      ? `${error.name}: ${error.message}`
+      : JSON.stringify(error);
+  if (error instanceof Error && error.name === 'AbortError') {
     return { statusCode: 408, statusText: `Request Timeout: ${errorMessage}` };
   } else {
     return {
