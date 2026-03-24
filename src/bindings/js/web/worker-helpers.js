c61ad5521ffafbcdd13444c7ad54c0e53ea682e4
Florian <florian@technotro.com>
2026-03-24 15:21:35 +0700

    attempt web worker fix (tbd)

--- src/bindings/js/web/worker-helpers.js
+++ src/bindings/js/web/worker-helpers.js
@@ -54,15 +54,23 @@ function waitForMessage(target, type) {
 function workerHelperMain() {
   let { default: init, wbg_rayon_start_worker } = wasm();
 
-  waitForMessage(self, 'wasm_bindgen_worker_init').then(async (data) => {
-    await init(data.module, data.memory);
-    postMessage({ type: 'wasm_bindgen_worker_ready' });
-    wbg_rayon_start_worker(data.receiver);
-  });
+  waitForMessage(self, 'wasm_bindgen_worker_init')
+    .then(async (data) => {
+      await init(data.module, data.memory);
+      postMessage({ type: 'wasm_bindgen_worker_ready' });
+      wbg_rayon_start_worker(data.receiver);
+    })
+    .catch((err) => {
+      postMessage({
+        type: 'wasm_bindgen_worker_error',
+        error: String(err?.stack ?? err),
+      });
+    });
 }
 workerHelperMain.deps = [wasm, waitForMessage];
 
 async function startWorkers(module, memory, builder) {
+  const startupTimeoutMs = 30_000;
   const workerInit = {
     type: 'wasm_bindgen_worker_init',
     module,
@@ -81,7 +89,35 @@ async function startWorkers(module, memory, builder) {
     self._workers.push(worker);
   }
   URL.revokeObjectURL(url);
-  await Promise.all(self._workers.map((w) => waitForMessage(w, 'wasm_bindgen_worker_ready')));
+
+  await Promise.all(
+    self._workers.map(
+      (w) =>
+        new Promise((resolve, reject) => {
+          let timer = setTimeout(() => {
+            cleanup();
+            reject(new Error('Timed out waiting for wasm worker startup'));
+          }, startupTimeoutMs);
+
+          function cleanup() {
+            clearTimeout(timer);
+            w.removeEventListener('message', onMessage);
+          }
+
+          function onMessage({ data }) {
+            if (data?.type === 'wasm_bindgen_worker_ready') {
+              cleanup();
+              resolve(w);
+            } else if (data?.type === 'wasm_bindgen_worker_error') {
+              cleanup();
+              reject(new Error('WASM worker init failed: ' + data.error));
+            }
+          }
+
+          w.addEventListener('message', onMessage);
+        })
+    )
+  );
   builder.build();
 }
 startWorkers.deps = [srcFromFunctionModule, waitForMessage, workerHelperMain];
