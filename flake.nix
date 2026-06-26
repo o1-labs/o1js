b6b05fdd56495d75e081662310988797f7deed69
Florian <florian@technotro.com>
2026-06-26 13:37:58 +0700

    more features flags

--- flake.nix
+++ flake.nix
@@ -287,6 +287,7 @@
           version = "0.1.0";
           CARGO_TARGET_DIR = "./target";
           cargoBuildFlags = [ "--features" "o1-utils/std" ];
+          cargoCheckFlags = [ "--features" "o1-utils/std" ];
           cargoLock = {
             lockFile = ./src/mina/src/lib/crypto/proof-systems/Cargo.lock;
             outputHashes = narHashesFromCargoLock
